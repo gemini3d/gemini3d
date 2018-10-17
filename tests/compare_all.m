3fd16dd44dbef1b0b2e061964d686381d15b645f
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-17 10:27:02 -0400

    matlab char type

--- tests/compare_all.m
+++ tests/compare_all.m
@@ -26,7 +26,7 @@ cwd = fileparts(mfilename('fullpath'));
 addpath([cwd, filesep, '..', filesep, 'script_utils'])
 addpath([cwd, filesep, '..', filesep, 'vis'])
 %% READ IN THE SIMULATION INFORMATION
-[ymd0,UTsec0,tdur,dtout,flagoutput,mloc]=readconfig([dir1,filesep,'inputs/config.ini']);
+[ymd0,UTsec0,tdur,dtout,~,mloc]=readconfig([dir1,filesep,'inputs/config.ini']);
 %% load grid
 xg=readgrid([dir1,filesep,'inputs',filesep]);
 %% TIMES OF INTEREST
@@ -41,39 +41,39 @@ for it=1:Nt
   [neA,v1A,TiA,TeA,J1A,v2A,v3A,J2A,J3A] = loadframe(dir1,UTsec,ymd,UTsec0,ymd0,mloc,xg);
   [neB,v1B,TiB,TeB,J1B,v2B,v3B,J2B,J3B] = loadframe(dir2,UTsec,ymd,UTsec0,ymd0,mloc,xg);
   
-  assert_allclose(neA,neB,rtolN,atol,["Ne accuracy, time #",int2str(it)])
+  assert_allclose(neA,neB,rtolN,atol,['Ne accuracy, time #',int2str(it)])
   
   if it > 1
-    assert_allclose(v1A,v1B,rtolV,atolV,["V1 accuracy, time #",int2str(it)])
+    assert_allclose(v1A,v1B,rtolV,atolV,['V1 accuracy, time #',int2str(it)])
   end
-  assert_allclose(v2A,v2B,rtolV,atolV,["V2 accuracy, time #",int2str(it)])
-  assert_allclose(v3A,v3B,rtolV,atolV,["V3 accuracy, time #",int2str(it)])
+  assert_allclose(v2A,v2B,rtolV,atolV,['V2 accuracy, time #',int2str(it)])
+  assert_allclose(v3A,v3B,rtolV,atolV,['V3 accuracy, time #',int2str(it)])
   
   if it > 1
-    assert_allclose(TiA,TiB,rtolT,atol,["Ti accuracy, time #",int2str(it)])
+    assert_allclose(TiA,TiB,rtolT,atol,['Ti accuracy, time #',int2str(it)])
   end
-  assert_allclose(TeA,TeB,rtolT,atol,["Te accuracy, time #",int2str(it)])
+  assert_allclose(TeA,TeB,rtolT,atol,['Te accuracy, time #',int2str(it)])
   
-  assert_allclose(J1A,J1B,rtolJ,atolJ,["J1 accuracy, time #",int2str(it)])
-  assert_allclose(J2A,J2B,rtolJ,atolJ,["J2 accuracy, time #",int2str(it)])
-  assert_allclose(J3A,J3B,rtolJ,atolJ,["J3 accuracy, time #",int2str(it)])
+  assert_allclose(J1A,J1B,rtolJ,atolJ,['J1 accuracy, time #',int2str(it)])
+  assert_allclose(J2A,J2B,rtolJ,atolJ,['J2 accuracy, time #',int2str(it)])
+  assert_allclose(J3A,J3B,rtolJ,atolJ,['J3 accuracy, time #',int2str(it)])
   
   %% assert time steps have unique output (earth always rotating...)
   if it>1
-    assert_allclose(Ne,neA,rtol,atol,["Ne time step ",int2str(it)," too similar to prior step"],true) 
+    assert_allclose(Ne,neA,rtol,atol,['Ne time step ',int2str(it),' too similar to prior step'],true) 
     if it<2
-      assert_allclose(v1,v1A,rtol,atol,["V1 time step ",int2str(it)," too similar to prior step"],true) 
-      assert_allclose(v2,v2A,rtol,atol,["V2 time step ",int2str(it)," too similar to prior step"],true) 
-      assert_allclose(v3,v3A,rtol,atol,["V3 time step ",int2str(it)," too similar to prior step"],true) 
-    endif
+      assert_allclose(v1,v1A,rtol,atol,['V1 time step ',int2str(it),' too similar to prior step'],true) 
+      assert_allclose(v2,v2A,rtol,atol,['V2 time step ',int2str(it),' too similar to prior step'],true) 
+      assert_allclose(v3,v3A,rtol,atol,['V3 time step ',int2str(it),' too similar to prior step'],true) 
+    end
     if it<4
-      assert_allclose(Ti,TiA,rtol,atol,["Ti time step ",int2str(it)," too similar to prior step"],true) 
-      assert_allclose(Te,TeA,rtol,atol,["Te time step ",int2str(it)," too similar to prior step"],true) 
+      assert_allclose(Ti,TiA,rtol,atol,['Ti time step ',int2str(it),' too similar to prior step'],true) 
+      assert_allclose(Te,TeA,rtol,atol,['Te time step ',int2str(it),' too similar to prior step'],true) 
     end
     if it==2
-      assert_allclose(J1,J1A,rtol,atol,["J1 time step ",int2str(it)," too similar to prior step"],true,true) 
-      assert_allclose(J2,J2A,rtol,atol,["J2 time step ",int2str(it)," too similar to prior step"],true,true) 
-      assert_allclose(J3,J3A,rtol,atol,["J3 time step ",int2str(it)," too similar to prior step"],true,true) 
+      assert_allclose(J1,J1A,rtol,atol,['J1 time step ',int2str(it),' too similar to prior step'],true,true) 
+      assert_allclose(J2,J2A,rtol,atol,['J2 time step ',int2str(it),' too similar to prior step'],true,true) 
+      assert_allclose(J3,J3A,rtol,atol,['J3 time step ',int2str(it),' too similar to prior step'],true,true) 
     end
   end
   Ne = neA; v1=v1A; v2=v2A; v3=v3A; Ti=TiA; Te=TeA; J1=J1A; J2=J2A; J3=J3A;
