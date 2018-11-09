83f058608f6325782f5aac6256d4ec3fe2ab3386
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-11-09 12:14:01 -0500

    INWORK: cleanup CMake includes, put MSIS in own .cmake file for clarity

--- setup/MSIS00/call_msis_gfortran.f90
+++ setup/MSIS00/call_msis_gfortran.f90
@@ -1,69 +1,67 @@
 program neuatmos
 
+use, intrinsic:: iso_fortran_env, only: sp=>real32
+
 implicit none
-integer :: iyd,sec,mass=48,iz,lz
-real :: f107a,f107,ap(7),stl,apday,ap3
-real :: d(9),t(2)
-real, allocatable :: glat(:),glon(:),alt(:)
-character(256) :: infile,outfile
+
+integer :: iyd,sec,mass=48,iz,lz, u
+real(sp) :: f107a,f107,ap(7),stl,apday,ap3
+real(sp) :: d(9),t(2)
+real(sp), allocatable :: glat(:),glon(:),alt(:)
+
+character(256) :: buf
+character(:), allocatable :: infile,outfile
 
 !read in msis inputs
-call get_command_argument(1,infile)
-!call getarg(1, infile)
-!open(unit=42,file=infile)
+if (command_argument_count() /= 2) error stop 'must specify input and output filenames'
 
-!time date etc.
-!read(42,*) iyd,sec
-!read(42,*) f107a,f107
-!read(42,*) apday,ap3
-!read(42,*) lz
+call get_command_argument(1,buf)
+infile = trim(buf)
 
-open(unit=42,file=infile,status='old',form='unformatted',access='stream')    !use binary to reduce file size and read times
-read(42) iyd
-read(42) sec
-read(42) f107a
-read(42) f107
-read(42) apday
-read(42) ap3
-read(42) lz
+open(newunit=u,file=infile,status='old',form='unformatted',access='stream', action='read')    !use binary to reduce file size and read times
 
-write(*,*) iyd,sec,f107a,f107,apday,ap3,lz
+read(u) iyd
+read(u) sec
+read(u) f107a
+read(u) f107
+read(u) apday
+read(u) ap3
+read(u) lz
 
+print *, infile,iyd,sec,f107a,f107,apday,ap3,lz
 
-!allocate correct amount of memory
 allocate(glat(lz),glon(lz),alt(lz))
 
-!lat,lon,alt data
-!do iz=1,lz
-!  read(42,*) glat(iz),glon(iz),alt(iz)
-!end do
+read(u) glat,glon,alt
 
-read(42) glat,glon,alt
+ close(u)
 
-close(42)
-
- ap(1:7)=apday
- ap(2)=ap3
+! --- Run MSIS
+ap(1:7)=apday
+ap(2)=ap3
 
 !switch to mksa units
- call meters(.true.)
+call meters(.true.)
 
 !output file
-!call getarg(2,outfile)
-call get_command_argument(2,outfile)
-open(unit=43,file=outfile,status='replace',form='unformatted',access='stream')    !use binary to reduce file size and read times
+call get_command_argument(2, buf)
+outfile = trim(buf)
+
+open(newunit=u,file=outfile,status='replace',form='unformatted',access='stream', action='write')
+!use binary to reduce file size and read times
 
 !call to msis routine
 do iz=1,lz
-  stl=sec/3600.+glon(iz)/15.
+  stl = sec/3600. + glon(iz)/15.
   call gtd7(iyd,sec,alt(iz),glat(iz),glon(iz),stl,f107a,f107,ap,mass,d,t)
-  write(43) alt(iz),d(1:9),t(2)
+  write(u) alt(iz),d(1:9),t(2)
 !  write(*,'(1f20.4, 9e20.8, 1f20.4)') alt(iz),d(1:9),t(2)
 ! write(*,1000),alt(iz),d(1:9),t(2)
 ! 1000 format(1F,9E,1F)
 end do
 
-close(43)
+ close(u)
+ 
 deallocate(glat,glon,alt)
 
 end program neuatmos
