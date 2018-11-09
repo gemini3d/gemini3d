26ed938f9c825d56c60ca99877b214b64bea7155
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-11-09 14:15:45 -0500

    cleaner msis independent build option

--- cmake/msis.cmake
+++
@@ -1,9 +0,0 @@
-
-add_library(msis vendor/msis00/msis00_gfortran.f)
-target_compile_options(msis PRIVATE -std=legacy -w -fno-implicit-none)
-
-# --- for setting up an equilibrium simulation --
-
-add_executable(msis_setup setup/MSIS00/call_msis_gfortran.f90)
-target_link_libraries(msis_setup PUBLIC msis)
-
