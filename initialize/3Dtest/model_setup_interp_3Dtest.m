ff8b4b0e4a2d8c2705457693a2231e38eb97a497
Matthew Zettergren <mattzett@users.noreply.github.com>
2018-11-10 07:39:34 -0500

    reduced code duplication in model interpolations

--- initialize/3Dtest/model_setup_interp_3Dtest.m
+++ initialize/3Dtest/model_setup_interp_3Dtest.m
@@ -36,7 +36,6 @@ ID='../../../simulations/3Dtest_eq/'
 %READ IN THE SIMULATION INFORMATION
 [ymd0,UTsec0,tdur,dtout,flagoutput,mloc]=readconfig([ID,'/inputs/config.ini']);
 xgin=readgrid([ID,'/inputs/']);
-direc=ID;
 
 
 %FIND THE DATE OF THE END FRAEM OF THE SIMULATION (PRESUMABLY THIS WILL BE THE STARTING POITN FOR ANOTEHR)
@@ -44,45 +43,13 @@ direc=ID;
 
 
 %LOAD THE FRAME
+direc=ID;
 [ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts]= ...
     loadframe(direc,ymdend,UTsecend,ymd0,UTsec0,tdur,dtout,flagoutput,mloc,xg);lsp=size(ns,4);
-lsp=size(ns,4);
 
 
 %DO THE INTERPOLATION
-if (lx3~=1)
-  fprintf('Starting interp3''s...\n');
-  [X2,X1,X3]=meshgrid(xgin.x2(3:end-2),xgin.x1(3:end-2),xgin.x3(3:end-2));
-  [X2i,X1i,X3i]=meshgrid(xg.x2(3:end-2),xg.x1(3:end-2),xg.x3(3:end-2));
-  for isp=1:lsp
-    tmpvar=interp3(X2,X1,X3,ns(:,:,:,isp),X2i,X1i,X3i);
-    inds=find(isnan(tmpvar));
-    tmpvar(inds)=1e0;
-    nsi(:,:,:,isp)=tmpvar;
-    tmpvar=interp3(X2,X1,X3,vs1(:,:,:,isp),X2i,X1i,X3i);
-    tmpvar(inds)=0e0;
-    vs1i(:,:,:,isp)=tmpvar;
-    tmpvar=interp3(X2,X1,X3,Ts(:,:,:,isp),X2i,X1i,X3i);
-    tmpvar(inds)=100e0;
-    Tsi(:,:,:,isp)=tmpvar;
-  end
-else
-  fprintf('Starting interp2''s...\n');
-  [X2,X1]=meshgrid(xgin.x2(3:end-2),xgin.x1(3:end-2));
-  [X2i,X1i]=meshgrid(xg.x2(3:end-2),xg.x1(3:end-2));
-  for isp=1:lsp
-    tmpvar=interp2(X2,X1,squeeze(ns(:,:,:,isp)),X2i,X1i);
-    inds=find(isnan(tmpvar));
-    tmpvar(inds)=1e0;
-    nsi(:,:,:,isp)=reshape(tmpvar,[lx1,lx2,1]);
-    tmpvar=interp2(X2,X1,squeeze(vs1(:,:,:,isp)),X2i,X1i);
-    tmpvar(inds)=0e0;
-    vs1i(:,:,:,isp)=reshape(tmpvar,[lx1,lx2,1]);
-    tmpvar=interp2(X2,X1,squeeze(Ts(:,:,:,isp)),X2i,X1i);
-    tmpvar(inds)=100e0;
-    Tsi(:,:,:,isp)=reshape(tmpvar,[lx1,lx2,1]);
-  end
-end
+[nsi,vs1i,Tsi]=model_resample(xgin,ns,vs1,Ts,xg);
 
 
 %WRITE OUT THE GRID
