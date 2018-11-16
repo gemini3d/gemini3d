62149002386b785655586a5562751aba1c85bb7e
Grubbs <ggrubbs1@gs673-guydt.ndc.nasa.gov>
2018-11-16 11:17:42 -0500

    Removed qproton.f90 from cmake, possibly make optional later.

--- cmake/glow.cmake
+++ cmake/glow.cmake
@@ -10,7 +10,7 @@ function(add_glow)
       ionization/glow/fieldm.f ionization/glow/solzen.f90 ionization/glow/ssflux.f90
       ionization/glow/rcolum.f90 ionization/glow/qback.f90
       ionization/glow/etrans.f90 ionization/glow/exsect.f
-      ionization/glow/gchem.f90 ionization/glow/bands.f90 ionization/glow/qproton.f90
+      ionization/glow/gchem.f90 ionization/glow/bands.f90 #ionization/glow/qproton.f90
       ionization/glow/ephoto.f90
       ionization/glow/egrid.f90 ionization/glow/maxt.f90)
   if(${CMAKE_Fortran_COMPILER_ID} STREQUAL GNU)
