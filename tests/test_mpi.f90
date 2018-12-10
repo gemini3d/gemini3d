5e034412f8195af69dedd9ea9ef24a903fcba72d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-10 10:10:54 -0500

    cleanup build output directories

--- tests/test_mpi.f90
+++ tests/test_mpi.f90
@@ -4,22 +4,29 @@ use mpi_f08
 
 implicit none
 
-integer :: ierr, mrank, msize, vlen
+integer :: mrank, msize, vlen
 character(MPI_MAX_LIBRARY_VERSION_STRING) :: version  ! allocatable not ok
 
 print *,compiler_version(), compiler_options()
 
-call MPI_INIT(ierr)
-call MPI_COMM_RANK(MPI_COMM_WORLD, mrank, ierr)
-call MPI_COMM_SIZE(MPI_COMM_WORLD, msize, ierr)
-call MPI_GET_LIBRARY_VERSION(version, vlen, ierr)
+call MPI_INIT()
+call MPI_COMM_RANK(MPI_COMM_WORLD, mrank)
+call MPI_COMM_SIZE(MPI_COMM_WORLD, msize)
+call MPI_GET_LIBRARY_VERSION(version, vlen)
 
 call MPI_FINALIZE()
 
 print '(A,I3,A,I3,A)', 'Image ', mrank, ' / ', msize, ':',version
 
 print '(A25,A7)','type','value'
-print '(A25,I4)','mpi_real',mpi_real
-print '(A25,I4)','mpi_real8',mpi_real8
+print *,'mpi_real',mpi_real
+print *,'mpi_real8',mpi_real8
 
 end program
+
+
+! intel:
+! type  value
+! mpi_real  1275069468
+! mpi_real8  1275070505
+
