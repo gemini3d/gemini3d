a8be02e33e0e64bd91ec1d7cb5c2c61983d80ad1
Matthew Zettergren <mattzett@users.noreply.github.com>
2018-12-14 11:40:27 +0100

    new erau build script

--- build_erau.sh
+++ build_erau.sh
@@ -4,10 +4,19 @@
 #
 # This is for Matt's machine, so he can use the older libraries he's been working with for some time.
 
-OPTS="-DMUMPS_ROOT=~/lib/MUMPS_4.10.0 -DSCALAPACK_ROOT=~/lib/scalapack-2.0.2"
+#OPTS="-DMUMPS_ROOT=~/lib/MUMPS_4.10.0 -DSCALAPACK_ROOT=~/lib/scalapack-2.0.2"
 
-[[ $1 == "-d" ]] && OPTS="-DCMAKE_BUILD_TYPE=Debug $OPTS"
+[[ $1 == "-d" ]] && OPTS="-DCMAKE_BUILD_TYPE=Debug"
+[[ $1 == "-t" ]] && OPTS="-DTRACE:BOOL=on $OPTS"
+
+MKLROOT=
+LD_LIBRARY_PATH=
+
+export FC=mpifort
+
+rm -r objects/*  # need this one-time in case different compiler e.g. ifort was previously used.
 
 cmake $OPTS -B objects .
 
 cmake --build objects -j
+
