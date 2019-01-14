2ded793800ba2c7189f20d8c35530929d632c71a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-14 15:32:17 -0500

    init [skip ci]

--- build_gnu_centos.sh
+++ build_gnu_centos.sh
@@ -7,14 +7,10 @@
 # *** for subsequent builds, you can just type "make" in the objects/ directory ***
 # (I keep a second Terminal tab for this purpose)
 
-
-OPTS="-DMPI_ROOT=$HOME/.local/openmpi-3.1-gcc7 $OPTS"
-OPTS="-DLAPACK_ROOT=$HOME/.local/lapack-gcc7 $OPTS"
-OPTS="-DSCALAPACK_ROOT=$HOME/.local/scalapack-gcc7 $OPTS"
-OPTS="-DMUMPS_ROOT=$HOME/.local/mumps-gcc7 $OPTS"
-
-export FC=$HOME/.local/openmpi-3.1-gcc7/bin/mpif90
-export CC=$HOME/.local/openmpi-3.1-gcc7/bin/mpicc
+#OPTS="-DMPI_ROOT=$HOME/.local/openmpi-3.1-gcc7 $OPTS"
+#OPTS="-DLAPACK_ROOT=$HOME/.local/lapack-gcc7 $OPTS"
+OPTS="-DSCALAPACK_ROOT=$HOME/flibs-gnu-nomkl/scalapack $OPTS"
+OPTS="-DMUMPS_ROOT=$HOME/flibs-gnu-nomkl/MUMPS $OPTS"
 
 # this temporarily disables Intel compiler (if installed) from messing up your gfortran environment.
 MKLROOT=
@@ -22,6 +18,8 @@ LD_LIBRARY_PATH=
 
 [[ $1 == "-d" ]] && OPTS="-DCMAKE_BUILD_TYPE=Debug $OPTS"
 [[ $1 == "-t" ]] && OPTS="-DTRACE:BOOL=on $OPTS"
+
+export FC=mpif90
 
 rm -rf objects/*  # need this one-time in case different compiler e.g. ifort was previously used.
 
