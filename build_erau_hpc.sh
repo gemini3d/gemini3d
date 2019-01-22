7d369be822825f9f7518756484c2eac6982284f0
Matthew Zettergren <zettergm@vegaln2.cm.cluster>
2019-01-22 16:48:35 -0500

    trying to get vega working...

--- build_erau_hpc.sh
+++ build_erau_hpc.sh
@@ -8,11 +8,18 @@ set -e
 
 cmake --version
 
+module load gcc/6.3.0
 module load blas/gcc/64/3.7.0
 module load lapack/gcc/64/3.7.0
-module load openmpi/gcc/64/1.10.3
+#module load openmpi/gcc/64/1.10.3
+module load openmpi/gcc/64/3.1.2
+module load scalapack/openmpi/gcc/64/2.0.2
 
-OPTS="-DMUMPS_ROOT=$HOME/zettergmdata/lib/MUMPS_4.10.0 -DSCALAPACK_ROOT=$HOME/zettergmdata/lib/scalapack-2.0.2 $OPTS"
+OPTS="-DMUMPS_ROOT=$HOME/zettergmdata/lib/MUMPS_4.10.0 $OPTS"
+OPTS="-DNP=4 $OPTS"
+
+export FC=/cm/shared/apps/openmpi/gcc/64/3.1.2/bin/mpifort
+export CC=/cm/shared/apps/openmpi/gcc/64/3.1.2/bin/mpicc
 
 [[ $1 == "-d" ]] && OPTS="-DCMAKE_BUILD_TYPE=Debug $OPTS"
 [[ $1 == "-t" ]] && OPTS="-DTRACE:BOOL=on $OPTS"
