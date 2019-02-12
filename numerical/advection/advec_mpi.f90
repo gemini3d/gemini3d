3dacb56853fb09fe3bf3302d02324c439c150a37
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-02-12 14:58:10 -0500

    started on the advection changes

--- numerical/advection/advec_mpi.f90
+++ numerical/advection/advec_mpi.f90
@@ -12,7 +12,7 @@ private
 !> OVERLOAD ADVECTION TO DEAL WITH THE CURVILINEAR GRID/MESH STRUCTURE.
 !> NOTE THAT THE LOWER-LEVEL CALLS ARE DISTINCT, NOT-OVERLOADED PROCEDURES.
 interface advec3D_MC_mpi
-  module procedure advec3D_MC_mpi_curv_3
+  module procedure advec3D_MC_mpi_curv_23
 end interface advec3D_MC_mpi
 
 interface advec_prep_mpi
@@ -186,6 +186,169 @@ v3i(:,:,1:lx3+1)=0.5d0*(vs3(1:lx1,1:lx2,0:lx3,isp)+vs3(1:lx1,1:lx2,1:lx3+1,isp))
 end subroutine advec_prep_mpi_3
 
 
+subroutine advec_prep_mpi_23(isp,flagperiodic,ns,rhovs1,vs1,vs2,vs3,rhoes,v1i,v2i,v3i)
+!! COMPUTE INTERFACE VELOCITIES AND LOAD UP GHOST CELLS
+!! FOR FLUID STATE VARIABLES
+!!
+!! Note that it is done on a per species basis
+!! 5/23/2015 - may need to be changed for 2D/1D sims which
+!! have only one element in the x2 direction...
+
+integer, intent(in) :: isp
+integer, intent(in) :: flagperiodic
+real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: ns,rhovs1,vs1,vs2,vs3,rhoes
+
+real(wp), dimension(1:size(vs1,1)-3,1:size(vs1,2)-4,1:size(vs1,3)-4), intent(out) :: v1i
+real(wp), dimension(1:size(vs1,1)-4,1:size(vs1,2)-3,1:size(vs1,3)-4), intent(out) :: v2i
+real(wp), dimension(1:size(vs1,1)-4,1:size(vs1,2)-4,1:size(vs1,3)-3), intent(out) :: v3i
+
+!    real(wp), parameter :: vellim=2000.0
+!    real(wp), parameter :: vellim=0.0
+real(wp) :: coeff
+integer :: ix2,ix3,lx1,lx2,lx3
+
+integer :: idleft,idright
+real(wp), dimension(-1:size(vs3,1)-2,-1:size(vs3,2)-2,-1:size(vs3,3)-2) :: param,param2,param3,param4
+
+real(wp) :: tstart,tfin
+
+lx1=size(vs1,1)-4
+lx2=size(vs1,2)-4
+lx3=size(vs1,3)-4
+
+
+!COMPUTE INTERFACE VELCOTIES AND APPLY LIMITING, IF NEEDED
+v1i(2:lx1,:,:)=0.5*(vs1(1:lx1-1,1:lx2,1:lx3,isp)+vs1(2:lx1,1:lx2,1:lx3,isp))   !first the interior points
+
+if (gridflag==0) then
+  v1i(1,:,:)=vs1(1,1:lx2,1:lx3,isp)   !lowest alt on grid.
+  v1i(lx1+1,:,:)=vs1(lx1,1:lx2,1:lx3,isp)   !lowest alt on grid.
+else if (gridflag==1) then
+  v1i(lx1+1,:,:)=vs1(lx1,1:lx2,1:lx3,isp)   !lowest alt on grid.
+  v1i(1,:,:) = min(v1i(2,1:lx2,1:lx3), 0._wp)    !highest alt; interesting that this is not vs1...   
+else
+  v1i(1,:,:) = vs1(1,1:lx2,1:lx3,isp)
+!!    v1i(lx1+1,:,:)=v1i(lx1,:,:)    !avoids issues with top boundary velocity spikes which may arise
+  v1i(lx1+1,:,:) = max(v1i(lx1,1:lx2,1:lx3),0._wp)    !interesting that this is not vs1...
+end if  
+
+v2i(:,1,:)=vs2(1:lx1,1,1:lx3,isp)
+v2i(:,2:lx2,:)=0.5*(vs2(1:lx1,1:lx2-1,1:lx3,isp)+vs2(1:lx1,2:lx2,1:lx3,isp))
+v2i(:,lx2+1,:)=vs2(1:lx1,lx2,1:lx3,isp)
+
+
+! THIS TYPE OF LIMITING MAY BE NEEDED FOR VERY HIGH-ALTITUDE SIMULATIONS...
+!    if (isp<lsp-1) then
+!      v1i(lx1+1,:,:)=max(vs1(lx1+1,:,:,isp),-1*vellim)    !limit inflow
+!    else if (isp==6) then
+!      v1i(lx1+1,:,:)=min(vs1(lx1+1,:,:,isp),10.0*vellim)      !limit outflow
+!      v1i(lx1+1,:,:)=max(v1i(lx1+1,:,:),0.0)                 !limit inflow
+!!        vadvx1(1,:,6)=mean(vadvx1(1,:,6));                          !for eq sims
+!    else
+!      v1i(lx1+1,:,:)=2.0*v1i(lx1,:,:)-v1i(lx1-1,:,:);      !cleans up large current situations
+!    end if
+
+
+!GHOST CELL VALUES FOR DENSITY (these need to be done last to avoid being overwritten by send/recv's!!!)
+ns(:,0,:,isp)=ns(:,1,:,isp)
+ns(:,-1,:,isp)=ns(:,1,:,isp) 
+ns(:,lx2+1,:,isp)=ns(:,lx2,:,isp)
+ns(:,lx2+2,:,isp)=ns(:,lx2,:,isp)
+
+do ix3=1,lx3
+  do ix2=1,lx2
+    !logical bottom
+    coeff=ns(2,ix2,ix3,isp)/ns(3,ix2,ix3,isp)
+    ns(0,ix2,ix3,isp)=min(coeff*ns(1,ix2,ix3,isp),ns(1,ix2,ix3,isp))
+    ns(-1,ix2,ix3,isp)=min(coeff*ns(0,ix2,ix3,isp),ns(0,ix2,ix3,isp))
+
+    !logical top
+    coeff=ns(lx1-1,ix2,ix3,isp)/ns(lx1-2,ix2,ix3,isp)
+    ns(lx1+1,ix2,ix3,isp)=min(coeff*ns(lx1,ix2,ix3,isp),ns(lx1,ix2,ix3,isp))
+    ns(lx1+2,ix2,ix3,isp)=min(coeff*ns(lx1+1,ix2,ix3,isp),ns(lx1+1,ix2,ix3,isp))
+  end do
+end do
+
+
+!FOR X1 MOMENTUM DENSITY
+rhovs1(:,0,:,isp)=rhovs1(:,1,:,isp);
+rhovs1(:,-1,:,isp)=rhovs1(:,1,:,isp);
+rhovs1(:,lx2+1,:,isp)=rhovs1(:,lx2,:,isp);
+rhovs1(:,lx2+2,:,isp)=rhovs1(:,lx2,:,isp);
+
+rhovs1(0,1:lx2,1:lx3,isp)=2d0*v1i(1,:,:)-vs1(1,1:lx2,1:lx3,isp);  !initially these are velocities.  Also loose definition of 'conformable'.  Also corners never get set, but I suppose they aren't really used anyway.
+rhovs1(-1,:,:,isp)=rhovs1(0,:,:,isp)+rhovs1(0,:,:,isp)-vs1(1,:,:,isp);
+rhovs1(lx1+1,1:lx2,1:lx3,isp)=2d0*v1i(lx1+1,:,:)-vs1(lx1,1:lx2,1:lx3,isp);
+rhovs1(lx1+2,:,:,isp)=rhovs1(lx1+1,:,:,isp)+rhovs1(lx1+1,:,:,isp)-vs1(lx1,:,:,isp);
+
+rhovs1(-1:0,:,:,isp)=rhovs1(-1:0,:,:,isp)*ns(-1:0,:,:,isp)*ms(isp)   !now convert to momentum density
+rhovs1(lx1+1:lx1+2,:,:,isp)=rhovs1(lx1+1:lx1+2,:,:,isp)*ns(lx1+1:lx1+2,:,:,isp)*ms(isp)
+
+
+!> FOR INTERNAL ENERGY
+rhoes(:,0,:,isp)=rhoes(:,1,:,isp);
+rhoes(:,-1,:,isp)=rhoes(:,1,:,isp);
+rhoes(:,lx2+1,:,isp)=rhoes(:,lx2,:,isp);
+rhoes(:,lx2+2,:,isp)=rhoes(:,lx2,:,isp);
+
+rhoes(0,:,:,isp)=rhoes(1,:,:,isp);
+rhoes(-1,:,:,isp)=rhoes(1,:,:,isp);
+rhoes(lx1+1,:,:,isp)=rhoes(lx1,:,:,isp);
+rhoes(lx1+2,:,:,isp)=rhoes(lx1,:,:,isp);
+
+
+!> NOW DEAL WITH ADVECTION ALONG X3; FIRST IDENTIFY MY NEIGHBORS
+idleft=myid-1; idright=myid+1
+
+
+!> PASS X3 BOUNDARY CONDITIONS WITH GENERIC HALOING ROUTINES
+param=vs3(:,:,:,isp)
+call halo(param,1,tagvs3BC)     !! we only need one ghost cell to compute interface velocities
+vs3(:,:,:,isp)=param
+
+!these will now be haloed internal ot the advection routines, viz. all advected quantities are haloed withine advection
+!param2=ns(:,:,:,isp)
+!call halo(param2,2,tagnsBC)
+!ns(:,:,:,isp)=param2
+!
+!param3=rhovs1(:,:,:,isp)
+!call halo(param3,2,tagrhovs1BC)
+!rhovs1(:,:,:,isp)=param3
+!
+!param4=rhoes(:,:,:,isp)
+!call halo(param4,2,tagrhoesBC)
+!rhoes(:,:,:,isp)=param4 
+
+if (flagperiodic==0) then
+  if (idleft==-1) then    !left side is at global boundary, assume haloing won't overwrite
+    vs3(:,:,0,isp)=vs3(:,:,1,isp)    !copy first cell to first ghost (vs3 not advected so only need only ghost)
+
+    ns(:,:,0,isp)=ns(:,:,1,isp)
+    ns(:,:,-1,isp)=ns(:,:,1,isp)
+    rhovs1(:,:,0,isp)=rhovs1(:,:,1,isp);
+    rhovs1(:,:,-1,isp)=rhovs1(:,:,1,isp);
+    rhoes(:,:,0,isp)=rhoes(:,:,1,isp);
+    rhoes(:,:,-1,isp)=rhoes(:,:,1,isp);
+  end if
+  if (idright==lid) then    !my right boundary is the global boundary, assume haloing won't overwrite
+    vs3(:,:,lx3+1,isp)=vs3(:,:,lx3,isp)    !copy last cell to first ghost (all that's needed since vs3 not advected)
+
+    ns(:,:,lx3+1,isp)=ns(:,:,lx3,isp)
+    ns(:,:,lx3+2,isp)=ns(:,:,lx3,isp)
+    rhovs1(:,:,lx3+1,isp)=rhovs1(:,:,lx3,isp);
+    rhovs1(:,:,lx3+2,isp)=rhovs1(:,:,lx3,isp);
+    rhoes(:,:,lx3+1,isp)=rhoes(:,:,lx3,isp);
+    rhoes(:,:,lx3+2,isp)=rhoes(:,:,lx3,isp);
+  end if
+end if
+
+
+!> AFTER HALOING CAN COMPUTE THE X3 INTERFACE VELOCITIES NORMALLY
+v3i(:,:,1:lx3+1)=0.5d0*(vs3(1:lx1,1:lx2,0:lx3,isp)+vs3(1:lx1,1:lx2,1:lx3+1,isp))
+
+end subroutine advec_prep_mpi_23
+
+
 function advec3D_MC_mpi_curv_3(f,v1i,v2i,v3i,dt,x,frank)
 
 !------------------------------------------------------------
@@ -349,10 +512,15 @@ lx3=size(f,3)-4
 !to avoid tearing artifacts...  In the future this should be removed in favor of explicit haloing withint
 !this procedure, which is already required for x2 message passsing.
 
+!we must recopying the boundary conditions after any halo operation (which assumes periodic)
+advec3D_MC_mpi_curv_23=f
+!call halo(advec3D_MC_mpi_curv_23,2,taggenericparam)
+
+
 !x3-sweep
 do ix2=1,lx2
   do ix1=1,lx1
-    fx3slice=f(ix1,ix2,:)
+    fx3slice=advec3D_MC_mpi_curv_23(ix1,ix2,:)
     v3slice=v3i(ix1,ix2,:)
     if (frank==0) then     !advecting a scalar quantity
       h31x3slice=x%h1(ix1,ix2,:)*x%h2(ix1,ix2,:)*x%h3(ix1,ix2,:)
@@ -367,12 +535,6 @@ do ix2=1,lx2
   end do
 end do
 
-
-!copy x1,x2 boundary conditions to partially updated variable for next sweeps
-advec3D_MC_mpi_curv_23(:,-1:0,:)=f(:,-1:0,:);   
-advec3D_MC_mpi_curv_23(:,lx2+1:lx2+2,:)=f(:,lx2+1:lx2+2,:);
-advec3D_MC_mpi_curv_23(-1:0,:,:)=f(-1:0,:,:);           
-advec3D_MC_mpi_curv_23(lx1+1:lx1+2,:,:)=f(lx1+1:lx1+2,:,:);
 
 !x1-sweep
 do ix3=1,lx3
