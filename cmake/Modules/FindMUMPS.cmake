82793af3e732f2905c22fe540a82e21a7786a5a7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-04-19 00:46:59 -0400

    findmumps template

--- cmake/Modules/FindMUMPS.cmake
+++ cmake/Modules/FindMUMPS.cmake
@@ -5,18 +5,11 @@
 FindMUMPS
 ---------
 
-Finds the MUMPS library
+Finds the MUMPS library.
+Note that MUMPS generally requires SCALAPACK and LAPACK as well.
 
 COMPONENTS
   s d c z   list one or more. Defaults to ``d``.
-
-Imported targets
-^^^^^^^^^^^^^^^^
-
-This module defines the following :prop_tgt:`IMPORTED` targets:
-
-MUMPS::MUMPS
-  the Mumps library components requested.
 
 Result Variables
 ^^^^^^^^^^^^^^^^
@@ -29,25 +22,24 @@ MUMPS_INCLUDE_DIRS
 
 #]=======================================================================]
 
-set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME d)
+
+if(NOT MUMPS_FIND_COMPONENTS)
+  set(MUMPS_FIND_COMPONENTS d)
+endif()
 
 find_path(MUMPS_INCLUDE_DIR
-          NAMES mumps_compat.h
-          PATH_SUFFIXES MUMPS include)
+          NAMES mumps_compat.h)
 
 find_library(MUMPS_COMMON
-             NAMES mumps_common
-             PATH_SUFFIXES MUMPS lib)
+             NAMES mumps_common)
 
 find_library(PORD
-             NAMES pord
-             PATH_SUFFIXES MUMPS lib)
+             NAMES pord)
 
 
 FOREACH(comp ${MUMPS_FIND_COMPONENTS})
   find_library(MUMPS_${comp}_lib
-              NAMES ${comp}mumps
-              PATH_SUFFIXES MUMPS lib)
+              NAMES ${comp}mumps)
 
   list(APPEND MUMPS_LIBRARY ${MUMPS_${comp}_lib})
   mark_as_advanced(MUMPS_${comp}_lib)
@@ -56,19 +48,12 @@ ENDFOREACH()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MUMPS
-    REQUIRED_VARS MUMPS_LIBRARY MUMPS_COMMON PORD MUMPS_INCLUDE_DIR)
+  REQUIRED_VARS MUMPS_LIBRARY MUMPS_COMMON PORD MUMPS_INCLUDE_DIR)
 
 # in this order!
-list(APPEND MUMPS_LIBRARIES ${MUMPS_LIBRARY} ${MUMPS_COMMON} ${PORD})
-set(MUMPS_INCLUDE_DIRS ${MUMPS_INCLUDE_DIR})
-
 if(MUMPS_FOUND)
-  if(NOT TARGET MUMPS::MUMPS)
-    add_library(MUMPS::MUMPS UNKNOWN IMPORTED)
-    set_target_properties(MUMPS::MUMPS PROPERTIES
-                        IMPORTED_LOCATION ${MUMPS_LIBRARY}
-                        INTERFACE_INCLUDE_DIRECTORIES ${MUMPS_INCLUDE_DIRS})
-  endif()
+  set(MUMPS_LIBRARIES ${MUMPS_LIBRARY} ${MUMPS_COMMON} ${PORD})
+  set(MUMPS_INCLUDE_DIRS ${MUMPS_INCLUDE_DIR})
 endif()
 
 mark_as_advanced(
