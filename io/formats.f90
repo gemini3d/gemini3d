1bd4ae10b1a7e524d3d6a791be031e17740645e6
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-05-06 22:06:19 -0400

    strict filename formatting

--- io/formats.f90
+++ io/formats.f90
@@ -27,16 +27,20 @@ integer, intent(in) :: ymd(3)
 class(*), intent(in) :: UTsec
 !! UTC second: real [0.0 .. 86400.0)
 
+!! the rather verbose handling below is to ensure proper formatting across all compilers and OS.
+
 character(12) :: sec_str
 
 select type(UTsec)
   type is (real(real32))
-    write(sec_str, '(I5.5, F7.6)') int(UTsec), UTsec-int(UTsec)
+    if (UTsec < 0 .or. UTsec >= 86400) error stop '0 <= utsec < 86400'
+    write(sec_str, '(I5.5, A1, I6.6)') int(UTsec), '.', int((UTsec-int(UTsec))*1000000)
   type is (real(real64))
-    write(sec_str, '(I5.5, F7.6)') int(UTsec), UTsec-int(UTsec)
+    if (UTsec < 0 .or. UTsec >= 86400) error stop '0 <= utsec < 86400'
+    write(sec_str, '(I5.5, A1, I6.6)') int(UTsec), '.', int((UTsec-int(UTsec))*1000000)
 end select
 
-write(fn,'(i4,2I2.2,a17)') ymd, '_' // sec_str // '.dat'
+write(fn,'(i4,2I2.2,a17)') ymd(1), ymd(2:), '_' // sec_str // '.dat'
 
 end function utsec2filename
 
--- tests/test_formats.f90
+++ tests/test_formats.f90
@@ -1,16 +1,28 @@
 !! This tests various text output formats.
 !! It necessarily requires visual inspection, or run from an enclosing program with Regex,
 !! such as a Python program configured to test as desired.
-use, intrinsic:: iso_fortran_env, only: sp=>real32, dp=>real64
+use, intrinsic:: iso_fortran_env, only: sp=>real32, dp=>real64, stderr=>error_unit
 use formats, only: utsec2filename
 
 implicit none
 
-print *, 'Single precision lacks adequate precision for dates beyond millisecond'
-print *, utsec2filename([2015,4,13], 12345.000003_sp)
+character(:), allocatable :: fn
 
-if (utsec2filename([2015,4,13], 12345.678910_dp) /= '20150413_12345.678910.dat') error stop 'mismatch real64'
+! print *, 'Single precision lacks adequate precision for dates beyond millisecond'
+! print *, utsec2filename([2015,4,13], 12345.000003_sp)
 
-if (utsec2filename([2015,4,13],  345.678910_dp) /= '20150413_00345.678910.dat') error stop 'mismatch leading zeros'
+fn = utsec2filename([2015,4,13], 12345.678910_dp)
+
+if (fn /= '20150413_12345.678910.dat') then
+  write(stderr,*) 'wrong output: '//fn
+  error stop 'mismatch real64'
+endif
+
+
+fn = utsec2filename([2015,4,13],  345.678911_dp)
+if (fn /= '20150413_00345.678911.dat') then
+  write(stderr,*) 'wrong output: '//fn
+  error stop 'mismatch leading zeros'
+endif
 
 end program
