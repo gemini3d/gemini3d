9f532c8abef3691a3548a18e0faacb5ad2627ca3
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-05-12 03:05:22 -0400

    add more timeinc test

--- io/test_formats.f90
+++ io/test_formats.f90
@@ -3,10 +3,13 @@
 !! such as a Python program configured to test as desired.
 use, intrinsic:: iso_fortran_env, only: sp=>real32, dp=>real64, stderr=>error_unit
 use formats, only: utsec2filename
+use timeutils, only: dateinc
 
 implicit none
 
 character(:), allocatable :: fn
+real(dp) :: UTsec, dtinc
+integer :: ymd(3), i
 
 ! print *, 'Single precision lacks adequate precision for dates beyond millisecond'
 ! print *, utsec2filename([2015,4,13], 12345.000003_sp)
@@ -33,5 +36,22 @@ if (fn /= '20130220_00060.000000.dat') then
   write(stderr,*) 'wrong output: '//fn
   error stop 'mismatch leading zeros'
 endif
+
+!! Increment time tests
+ymd = [2013,2,20]
+UTsec = 0._dp
+call dateinc(1e-6_dp, ymd, UTsec)
+
+do i = 1,60
+  call dateinc(1._dp, ymd, UTsec)
+enddo
+
+fn = utsec2filename(ymd, UTsec)
+
+if (fn /= '20130220_00060.000000.dat') then
+  write(stderr,*) 'wrong output: '//fn
+  error stop 'mismatch leading zeros'
+endif
+
 
 end program
