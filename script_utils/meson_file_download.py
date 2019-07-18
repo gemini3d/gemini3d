1dd5667003c65038453f8499149ea0b7acd4a02a
Michael Hirsch, Ph.D <10931741+scivision@users.noreply.github.com>
2019-07-18 12:28:20 -0400

    Implement Azure Pipelines CI for Meson and CMake (#2)
    * log artifact [skip appveyor] [skip travis]
    
    * Always artifact build log meson [skip travis] [skip appveyor]
    
    * meson download and extract [skip appveyor] [skip travis]
    
    * make ../simulations if not exist [skip travis] [skip appveyor]
    
    * explict ninja build [skip travis] [skip appveyor]
    
    * debug azure paths [skip appveyor] [skip travis]
    
    * setup python prereq
    
    * more debug meson [skip travis] [skip appveyor]
    
    * meson debug extract azure [skip travis] [skip appveyor]
    
    * correct typo
    
    * use urllib.request instead of requests [skip travis] [skip appveyor]
    
    * unit tests first [skip appveyor] [skip travis]
    
    * stdlib only [skip travis] [skip appveyor]
    
    * allow compile if zenodo test data not available, with warning [skip appveyor] [skip travis]
    
    * doc [skip ci]

---
+++ script_utils/meson_file_download.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from pathlib import Path
+import urllib.request
+import hashlib
+import argparse
+
+
+def url_retrieve(url: str, outfile: Path, md5sum: str = None, overwrite: bool = False):
+    outfile = Path(outfile).expanduser().resolve()
+    # need .resolve() in case intermediate relative dir doesn't exist
+    if outfile.is_file() and not overwrite:
+        return
+    outfile.parent.mkdir(parents=True, exist_ok=True)
+
+    urllib.request.urlretrieve(url, str(outfile))
+
+    if md5sum:
+        if not file_checksum(outfile, md5sum, 'md5'):
+            raise OSError('hash mismatch: Failed to download {}'.format(outfile))
+
+
+def file_checksum(fn: Path, hash: str, mode: str) -> bool:
+    h = hashlib.new(mode)
+    h.update(fn.read_bytes())
+    return h.hexdigest() == hash
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('url', help='URL to file download')
+    p.add_argument('outfile', help='filename to download to')
+    p.add_argument('md5sum', help='expected MD5 hash', nargs='?')
+    P = p.parse_args()
+
+    url_retrieve(P.url, P.outfile, P.md5sum)
---
+++ script_utils/meson_file_extract.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from pathlib import Path
+import argparse
+import zipfile
+
+
+def extract_files(zipfn: Path, outpath: Path, overwrite: bool = False):
+    outpath = Path(outpath).expanduser().resolve()
+    # need .resolve() in case intermediate relative dir doesn't exist
+    if outpath.is_dir() and not overwrite:
+        return
+    zipfn = Path(zipfn).expanduser().resolve()
+    with zipfile.ZipFile(zipfn) as z:
+        z.extractall(str(outpath.parent))
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('zipfile', help='.zip file to extract')
+    p.add_argument('outpath', help='path to extract into')
+    P = p.parse_args()
+
+    extract_files(P.zipfile, P.outpath)
