7d9f18f822c7e7406e204e6b242fed01921ccfc9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-08-09 13:48:03 -0400

    cmake: correct CI-detection logic
    meson: correct CI-detection logic

---
+++ script_utils/meson_get_environment_variable.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Meson can't get environment variables into meson.build,
+so we do it with this helper script.
+
+Only one environment variable may be read at a time.
+
+returns nothing '' if variable doesn't exist
+"""
+import os
+import sys
+
+var = os.environ.get(sys.argv[1], None)
+if var:
+    print(var, end="")
