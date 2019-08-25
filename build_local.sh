d27c97e958d67f943630b8bd3c19cd52d432b15a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-08-25 02:15:37 -0400

    need readlink to resolve relative path

--- build_local.sh
+++ build_local.sh
@@ -10,6 +10,8 @@ B=build
 mpidir=$R/lib_gcc/openmpi-3.1.4/bin
 
 #=======================================
+mpidir=$(readlink -f $mpidir)
+
 export FC=$mpidir/mpif90
 export CC=$mpidir/mpicc
 export MPIFC=$FC
