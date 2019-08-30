97c4453533106139c1047fbcf2a36bfa04fda4ff
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-08-30 13:09:45 -0400

    matlab: grid writing allow read32/64

--- script_utils/writedata.m
+++ script_utils/writedata.m
@@ -1,4 +1,4 @@
-function writedata(dmy,time,ns,vsx1,Ts,outdir,outID)
+function writedata(dmy,time,ns,vsx1,Ts,outdir,outID, realbits)
 
 %--------------------------------------------------------
 %-----WRITE STATE VARIABLE DATA TO BE USED AS INITIAL
@@ -10,7 +10,7 @@ function writedata(dmy,time,ns,vsx1,Ts,outdir,outID)
 %-----INPUT ARRAYS SHOULD BE TRIMMED TO THE CORRECT SIZE
 %-----(I.E. THEY SHOULD NOT INCLUDE GHOST CELLS
 %--------------------------------------------------------
-narginchk(7,7)
+narginchk(7,8)
 validateattributes(dmy, {'numeric'}, {'vector', 'positive', 'numel', 3}, mfilename, 'day, month, year', 1)
 validateattributes(time, {'numeric'}, {'scalar', 'nonnegative'}, mfilename, 'time', 2)
 validateattributes(ns, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'density', 3)
@@ -19,25 +19,36 @@ validateattributes(Ts, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'temp
 validateattributes(outdir, {'char'}, {'vector'}, mfilename, 'output directory',6)
 validateattributes(outID, {'char'}, {'vector'}, mfilename, 'output filestem',7)
 
-  %% CHECK THAT THE DIRECTORY EXISTS - IF NOT CREATE IT AND GIVE THE USER AN INDICATION OF WHAT WAS DONE
-  if (~(exist(outdir,'dir')==7))
-    mkdir(outdir);
-    disp(['Created: ', outdir])
-  end
+if nargin < 8
+  realbits = 64;
+end
+validateattr(realbits, {'numeric'}, {'scalar', 'integer', 'positive'}, mfilename,'real bits',8)
+
+switch realbits
+  case 64, freal = 'float64';
+  case 32, freal = 'float32';
+  otherwise, error(['unknown precision', num2str(realbits)])
+end
+
+%% CHECK THAT THE DIRECTORY EXISTS - IF NOT CREATE IT AND GIVE THE USER AN INDICATION OF WHAT WAS DONE
+if exist(outdir,'dir')~=7
+  mkdir(outdir);
+  disp(['Created: ', outdir])
+end
 
 
-  %% OPEN THE FILE
-  filename=[outdir,filesep,outID,'_ICs.dat'];
-  fid=fopen(filename,'w');
+%% OPEN THE FILE
+filename=[outdir,filesep,outID,'_ICs.dat'];
+fid=fopen(filename,'w');
 
 
-  %% WRITE THE DATA
-  fwrite(fid,dmy,'real*8');
-  fwrite(fid,time,'real*8');
-  fwrite(fid,ns,'real*8');
-  fwrite(fid,vsx1,'real*8');
-  fwrite(fid,Ts,'real*8');
+%% WRITE THE DATA
+fwrite(fid,dmy, freal);
+fwrite(fid,time, freal);
+fwrite(fid,ns, freal);
+fwrite(fid,vsx1, freal);
+fwrite(fid,Ts, freal);
 
-  fclose(fid);
+fclose(fid);
 
 end
--- script_utils/writegrid.m
+++ script_utils/writegrid.m
@@ -1,91 +1,102 @@
-function writegrid(xg, outdir)
+function writegrid(xg, outdir, realbits)
 
 %--------------------------------------------------------
 %-----NOTE THAT WE DO STORE *ALL*
 %-----GRID DATA (INCLUDING STUFF NOT NEEDED BY
 %-----FORTRAN CODE BUT POSSIBLY USEFUL FOR PLOTTING)
 %--------------------------------------------------------
-narginchk(2,2)
+narginchk(2,3)
 validateattributes(xg, {'struct'}, {'scalar'}, mfilename, 'grid parameters', 1)
 validateattributes(outdir, {'char'}, {'vector'}, mfilename,'output directory',2)
 
-  %% MAKE THE OUTPUT DIRECTORY IF IT DOESN'T EXIST AND NOTIFY USER
-  if (~(exist(outdir,'dir')==7))
-    mkdir(outdir);
-    disp(['Created: ', outdir])
-  end
+if nargin < 3
+  realbits = 64;
+end
+validateattr(realbits, {'numeric'}, {'scalar', 'integer', 'positive'}, mfilename,'real bits',3)
+
+switch realbits
+  case 64, freal = 'float64';
+  case 32, freal = 'float32';
+  otherwise, error(['unknown precision', num2str(realbits)])
+end
+
+%% MAKE THE OUTPUT DIRECTORY IF IT DOESN'T EXIST AND NOTIFY USER
+if exist(outdir, 'dir') ~= 7
+  mkdir(outdir);
+  disp(['Created: ', outdir])
+end
 
 
-  filename=[outdir,filesep,'simsize.dat'];
-  fid=fopen(filename,'w');
-  fwrite(fid,xg.lx,'integer*4');
-  fclose(fid);
+filename=[outdir,filesep,'simsize.dat'];
+fid=fopen(filename,'w');
+fwrite(fid, xg.lx, 'integer*4');
+fclose(fid);
 
-  fid=fopen([outdir,filesep,'simgrid.dat'],'w');
+fid=fopen([outdir,filesep,'simgrid.dat'],'w');
 
-  fwrite(fid,xg.x1,'real*8');    %coordinate values
-  fwrite(fid,xg.x1i,'real*8');
-  fwrite(fid,xg.dx1b,'real*8');
-  fwrite(fid,xg.dx1h,'real*8');
+fwrite(fid,xg.x1, freal);    %coordinate values
+fwrite(fid,xg.x1i, freal);
+fwrite(fid,xg.dx1b, freal);
+fwrite(fid,xg.dx1h, freal);
 
-  fwrite(fid,xg.x2,'real*8');
-  fwrite(fid,xg.x2i,'real*8');
-  fwrite(fid,xg.dx2b,'real*8');
-  fwrite(fid,xg.dx2h,'real*8');
+fwrite(fid,xg.x2, freal);
+fwrite(fid,xg.x2i, freal);
+fwrite(fid,xg.dx2b, freal);
+fwrite(fid,xg.dx2h, freal);
 
-  fwrite(fid,xg.x3,'real*8');
-  fwrite(fid,xg.x3i,'real*8');
-  fwrite(fid,xg.dx3b,'real*8');
-  fwrite(fid,xg.dx3h,'real*8');
+fwrite(fid,xg.x3, freal);
+fwrite(fid,xg.x3i, freal);
+fwrite(fid,xg.dx3b, freal);
+fwrite(fid,xg.dx3h, freal);
 
-  fwrite(fid,xg.h1,'real*8');   %cell-centered metric coefficients
-  fwrite(fid,xg.h2,'real*8');
-  fwrite(fid,xg.h3,'real*8');
+fwrite(fid,xg.h1, freal);   %cell-centered metric coefficients
+fwrite(fid,xg.h2, freal);
+fwrite(fid,xg.h3, freal);
 
-  fwrite(fid,xg.h1x1i,'real*8');    %interface metric coefficients
-  fwrite(fid,xg.h2x1i,'real*8');
-  fwrite(fid,xg.h3x1i,'real*8');
+fwrite(fid,xg.h1x1i, freal);    %interface metric coefficients
+fwrite(fid,xg.h2x1i, freal);
+fwrite(fid,xg.h3x1i, freal);
 
-  fwrite(fid,xg.h1x2i,'real*8');
-  fwrite(fid,xg.h2x2i,'real*8');
-  fwrite(fid,xg.h3x2i,'real*8');
+fwrite(fid,xg.h1x2i, freal);
+fwrite(fid,xg.h2x2i, freal);
+fwrite(fid,xg.h3x2i, freal);
 
-  fwrite(fid,xg.h1x3i,'real*8');
-  fwrite(fid,xg.h2x3i,'real*8');
-  fwrite(fid,xg.h3x3i,'real*8');
+fwrite(fid,xg.h1x3i, freal);
+fwrite(fid,xg.h2x3i, freal);
+fwrite(fid,xg.h3x3i, freal);
 
-  %gravity, geographic coordinates, magnetic field strength? unit vectors?
-  fwrite(fid,xg.gx1,'real*8');    %gravitational field components
-  fwrite(fid,xg.gx2,'real*8');
-  fwrite(fid,xg.gx3,'real*8');
+%gravity, geographic coordinates, magnetic field strength? unit vectors?
+fwrite(fid,xg.gx1, freal);    %gravitational field components
+fwrite(fid,xg.gx2, freal);
+fwrite(fid,xg.gx3, freal);
 
-  fwrite(fid,xg.alt,'real*8');    %geographic coordinates
-  fwrite(fid,xg.glat,'real*8');
-  fwrite(fid,xg.glon,'real*8');
+fwrite(fid,xg.alt, freal);    %geographic coordinates
+fwrite(fid,xg.glat, freal);
+fwrite(fid,xg.glon, freal);
 
-  fwrite(fid,xg.Bmag,'real*8');    %magnetic field strength
+fwrite(fid,xg.Bmag, freal);    %magnetic field strength
 
-  fwrite(fid,xg.I,'real*8');    %magnetic field inclination
+fwrite(fid,xg.I, freal);    %magnetic field inclination
 
-  fwrite(fid,xg.nullpts,'real*8');    %points not to be solved
+fwrite(fid,xg.nullpts, freal);    %points not to be solved
 
 
-  %NOT ALL OF THE REMAIN INFO IS USED IN THE FORTRAN CODE, BUT IT INCLUDED FOR COMPLETENESS
-  fwrite(fid,xg.e1,'real*8');   %4D unit vectors (in cartesian components)
-  fwrite(fid,xg.e2,'real*8');
-  fwrite(fid,xg.e3,'real*8');
+%NOT ALL OF THE REMAIN INFO IS USED IN THE FORTRAN CODE, BUT IT INCLUDED FOR COMPLETENESS
+fwrite(fid,xg.e1, freal);   %4D unit vectors (in cartesian components)
+fwrite(fid,xg.e2, freal);
+fwrite(fid,xg.e3, freal);
 
-  fwrite(fid,xg.er,'real*8');    %spherical unit vectors
-  fwrite(fid,xg.etheta,'real*8');
-  fwrite(fid,xg.ephi,'real*8');
+fwrite(fid,xg.er, freal);    %spherical unit vectors
+fwrite(fid,xg.etheta, freal);
+fwrite(fid,xg.ephi, freal);
 
-  fwrite(fid,xg.r,'real*8');    %spherical coordinates
-  fwrite(fid,xg.theta,'real*8');
-  fwrite(fid,xg.phi,'real*8');
+fwrite(fid,xg.r, freal);    %spherical coordinates
+fwrite(fid,xg.theta, freal);
+fwrite(fid,xg.phi, freal);
 
-  fwrite(fid,xg.x,'real*8');     %cartesian coordinates
-  fwrite(fid,xg.y,'real*8');
-  fwrite(fid,xg.z,'real*8');
+fwrite(fid,xg.x, freal);     %cartesian coordinates
+fwrite(fid,xg.y, freal);
+fwrite(fid,xg.z, freal);
 
-  fclose(fid);
+fclose(fid);
 end
