e2f692a298ae87f09491780e5785ec3f304173f9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-10-02 05:30:43 -0400

    plot diff plots in bwr for v* like plotall

--- gemini/plotdiff.py
+++ gemini/plotdiff.py
@@ -15,11 +15,16 @@ def plotdiff(A: np.ndarray, B: np.ndarray, name: str, time: datetime, outdir: Pa
     fg = Figure(tight_layout=True, figsize=(12, 5))
     axs = fg.subplots(1, 2)
 
-    hi = axs[0].pcolormesh(A)
+    if name.startswith("v"):
+        cmap = "bwr"
+    else:
+        cmap = None
+
+    hi = axs[0].pcolormesh(A, cmap=cmap)
     fg.colorbar(hi, ax=axs[0])
     axs[0].set_title(str(outdir))
 
-    hi = axs[1].pcolormesh(B)
+    hi = axs[1].pcolormesh(B, cmap=cmap)
     fg.colorbar(hi, ax=axs[1])
     axs[1].set_title(str(refdir))
 
