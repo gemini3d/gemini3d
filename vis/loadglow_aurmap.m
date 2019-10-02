baa9ac21e1ce3a2e264778127212ea442423b31e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-10-02 08:25:51 -0400

    init

---
+++ vis/loadglow_aurmap.m
@@ -0,0 +1,13 @@
+function cAur = loadglow_aurmap(filename, lx2, lx3, lwave)
+narginchk(4,4)
+assert(is_file(filename), [filename, " is not a filename"])
+validateattributes(lx2, {'numeric'}, {'scalar', 'integer', 'positive'})
+validateattributes(lx3, {'numeric'}, {'scalar', 'integer', 'positive'})
+validateattributes(lwave, {'numeric'}, {'scalar', 'integer', 'positive'})
+
+fid=fopen(filename,'r');
+cAur = fread(fid,lx2*lx3*lwave,'real*8');
+cAur = reshape(cAur,[lx2,lx3,lwave]);
+fclose(fid);
+
+end
