5944dfe3be5cfd664af8ebd0cf05d048d314cfd9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-10-15 23:41:35 -0400

    meson: move complicated test logic inc. downloads to Python
    this avoids downloading test data for tests not used (good for CI)
    
    meson: remove unused options

--- script_utils/meson_cpu_count.py
+++ script_utils/meson_cpu_count.py
@@ -12,21 +12,9 @@ except ImportError:
     # pip install psutil will improve CPU utilization.
 
 
-def get_simsize(fn: Path) -> tuple:
-    fn = Path(fn).expanduser()
-    if fn.stat().st_size != 12:
-        raise ValueError(f"{fn} is not expected 12 bytes long")
-    return struct.unpack("III", fn.open("rb").read(12))
-
-
-if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument("fn", help="simsize.dat to read")
-    p.add_argument("-f", "--force", help="force CPU count", type=int)
-    P = p.parse_args()
-
-    if P.force:
-        max_cpu = P.force
+def get_mpi_count(fn: Path, force: int = None) -> int:
+    if force:
+        max_cpu = force
         extradiv = 1
     else:
         max_cpu = None
@@ -41,7 +29,7 @@ if __name__ == "__main__":
             max_cpu = os.cpu_count()
             extradiv = 2
 
-    size = get_simsize(P.fn)
+    size = get_simsize(fn)
     # need at least 2 images for MPI to function for Gemini
     mpi_count = 2
     if size[2] == 1:  # 2D sim
@@ -57,7 +45,23 @@ if __name__ == "__main__":
 
     mpi_count //= extradiv
 
-    mpi_count = max(mpi_count, 2)
+    return max(mpi_count, 2)
+
+
+def get_simsize(fn: Path) -> tuple:
+    fn = Path(fn).expanduser()
+    if fn.stat().st_size != 12:
+        raise ValueError(f"{fn} is not expected 12 bytes long")
+    return struct.unpack("III", fn.open("rb").read(12))
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("fn", help="simsize.dat to read")
+    p.add_argument("-f", "--force", help="force CPU count", type=int)
+    P = p.parse_args()
+
+    mpi_count = get_mpi_count(P.fn, P.force)
 
     # need end='' or you'll have to .strip() in Meson
     print(mpi_count, end="")
---
+++ script_utils/meson_run_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+run test
+"""
+import argparse
+import typing
+import subprocess
+from pathlib import Path
+
+from meson_cpu_count import get_mpi_count
+from meson_file_download import url_retrieve
+from meson_file_extract import extract_zip
+
+R = Path(__file__).resolve().parents[1]
+
+zenodo: typing.Dict[str, typing.Any] = {
+  '2d_fang': {
+    'url': 'https://zenodo.org/record/3477385/files/zenodo2d_fang.zip?download=1',
+    'md5': '57d72fd0005247c8eedf122ac4670ad0',
+    'dir': R / 'tests/data/zenodo2d_fang',
+    'zip': R / 'tests/data/zenodo2d_fang.zip'},
+  '2d_glow': {
+    'url': 'https://zenodo.org/record/3477385/files/zenodo2d_glow.zip?download=1',
+    'md5': '557bc6a91d8bf3464abdc5c8784f3042',
+    'dir': R / 'tests/data/zenodo2d_glow',
+    'zip': R / 'tests/data/zenodo2d_glow.zip'},
+  '3d_fang': {
+    'url': 'https://zenodo.org/record/3477330/files/zenodo3d_fang.zip?download=1',
+    'md5': 'cf73d6eb166369c522da7a371492a1ce',
+    'dir': R / 'tests/data/zenodo3d_fang',
+    'zip': R / 'tests/data/zenodo3d_fang.zip'},
+  '3d_glow': {
+     'url': 'https://zenodo.org/record/3477330/files/zenodo3d_glow.zip?download=1',
+     'md5': '3528946525295cc8271aa41bc262d7f1',
+     'dir': R / 'tests/data/zenodo3d_glow',
+     'zip': R / 'tests/data/zenodo3d_glow.zip'},
+  }
+
+
+def run_test(testname: str, mpiexec: str, exe: str, nml: str, outdir: str):
+    z = zenodo[testname]
+    url_retrieve(z['url'], z['zip'], ('md5', z['md5']))
+    extract_zip(z['zip'], z['dir'])
+
+    mpi_count = get_mpi_count(z['dir'] / 'inputs/simsize.dat')
+
+    # have to get exe as absolute path
+    exe_abs = Path(exe).resolve()
+    subprocess.check_call([mpiexec, '-np', str(mpi_count), str(exe_abs), nml, outdir], cwd=R)
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('testname')
+    p.add_argument('mpiexec')
+    p.add_argument('exe')
+    p.add_argument('nml')
+    p.add_argument('outdir')
+    P = p.parse_args()
+
+    run_test(P.testname, P.mpiexec, P.exe, P.nml, P.outdir)
