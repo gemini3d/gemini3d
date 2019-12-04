e82f0f17020899a25b6fe69cf21ed1d62adb5e98
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-04 16:17:50 -0500

    cmake: glow use master head

--- cmake/glow.cmake
+++ cmake/glow.cmake
@@ -2,7 +2,7 @@ include(ExternalProject)
 
 ExternalProject_Add(GLOW_proj
   GIT_REPOSITORY https://github.com/gemini3d/glow.git
-  GIT_TAG 4828736
+  GIT_TAG master
   INSTALL_COMMAND ""  # disables the install step for the external project
 )
 
