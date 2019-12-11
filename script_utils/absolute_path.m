8b8a2ad6b844a9a7d8e7b98f07453b62db68a602
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-11 12:32:23 -0500

    matlab: bugfix for matlab only, resolving absolute path from other directories

--- script_utils/absolute_path.m
+++ script_utils/absolute_path.m
@@ -7,7 +7,11 @@ path = expanduser(path);
 if isoctave
   abspath = make_absolute_filename(path);
 else
+  if path(1) == '.'
+    % this is necessary when run from arbitrary directories.
+    path = [pwd, filesep, path];
+  end
   abspath = char(java.io.File(path).getCanonicalPath());
 end
 
-end
+end % function
