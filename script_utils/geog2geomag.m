78a4ad4a175c56ac2aed553af41ec2b499198027
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-17 12:55:46 -0500

    geog2geomag: use builtin mod() and logical indexing

--- script_utils/geog2geomag.m
+++ script_utils/geog2geomag.m
@@ -1,30 +1,27 @@
-function [thetat,phit]=geog2geomag(lat,lon)
+function [thetat,phit] = geog2geomag(lat,lon)
+narginchk(2,2)
+validateattributes(lat, {'numeric'}, {'scalar', 'vector'}, mfilename)
+validateattributes(lon, {'numeric'}, {'scalar', 'vector'}, mfilename)
 
-%validateattr(lat, {'numeric'}, {'vector'}, mfilename)   %these can also be scalar
-%validateattr(lon, {'numeric'}, {'vector'}, mfilename)   %can also be scalar
-  
-  thetan=11*pi/180;
-  phin=289*pi/180;
+thetan = deg2rad(11);
+phin = deg2rad(289);
 
 
-  %enforce a [0,360] longitude
-  inds=find(lon<0);
-  loncorrected=lon;
-  loncorrected(inds)=lon(inds)+360;
-  inds=find(lon>360);
-  loncorrected(inds)=lon(inds)-360;
+%enforce [0,360] longitude
+loncorrected = mod(lon, 360);
 
-  thetagp=pi/2-lat*pi/180;
-%  phig=lon*pi/180;
-  phig=loncorrected*pi/180;
+thetagp = pi/2 - deg2rad(lat);
+phig = deg2rad(loncorrected);
 
-  thetat=acos(cos(thetagp).*cos(thetan)+sin(thetagp).*sin(thetan).*cos(phig-phin));
-  argtmp=(cos(thetagp)-cos(thetat).*cos(thetan))./(sin(thetat).*sin(thetan));
-  alpha=acos( max(min(argtmp,1),-1) );
-  phit=zeros(size(lat));
-  inds=find( (phin>phig & phin-phig>pi) | (phin<phig & phig-phin<pi) );
-  phit(inds)=pi-alpha(inds);
-  inds=find( ~( (phin>phig & phin-phig>pi) | (phin<phig & phig-phin<pi) ) );
-  phit(inds)=alpha(inds)+pi;
+thetat = acos(cos(thetagp).*cos(thetan)+sin(thetagp).*sin(thetan).*cos(phig-phin));
+argtmp = (cos(thetagp)-cos(thetat).*cos(thetan))./(sin(thetat).*sin(thetan));
+alpha = acos( max(min(argtmp,1),-1) );
+phit = zeros(size(lat));
+
+i = (phin>phig & phin-phig>pi) | (phin<phig & phig-phin<pi);
+phit(i) = pi-alpha(i);
+
+i = ~( (phin>phig & phin-phig>pi) | (phin<phig & phig-phin<pi) );
+phit(i) = alpha(i)+pi;
 
 end
