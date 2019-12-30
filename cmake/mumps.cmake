4997590e27e3fa26b0804f05d6f82101fbc88a3c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-12-30 12:21:23 -0500

    cmake doc: issue with Scalapack_runs_ok check and non-default gfortran

--- cmake/mumps.cmake
+++ cmake/mumps.cmake
@@ -55,9 +55,14 @@ if(NOT SCALAPACK_Compiles_OK)
 message(FATAL_ERROR "Scalapack ${SCALAPACK_LIBRARIES} not building with LAPACK ${LAPACK_LIBRARIES} and ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
 endif()
 
+# FIXME: figure out SCALAPACK_Runs_OK with non-default libgfortran.so
+# as this test fails in that case. Is it an issue with LD_LIBRARY_PATH?
+# if so, may be able to pass via CMAKE_REQUIRED_LINK_OPTIONS of check_fortran_source_runs()
+# to underlying try_run() link_options. This would have to be tested on a system where
+# this check currently fails, such as one using a non-default Gfortran.
 check_fortran_source_runs(${_code} SCALAPACK_Runs_OK SRC_EXT f90)
 if(NOT SCALAPACK_Runs_OK)
-message(WARNING "Scalapack ${SCALAPACK_LIBRARIES} not running with LAPACK ${LAPACK_LIBRARIES} and ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
+message(STATUS "Scalapack ${SCALAPACK_LIBRARIES} not running with LAPACK ${LAPACK_LIBRARIES} and ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}")
 endif()
 
 
