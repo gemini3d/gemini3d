230b68a428921116016a1b00884a6138c7c06918
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-13 01:47:59 -0500

    cmake: hdf5 allow more find methods except on windows

--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -1,15 +1,19 @@
 # don't enclose this all in "if(NOT DEFINED HDF5OK)" because CMake intermittantly doesn't cache needed HDF5 variables.
 
-set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE true)
-set(HDF5_SEARCH_WRAPPER false)
-
+if(WIN32)
+  set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE true)
+  set(HDF5_SEARCH_WRAPPER false)
+endif(WIN32)
 # set(HDF5_FIND_DEBUG true)
 
 find_package(HDF5 REQUIRED COMPONENTS Fortran Fortran_HL)
 set(HDF5_LIBRARIES ${HDF5_Fortran_HL_LIBRARIES} ${HDF5_Fortran_LIBRARIES} ${HDF5_LIBRARIES})
 list(APPEND HDF5_INCLUDE_DIRS ${HDF5_Fortran_INCLUDE_DIRS})
 
-if(NOT DEFINED HDF5OK)
+
+if(DEFINED HDF5OK)
+  return()
+endif()
 
 message(STATUS "HDF5 include: ${HDF5_INCLUDE_DIRS}")
 message(STATUS "HDF5 library: ${HDF5_LIBRARIES}")
@@ -19,7 +23,6 @@ endif()
 if(HDF5_Fortran_DEFINITIONS)
   message(STATUS "HDF5 compiler defs: ${HDF5_Fortran_DEFINITIONS}")
 endif()
-endif(NOT DEFINED HDF5OK)
 
 set(CMAKE_REQUIRED_INCLUDES ${HDF5_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${HDF5_LIBRARIES})
