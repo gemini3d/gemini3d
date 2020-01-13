286e245e4c951b7be0092ca71e082dbd22af0f42
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-13 06:40:05 -0500

    cmake: hdf5/nc4 via FetchContent

--- cmake/h5fortran.cmake
+++ cmake/h5fortran.cmake
@@ -1,17 +1,8 @@
-include(${CMAKE_CURRENT_LIST_DIR}/hdf5.cmake)
+include(FetchContent)
 
-include(ExternalProject)
-
-ExternalProject_Add(h5fortran_proj
+FetchContent_Declare(h5fortran_proj
   GIT_REPOSITORY https://github.com/scivision/h5fortran.git
-  GIT_TAG v2.3.1
-  INSTALL_COMMAND ""  # disables the install step for the external project
+  GIT_TAG v2.4.1
 )
 
-ExternalProject_Get_Property(h5fortran_proj BINARY_DIR)
-
-add_library(h5fortran STATIC IMPORTED GLOBAL)
-add_dependencies(h5fortran h5fortran_proj)
-set_target_properties(h5fortran PROPERTIES
-  IMPORTED_LOCATION ${BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}h5fortran${CMAKE_STATIC_LIBRARY_SUFFIX}
-  INTERFACE_INCLUDE_DIRECTORIES ${BINARY_DIR})
+FetchContent_MakeAvailable(h5fortran_proj)
--- cmake/nc4fortran.cmake
+++ cmake/nc4fortran.cmake
@@ -1,17 +1,8 @@
-include(${CMAKE_CURRENT_LIST_DIR}/netcdf.cmake)
+include(FetchContent)
 
-include(ExternalProject)
-
-ExternalProject_Add(nc4fortran_proj
+FetchContent_Declare(nc4fortran_proj
   GIT_REPOSITORY https://github.com/scivision/nc4fortran.git
   GIT_TAG v0.0.1
-  INSTALL_COMMAND ""  # disables the install step for the external project
 )
 
-ExternalProject_Get_Property(nc4fortran_proj BINARY_DIR)
-
-add_library(nc4fortran STATIC IMPORTED GLOBAL)
-add_dependencies(nc4fortran nc4fortran_proj)
-set_target_properties(nc4fortran PROPERTIES
-  IMPORTED_LOCATION ${BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}nc4fortran${CMAKE_STATIC_LIBRARY_SUFFIX}
-  INTERFACE_INCLUDE_DIRECTORIES ${BINARY_DIR})
+FetchContent_MakeAvailable(nc4fortran_proj)
