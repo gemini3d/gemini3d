78fcc735b0b46552bc8dda155bb447a215172301
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-15 23:09:17 -0500

    test_diffusion => hdf5

--- numerical/diffusion/test_diffusion1D.py
+++ numerical/diffusion/test_diffusion1D.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from pathlib import Path
 import numpy as np
+import h5py
 import argparse
 import sys
 
@@ -12,23 +13,25 @@ def read_diffusion1D(fn: Path, doplot: bool = False):
         print(fn, "not found", file=sys.stderr)
         raise SystemExit(77)
 
-    with fn.open("r") as f:
-        lt, lx1 = np.fromfile(f, int, 2, sep=" ")
-        x1 = np.fromfile(f, float, lx1 + 4, sep=" ")
-
+    with h5py.File(fn, 'r') as f:
+        lt = f['/lt'][()]
+        lx1 = f['/lx1'][()]
+        x1 = f['/x1'][:]
+        t = np.empty(lt)
         TsEuler = np.empty((lx1, lt))
         TsBDF2 = np.empty((lx1, lt))
         Tstrue = np.empty((lx1, lt))
-        t = np.empty(lt)
         for i in range(lt):
-            t[i] = np.fromfile(f, float, 1, sep=" ")
-            TsEuler[:, i] = np.fromfile(f, float, lx1, sep=" ")
-            TsBDF2[:, i] = np.fromfile(f, float, lx1, sep=" ")
-            Tstrue[:, i] = np.fromfile(f, float, lx1, sep=" ")
+            t[i] = f[f'/t{i+1:04d}'][()]
+            TsEuler[:, i] = f[f'/TsEuler{i+1:04d}'][:]
+            TsBDF2[:, i] = f[f'/TsBDF2{i+1:04d}'][:]
+            Tstrue[:, i] = f[f'/TsTrue{i+1:04d}'][:]
 
     assert np.isclose(TsEuler[12, 12], 0.770938954253086), "1-D Euler diffusion accuracy"
     assert np.isclose(TsBDF2[12, 12], 0.763236513549944), "1-D BDF2 diffusion accuracy"
     assert np.isclose(Tstrue[12, 12], 0.763014494788105), "1-D true diffusion accuracy"
+
+    print('OK: 1d diffusion')
 
     if not doplot:
         return
@@ -64,3 +67,13 @@ if __name__ == "__main__":
 
     if P.plot:
         show()
+
+    # with fn.open("r") as f:
+    #     lt, lx1 = np.fromfile(f, int, 2, sep=" ")
+    #     x1 = np.fromfile(f, float, lx1 + 4, sep=" ")
+    #
+    #     for i in range(lt):
+    #         t[i] = np.fromfile(f, float, 1, sep=" ")
+    #         TsEuler[:, i] = np.fromfile(f, float, lx1, sep=" ")
+    #         TsBDF2[:, i] = np.fromfile(f, float, lx1, sep=" ")
+    #         Tstrue[:, i] = np.fromfile(f, float, lx1, sep=" ")
