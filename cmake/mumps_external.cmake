08be23999052a3a33ac4e5666c660bd605dfeae4
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-17 13:30:47 -0500

    cmake: mumps v5.2.1.6 FetchContent

--- cmake/mumps_external.cmake
+++ cmake/mumps_external.cmake
@@ -1,34 +1,11 @@
-include(ExternalProject)
+include(FetchContent)
 
-ExternalProject_Add(MUMPS
+FetchContent_Declare(MUMPS_proj
   GIT_REPOSITORY https://github.com/scivision/mumps.git
-  GIT_TAG v5.2.1.5
+  GIT_TAG v5.2.1.6
   CMAKE_ARGS "-Darith=${arith}" "-Dparallel=true" "-Dmetis=${metis}" "-Dscotch=${scotch}" "-Dopenmp=false"
-  INSTALL_COMMAND ""  # disables the install step for the external project
 )
 
-ExternalProject_Get_Property(MUMPS BINARY_DIR SOURCE_DIR)
-set(MUMPS_BINARY_DIR ${BINARY_DIR})
-set(MUMPS_SOURCE_DIR ${SOURCE_DIR})
+FetchContent_MakeAvailable(MUMPS_proj)
 
-# FIXME: this doesn't use Scotch or Metis, but could simply.
-
-unset(MUMPS_LIBRARIES)
-foreach(a ${arith})
-  list(APPEND MUMPS_LIBRARIES ${a}mumps)
-endforeach()
-
-list(APPEND MUMPS_LIBRARIES mumps_common pord)
-
-foreach(l ${MUMPS_LIBRARIES})
-  add_library(${l} STATIC IMPORTED GLOBAL)
-  add_dependencies(${l} MUMPS)
-  set_target_properties(${l} PROPERTIES
-    IMPORTED_LOCATION ${MUMPS_BINARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${l}${CMAKE_STATIC_LIBRARY_SUFFIX})
-endforeach()
-
-set(MUMPS_INCLUDE_DIRS ${MUMPS_BINARY_DIR} ${MUMPS_SOURCE_DIR}/include)
-
-foreach(a ${arith})
-  target_link_libraries(${a}mumps INTERFACE mumps_common pord)
-endforeach()
+set(MUMPS_LIBRARIES mumps::mumps)
