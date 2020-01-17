d15885f024d6245715cd1ebdbb668f13b1038a61
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-01-17 14:20:35 -0500

    cmake: remove -Wpedantic as too many false postives

--- cmake/compiler_gnu.cmake
+++ cmake/compiler_gnu.cmake
@@ -1,5 +1,7 @@
 set(CMAKE_Fortran_FLAGS "-march=native -fimplicit-none -Wall -Wextra ")
 
+# -Wpedantic makes too many false positives, through Gfortran 9
+
 if(CMAKE_BUILD_TYPE STREQUAL Debug)
   string(APPEND CMAKE_Fortran_FLAGS "-Werror=array-bounds -fcheck=all ")
   # string(APPEND CMAKE_Fortran_FLAGS "-ffpe-trap=invalid,zero,overflow ")#,underflow)
@@ -9,5 +11,5 @@ endif()
 
 # enforce Fortran 2018 standard
 if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
-   string(APPEND CMAKE_Fortran_FLAGS "-std=f2018 -Wpedantic ")
+   string(APPEND CMAKE_Fortran_FLAGS "-std=f2018 ")
 endif()
