0c231dde2fd0e01feb91f12e48a55f4779bd9ddf
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-02 12:01:03 -0500

    python: functionlize efield plot

--- vis/plot_Efield.py
+++ vis/plot_Efield.py
@@ -6,17 +6,33 @@ plot electric field input to simulation "Efield_inputs"
 import argparse
 from matplotlib.pyplot import figure, show
 import gemini
+import numpy as np
 
 
-p = argparse.ArgumentParser()
-p.add_argument("fn", help=".dat or .h5 filename to load directly")
-P = p.parse_args()
+def plotVmaxx1it(V: np.ndarray):
 
-dat = gemini.read_Efield(P.fn)
+    V = V.squeeze()
+    fg = figure()
+    ax = fg.gca()
+    ax.set_title("Vmaxx1it: Potential")
+    if V.ndim == 1:
+        ax.plot(dat["mlat"], V)
+        ax.set_ylabel("Potential [V]")
+        ax.set_xlabel("mag. latitude [deg.]")
+    elif V.ndim == 2:
+        hi = ax.pcolormesh(dat["mlon"], dat["mlat"], V, cmap="bwr")
+        ax.set_xlabel("mag. longitude [deg.]")
+        ax.set_ylabel("mag. latitude [deg.]")
+        fg.colorbar(hi, ax=ax).set_label("potential [V]")
 
-fg = figure()
-ax = fg.gca()
-hi = ax.plot(dat["mlat"], dat["Vmaxx1it"].squeeze())
-ax.set_ylabel("Potential [V]")
-ax.set_xlabel("mag. latitude [deg.]")
-show()
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("fn", help=".dat or .h5 filename to load directly")
+    P = p.parse_args()
+
+    dat = gemini.read_Efield(P.fn)
+
+    plotVmaxx1it(dat["Vmaxx1it"])
+
+    show()
