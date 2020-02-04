79050ce4d9218cf5953d473a156f7a7071ef28c8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-04 12:56:57 -0500

    meson configure: real64 test_scalapack

--- cmake/scalapack.cmake
+++ cmake/scalapack.cmake
@@ -22,6 +22,7 @@ set(CMAKE_REQUIRED_INCLUDES ${SCALAPACK_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${SCALAPACK_LIBRARIES} ${LAPACK_LIBRARIES} MPI::MPI_Fortran)
 # MPI needed for ifort
 
+# FIXME: make real32 test
 file(READ ${CMAKE_SOURCE_DIR}/tests/test_scalapack_d.f90 _code)
 
 check_fortran_source_compiles(${_code} SCALAPACK_Compiles_OK SRC_EXT f90)
