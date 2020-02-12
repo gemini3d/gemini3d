8b63afa5cfe0d24e127bfc63a695989ae8113267
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-12 16:10:58 -0500

    python: job.py: hint where gemini.bin is

--- initialize/test2d_eq/job.py
+++ initialize/test2d_eq/job.py
@@ -20,16 +20,22 @@ def main(mpiexec: Pathlike, gemexe: Pathlike, config_file: Pathlike, out_dir: Pa
     if not mpiexec:
         mpiexec = "mpiexec"
     mpi_root = os.getenv("MPI_ROOT")
-    mpiexec = shutil.which(mpiexec, path=mpi_root)
+    mpiexec = shutil.which(mpiexec, path=mpi_root + "/bin")
     if not mpiexec:
         raise FileNotFoundError("Need mpiexec to run simulations")
     print("mpiexec:", mpiexec)
 
-    if not gemexe:
-        gemexe = Path(__file__).parents[2] / 'build/gemini.bin'
-    gemexe = shutil.which(gemexe)
-    if not gemexe:
-        raise FileNotFoundError("Cannot find gemini.bin")
+    if gemexe:
+        gemexe = Path(gemexe).expanduser()
+        if not gemexe.is_file():
+            raise FileNotFoundError(gemexe)
+    else:
+        build_dir = Path(__file__).resolve().parents[2] / 'build'
+        if not build_dir.is_dir():
+            raise NotADirectoryError(build_dir)
+        gemexe = shutil.which("gemini.bin", path=str(build_dir))
+        if not gemexe:
+            raise FileNotFoundError("Cannot find gemini.bin")
     print("gemini executable:", gemexe)
 
     out_dir = Path(out_dir).expanduser()
