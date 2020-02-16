4f65fe8b7f8e3902003def2952495e5a7f25e64b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-15 22:37:03 -0500

    runner: add checks and functionalize

--- gemini/job.py
+++ gemini/job.py
@@ -1,15 +1,51 @@
 import typing as T
 import os
+import logging
 import subprocess
 import shutil
 from pathlib import Path
+
 from .utils import get_mpi_count
+from .config import read_config
 
 Pathlike = T.Union[str, Path]
 cwd = os.getcwd()
 
 
 def runner(mpiexec: Pathlike, gemexe: Pathlike, config_file: Pathlike, out_dir: Path):
+
+    # load configuration to know what directories to check
+    p = read_config(config_file)
+
+    if p.get('flagE0file') == 1:
+        E0dir = p["E0dir"].resolve()
+        if not E0dir.is_dir():
+            raise FileNotFoundError(E0dir)
+    if p.get("flagprecfile") == 1:
+        precdir = p["precdir"].resolve()
+        if not precdir.is_dir():
+            raise FileNotFoundError(precdir)
+
+    # check MPIexec
+    mpiexec = check_mpiexec(mpiexec)
+    logging.info(f"Using mpiexec: {mpiexec}")
+
+    gemexe = check_gemini_exe(gemexe)
+    logging.info(f"using gemini executable: {gemexe}")
+
+    out_dir = check_outdir(out_dir)
+
+    Nmpi = get_mpi_count(config_file, cwd=cwd)
+
+    cmd = [str(mpiexec), "-n", str(Nmpi), str(gemexe), str(config_file), str(out_dir)]
+    print(" ".join(cmd), "\n")
+    ret = subprocess.run(cmd)
+
+    raise SystemExit(ret.returncode)
+
+
+def check_mpiexec(mpiexec: Pathlike) -> str:
+    """ check that specified mpiexec exists on this system """
 
     if not mpiexec:
         mpiexec = "mpiexec"
@@ -24,7 +60,12 @@ def runner(mpiexec: Pathlike, gemexe: Pathlike, config_file: Pathlike, out_dir: 
             msg += "\na) Windows Subsystem for Linux (WSL) <-- recommended \nb) Cygwin"
             msg += "\nc) Intel Parallel Studio for Windows (or WSL)"
         raise FileNotFoundError(msg)
-    print("Using mpiexec:", mpiexec)
+
+    return mpiexec
+
+
+def check_gemini_exe(gemexe: Pathlike) -> str:
+    """ check that Gemini exectuable can run on this system """
 
     if gemexe:
         gemexe = Path(gemexe).expanduser()
@@ -47,7 +88,10 @@ def runner(mpiexec: Pathlike, gemexe: Pathlike, config_file: Pathlike, out_dir: 
             f"{ret.stderr}"
         )
 
-    print("using gemini executable", gemexe)
+    return gemexe
+
+
+def check_outdir(out_dir: Pathlike) -> Path:
 
     out_dir = Path(out_dir).expanduser()
     if out_dir.is_file():
@@ -55,10 +99,4 @@ def runner(mpiexec: Pathlike, gemexe: Pathlike, config_file: Pathlike, out_dir: 
     if not out_dir.is_dir():
         out_dir.mkdir(parents=True, exist_ok=True)
 
-    Nmpi = get_mpi_count(config_file, cwd=cwd)
-
-    cmd = [str(mpiexec), "-n", str(Nmpi), str(gemexe), str(config_file), str(out_dir)]
-    print(" ".join(cmd))
-    ret = subprocess.run(cmd)
-
-    raise SystemExit(ret.returncode)
+    return out_dir
