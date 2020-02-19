2190aea6e6d5f6d4ba4a326e23673901bc71d00a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-19 13:43:09 -0500

    matlab: read_nml more robust

--- script_utils/read_config.m
+++ script_utils/read_config.m
@@ -7,9 +7,9 @@ filename = get_configfile(path);
 [~,~,ext] = fileparts(filename);
 
 switch ext
-    case '.ini', params = read_ini(filename);
-    case '.nml', params = read_nml(filename);
-    otherwise, error(['not sure how to read config file ', filename])
+  case '.ini', params = read_ini(filename);
+  case '.nml', params = read_nml(filename);
+  otherwise, error(['not sure how to read config file ', filename])
 end
 
 end % function
