bb68135e24b0b031df7581f468b97fd2f8d0b01a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-19 12:24:20 -0500

    matlab read_nml: handle empty and comment lines

--- script_utils/read_nml_group.m
+++ script_utils/read_nml_group.m
@@ -1,8 +1,8 @@
 function params = read_nml_group(filename, group)
 
 narginchk(2,2)
-assert(is_file(filename), ['config file ', filename, ' not found.'])
-validateattr(group, {'char'}, {'vector'}, mfilename, 'nml group name', 2)
+assert(is_file(filename), [filename, ' not found.'])
+validateattributes(group, {'char'}, {'vector'}, mfilename, 'nml group name', 2)
 
 params = struct();
 
@@ -19,12 +19,21 @@ assert(~isempty(mgrp), ['did not find group ', group, ' in ', filename])
 
 while ~feof(fid)
   line = fgetl(fid);
+  % detect end of group
   mend = regexp(line, '^\s*/\s*$', 'match');
   if ~isempty(mend)
      break
   end
 
+  comment_line = regexp(line, '^\s*!', 'match');
+  if ~isempty(comment_line)
+    continue
+  end
+
   [k, v] = strtok(line, '=');
+  if isempty(k) || isempty(v)  % blank or malformed line
+    continue
+  end
   v = strtok(v(2:end), '!');  % discard comments
   % is it floats of up to length 3?
   vals = sscanf(v, '%f,%f,%f');
