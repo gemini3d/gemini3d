8749452fa6858e6e23e98f2d2085a055fd411580
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-20 22:46:04 -0500

    fix cmake_fortran_flags, h5fortran 2.5.5

--- cmake/h5fortran.cmake
+++ cmake/h5fortran.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(h5fortran_proj
   GIT_REPOSITORY https://github.com/scivision/h5fortran.git
-  GIT_TAG v2.5.4
+  GIT_TAG v2.5.5
 )
 
 FetchContent_MakeAvailable(h5fortran_proj)
