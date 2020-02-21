95403d1f2b9e3cbc725201a9dafe1ff4f0786be7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-21 00:45:46 -0500

    ci: new 2d hdf5 test data

--- cmake/download.cmake
+++ cmake/download.cmake
@@ -1,4 +1,4 @@
-function(download_testfiles HASH REFNUM REFNAME ROOT)
+function(download_testfiles HASH URL REFNAME ROOT)
 # FetchContent is too aggressive, it deletes the output directory before extracting.
 # That could delete wrong directory causing data loss.
 # So use this more specific but safe method.
@@ -6,29 +6,33 @@ function(download_testfiles HASH REFNUM REFNAME ROOT)
 # We can't use CMAKE_ROLE to defer downloading because the tests parameters are defined
 # using the test reference data.
 
-set(ARCHIVE ${REFNAME}.zip)
-set(URL https://zenodo.org/record/${REFNUM}/files/${REFNAME}.zip?download=1)
+set(_zipname ${REFNAME}.zip)
+set(_zipfile ${ROOT}/${_zipname})
 
-# --- ensure reference data is available for self-test
 if(EXISTS ${ROOT}/${REFNAME})
+  # reference file already extracted to directory
   return()
 endif()
 
-if(NOT EXISTS ${ROOT}/${ARCHIVE})
-
-file(DOWNLOAD ${URL} ${ROOT}/${ARCHIVE})
-# don't check hash during download in case of failure, so it doesn't stop using rest of program.
+if(NOT EXISTS ${_zipfile})
+  # don't check hash during download in case of failure, so it doesn't stop using rest of program.
+  file(DOWNLOAD ${URL} ${_zipfile})
 endif()
 
-file(MD5 ${ROOT}/${ARCHIVE} FHASH)
-
-if(FHASH STREQUAL HASH)
-# extract zip file (os-agnostic)
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf ${ARCHIVE}
-                WORKING_DIRECTORY ${ROOT})
-else()
-  message(WARNING "Self-tests may not work. Reference data did not match hash ${HASH} \n Download ${URL}  to  ${ARCHIVE} \n")
+file(SIZE ${_zipfile} _zipsize)
+if(_zipsize LESS 1000000)
+  message(STATUS "SKIP: outdated URL or truncated download: ${REFNAME}")
+  return()
 endif()
+
+file(MD5 ${_zipfile} _ziphash)
+
+if(NOT _ziphash STREQUAL HASH) # os-agnostic
+  message(STATUS "SKIP: wrong URL or corrupted file: ${REFNAME}")
+  return()
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf ${_zipname} WORKING_DIRECTORY ${ROOT})
 
 
 endfunction(download_testfiles)
