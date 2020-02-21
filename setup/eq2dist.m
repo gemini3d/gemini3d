caca3a9c846c4b6f6370b24f600bfac0ad848276
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-21 13:50:05 -0500

    matlab: add more checks for interpolation sanity

--- setup/eq2dist.m
+++ setup/eq2dist.m
@@ -1,4 +1,4 @@
-function [nsi,vs1i,Tsi,xgin,ns,vs1,Ts] = eq2dist(p, xg)
+function [nsi,vs1i,Tsi] = eq2dist(p, xg)
 % read and interpolate equilibrium simulation data, writing new
 % interpolated grid.
 narginchk(2, 2)
@@ -19,21 +19,48 @@ xgin = readgrid(p.eqnml, p.format);
 %% LOAD THE last equilibrium frame
 dat = loadframe(p.eqdir, ymd_end, UTsec_end, peq.flagoutput, peq.mloc, xgin, p.format, p.eqnml);
 
-%% check input to interpolation
-assert(all(isfinite(dat.ns(:))), 'non-finite density')
-assert(all(isfinite(dat.vs1(:))), 'non-finite drift')
-assert(all(isfinite(dat.Ts(:))), 'non-finite temperature')
+%% sanity check equilibrium simulation input to interpolation
+check_density(dat.ns)
+check_drift(dat.vs1)
+check_temperature(dat.Ts)
 
 %% DO THE INTERPOLATION
 [nsi,vs1i,Tsi] = model_resample(xgin, dat.ns, dat.vs1, dat.Ts, xg);
 
-%% check IF THE INTERPOLATION WENT WEIRD...
-assert(all(isfinite(nsi(:))), 'non-finite interpolated density')
-assert(all(isfinite(vs1i(:))), 'non-finite interpolated drift')
-assert(all(isfinite(Tsi(:))), 'non-finite interpolated temperature')
+%% sanity check interpolated variables
+check_density(nsi)
+check_drift(vs1i)
+check_temperature(Tsi)
 
 %% WRITE OUT THE GRID
 writegrid(p, xg)
 writedata(ymd_end, UTsec_end,nsi,vs1i,Tsi, p.simdir, p.format);
 
 end % function eq2dist
+
+
+function check_density(n)
+narginchk(1,1)
+
+n = n(:);
+assert(all(isfinite(n)), 'non-finite density')
+assert(all(n > 0), 'negative density')
+assert(max(n > 1e6), 'too small maximum density')
+end
+
+function check_drift(v)
+narginchk(1,1)
+
+v = v(:);
+assert(all(isfinite(v)), 'non-finite drift')
+assert(all(abs(v) < 10e3), 'excessive drift velocity')
+end
+
+function check_temperature(T)
+
+T = T(:);
+assert(all(isfinite(T)), 'non-finite temperature')
+assert(all(T > 0), 'negative temperature')
+assert(max(T) > 500, 'too cold maximum temperature')
+
+end
