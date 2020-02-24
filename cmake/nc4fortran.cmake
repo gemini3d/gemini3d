895859cc5c44f6ef9fefb30d9bf3ada68eebbcb1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-23 22:36:31 -0500

    h5fortran v2.6.1

--- cmake/nc4fortran.cmake
+++ cmake/nc4fortran.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(nc4fortran_proj
   GIT_REPOSITORY https://github.com/scivision/nc4fortran.git
-  GIT_TAG v0.1.0
+  GIT_TAG v0.2.0
 )
 
 FetchContent_MakeAvailable(nc4fortran_proj)
