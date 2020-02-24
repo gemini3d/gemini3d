008aa7902b6f4d6ed90160adb91c8e1cc6809085
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-24 14:51:37 -0500

    matlab: write grid in real32 or real64

--- script_utils/writedata.m
+++ script_utils/writedata.m
@@ -1,4 +1,4 @@
-function writedata(ymd, UTsec,ns,vsx1,Ts, outdir, file_format)
+function writedata(ymd, UTsec,ns,vsx1,Ts, outdir, file_format, realbits)
 
 %--------------------------------------------------------
 %-----WRITE STATE VARIABLE DATA TO BE USED AS INITIAL
@@ -10,44 +10,51 @@ function writedata(ymd, UTsec,ns,vsx1,Ts, outdir, file_format)
 %-----INPUT ARRAYS SHOULD BE TRIMMED TO THE CORRECT SIZE
 %-----(I.E. THEY SHOULD NOT INCLUDE GHOST CELLS
 %--------------------------------------------------------
-narginchk(7,7)
+narginchk(8,8)
 validateattributes(ymd, {'numeric'}, {'vector', 'positive', 'numel', 3}, mfilename, 'year, month, day', 1)
 validateattributes(UTsec, {'numeric'}, {'scalar', 'nonnegative'}, mfilename, 'seconds since UT midnight', 2)
 validateattributes(ns, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'density', 3)
 validateattributes(vsx1, {'numeric'}, {'ndims', 4}, mfilename, 'velocity', 4)
 validateattributes(Ts, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'temperature', 5)
 validateattributes(outdir, {'char'}, {'vector'}, mfilename, 'output directory',6)
-validateattr(file_format, {'char'}, {'vector'}, mfilename,'hdf5 or raw',7)
+validateattributes(file_format, {'char'}, {'vector'}, mfilename,'hdf5 or raw',7)
+validateattributes(realbits, {'numeric'}, {'scalar','integer'},mfilename, '32 or 64',8)
 
 outdir = absolute_path(outdir);
 makedir(outdir)
 
 switch file_format
-  case {'h5','hdf5'}, write_hdf5(outdir, ymd, UTsec, ns, vsx1, Ts)
-  case {'dat','raw'}, write_raw(outdir, ymd, UTsec, ns, vsx1, Ts)
+  case {'h5','hdf5'}, write_hdf5(outdir, ymd, UTsec, ns, vsx1, Ts, realbits)
+  case {'dat','raw'}, write_raw(outdir, ymd, UTsec, ns, vsx1, Ts, realbits)
   otherwise, error('unknown file_format')
 end
 
 end % function
 
 
-function write_hdf5(outdir, ymd, UTsec, ns, vsx1, Ts)
+function write_hdf5(outdir, ymd, UTsec, ns, vsx1, Ts, realbits)
 fn = [outdir,'/initial_conditions.h5'];
 disp(['write ',fn])
 h5save(fn, '/ymd', int32(ymd))
-h5save(fn, '/UTsec', UTsec)
-h5save(fn, '/ns', ns)
-h5save(fn, '/vsx1', vsx1)
-h5save(fn, '/Ts', Ts)
-end
+
+freal = ['float',int2str(realbits)];
+
+h5save(fn, '/UTsec', UTsec, [], freal)
+h5save(fn, '/ns', ns, [], freal)
+h5save(fn, '/vsx1', vsx1, [], freal)
+h5save(fn, '/Ts', Ts, [], freal)
+
+end % function
 
 
-function write_raw(outdir, ymd, UTsec, ns, vsx1, Ts)
+function write_raw(outdir, ymd, UTsec, ns, vsx1, Ts, realbits)
 
 fn = [outdir,'/initial_conditions.dat'];
 disp(['write ',fn])
 fid=fopen(fn, 'w');
-freal = 'float64';
+
+freal = ['float',int2str(realbits)];
+
 fwrite(fid, ymd, freal);
 fwrite(fid, UTsec, freal);
 fwrite(fid,ns, freal);
--- script_utils/writegrid.m
+++ script_utils/writegrid.m
@@ -10,15 +10,15 @@ outdir = absolute_path(p.simdir);
 makedir(outdir)
 
 switch p.format
-  case {'dat','raw'}, write_raw(outdir, xg)
-  case {'h5','hdf5'}, write_hdf5(outdir, xg)
+  case {'dat','raw'}, write_raw(outdir, xg, p.realbits)
+  case {'h5','hdf5'}, write_hdf5(outdir, xg, p.realbits)
   otherwise, error(['unknown file format ', p.format])
 end
 
 end % function
 
 
-function write_hdf5(dir_out, xg)
+function write_hdf5(dir_out, xg, realbits)
 
 fn = [dir_out, '/simsize.h5'];
 disp(['write ',fn])
@@ -30,69 +30,74 @@ h5save(fn, '/lx3', int32(xg.lx(3)))
 fn = [dir_out, '/simgrid.h5'];
 disp(['write ',fn])
 if isfile(fn), delete(fn), end
-h5save(fn, '/x1', xg.x1)
-h5save(fn, '/x1i', xg.x1i)
-h5save(fn, '/dx1b', xg.dx1b)
-h5save(fn, '/dx1h', xg.dx1h)
 
-h5save(fn, '/x2', xg.x2)
-h5save(fn, '/x2i', xg.x2i)
-h5save(fn, '/dx2b', xg.dx2b)
-h5save(fn, '/dx2h', xg.dx2h)
+freal = ['float',int2str(realbits)];
 
-h5save(fn, '/x3', xg.x3)
-h5save(fn, '/x3i', xg.x3i)
-h5save(fn, '/dx3b', xg.dx3b)
-h5save(fn, '/dx3h', xg.dx3h)
+h5save(fn, '/x1', xg.x1, [], freal)
+h5save(fn, '/x1i', xg.x1i, [], freal)
+h5save(fn, '/dx1b', xg.dx1b, [], freal)
+h5save(fn, '/dx1h', xg.dx1h, [], freal)
 
-h5save(fn, '/h1', xg.h1)
-h5save(fn, '/h2', xg.h2)
-h5save(fn, '/h3', xg.h3)
+h5save(fn, '/x2', xg.x2, [], freal)
+h5save(fn, '/x2i', xg.x2i, [], freal)
+h5save(fn, '/dx2b', xg.dx2b, [], freal)
+h5save(fn, '/dx2h', xg.dx2h, [], freal)
 
-h5save(fn, '/h1x1i', xg.h1x1i)
-h5save(fn, '/h2x1i', xg.h2x1i)
-h5save(fn, '/h3x1i', xg.h3x1i)
+h5save(fn, '/x3', xg.x3, [], freal)
+h5save(fn, '/x3i', xg.x3i, [], freal)
+h5save(fn, '/dx3b', xg.dx3b, [], freal)
+h5save(fn, '/dx3h', xg.dx3h, [], freal)
 
-h5save(fn, '/h1x2i', xg.h1x2i)
-h5save(fn, '/h2x2i', xg.h2x2i)
-h5save(fn, '/h3x2i', xg.h3x2i)
+h5save(fn, '/h1', xg.h1, [], freal)
+h5save(fn, '/h2', xg.h2, [], freal)
+h5save(fn, '/h3', xg.h3, [], freal)
 
-h5save(fn, '/h1x3i', xg.h1x3i)
-h5save(fn, '/h2x3i', xg.h2x3i)
-h5save(fn, '/h3x3i', xg.h3x3i)
+h5save(fn, '/h1x1i', xg.h1x1i, [], freal)
+h5save(fn, '/h2x1i', xg.h2x1i, [], freal)
+h5save(fn, '/h3x1i', xg.h3x1i, [], freal)
 
-h5save(fn, '/gx1', xg.gx1)
-h5save(fn, '/gx2', xg.gx2)
-h5save(fn, '/gx3', xg.gx3)
+h5save(fn, '/h1x2i', xg.h1x2i, [], freal)
+h5save(fn, '/h2x2i', xg.h2x2i, [], freal)
+h5save(fn, '/h3x2i', xg.h3x2i, [], freal)
 
-h5save(fn, '/alt', xg.alt)
-h5save(fn, '/glat', xg.glat)
-h5save(fn, '/glon', xg.glon)
+h5save(fn, '/h1x3i', xg.h1x3i, [], freal)
+h5save(fn, '/h2x3i', xg.h2x3i, [], freal)
+h5save(fn, '/h3x3i', xg.h3x3i, [], freal)
 
-h5save(fn, '/Bmag', xg.Bmag)
-h5save(fn, '/I', xg.I)
-h5save(fn, '/nullpts', xg.nullpts)
+h5save(fn, '/gx1', xg.gx1, [], freal)
+h5save(fn, '/gx2', xg.gx2, [], freal)
+h5save(fn, '/gx3', xg.gx3, [], freal)
 
-h5save(fn, '/e1', xg.e1)
-h5save(fn, '/e2', xg.e2)
-h5save(fn, '/e3', xg.e3)
+h5save(fn, '/alt', xg.alt, [], freal)
+h5save(fn, '/glat', xg.glat, [], freal)
+h5save(fn, '/glon', xg.glon, [], freal)
 
-h5save(fn, '/er', xg.er)
-h5save(fn, '/etheta', xg.etheta)
-h5save(fn, '/ephi', xg.ephi)
+h5save(fn, '/Bmag', xg.Bmag, [], freal)
+h5save(fn, '/I', xg.I, [], freal)
+h5save(fn, '/nullpts', xg.nullpts, [], freal)
 
-h5save(fn, '/r', xg.r)
-h5save(fn, '/theta', xg.theta)
-h5save(fn, '/phi', xg.phi)
+h5save(fn, '/e1', xg.e1, [], freal)
+h5save(fn, '/e2', xg.e2, [], freal)
+h5save(fn, '/e3', xg.e3, [], freal)
 
-h5save(fn, '/x', xg.x)
-h5save(fn, '/y', xg.y)
-h5save(fn, '/z', xg.z)
+h5save(fn, '/er', xg.er, [], freal)
+h5save(fn, '/etheta', xg.etheta, [], freal)
+h5save(fn, '/ephi', xg.ephi, [], freal)
+
+h5save(fn, '/r', xg.r, [], freal)
+h5save(fn, '/theta', xg.theta, [], freal)
+h5save(fn, '/phi', xg.phi, [], freal)
+
+h5save(fn, '/x', xg.x, [], freal)
+h5save(fn, '/y', xg.y, [], freal)
+h5save(fn, '/z', xg.z, [], freal)
+
 end % function
 
 
-function write_raw(outdir, xg)
-freal = 'float64';
+function write_raw(outdir, xg, realbits)
+
+freal = ['float', int2str(realbits)];
 
 filename = [outdir, '/simsize.dat'];
 disp(['write ',filename])
