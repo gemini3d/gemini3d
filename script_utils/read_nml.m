fb82641c870898304323775085468227a95d0271
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-27 13:59:57 -0500

    matlab:read_nml: make absolute paths upon read

--- script_utils/read_nml.m
+++ script_utils/read_nml.m
@@ -6,6 +6,9 @@ narginchk(1,1)
 filename = get_configfile(path);
 
 p = read_nml_group(filename, 'base');
+p.indat_file = absolute_path(p.indat_file);
+p.indat_size = absolute_path(p.indat_size);
+p.indat_grid = absolute_path(p.indat_grid);
 
 if ~isfield(p, 'nml')
   p.nml = filename;
@@ -13,6 +16,9 @@ end
 
 try %#ok<TRYNC>
 p = merge_struct(p, read_nml_group(filename, 'setup'));
+end
+if isfield(p, 'eqdir')
+  p.eqdir = absolute_path(p.eqdir);
 end
 
 if isfield(p, 'flagdneu') && p.flagdneu
@@ -24,10 +30,12 @@ end
 
 if isfield(p, 'flagprecfile') && p.flagprecfile
   p = merge_struct(p, read_nml_group(filename, 'precip'));
+  p.prec_dir = absolute_path(p.prec_dir);
 end
 
 if isfield(p, 'flagE0file') && p.flagE0file
   p = merge_struct(p, read_nml_group(filename, 'efield'));
+  p.E0_dir = absolute_path(p.E0_dir);
 end
 
 if isfield(p, 'flagglow') && p.flagglow
