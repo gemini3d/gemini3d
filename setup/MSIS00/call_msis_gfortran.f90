5a8cbbbe66f40be16343d206765f97acc10cccaf
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-29 21:55:36 -0500

    add more checks to msis_setup

--- setup/MSIS00/call_msis_gfortran.f90
+++ setup/MSIS00/call_msis_gfortran.f90
@@ -2,7 +2,8 @@ use, intrinsic:: iso_fortran_env, only: sp=>real32
 
 implicit none
 
-integer :: iyd,sec,mass=48,iz,lz
+integer, parameter :: mass=48
+integer :: iyd,sec,lz, i
 real(sp) :: f107a,f107,ap(7),stl,apday,ap3
 real(sp) :: d(9),t(2)
 real(sp), allocatable :: glat(:),glon(:),alt(:)
@@ -11,14 +12,14 @@ character(256) :: buf
 character(:), allocatable :: infile,outfile
 
 !> read in msis inputs
-if (command_argument_count() /= 2) error stop 'must specify input and output filenames'
+if (command_argument_count() < 2) error stop 'msis_setup: must specify input and output filenames'
 
 call get_command_argument(1,buf)
 infile = trim(buf)
 
 block
   integer :: u
-  open(newunit=u,file=infile,status='old',form='unformatted',access='stream', action='read')
+  open(newunit=u,file=infile, status='old',form='unformatted',access='stream', action='read')
   !! use binary to reduce file size and read times
 
   read(u) iyd
@@ -29,7 +30,17 @@ block
   read(u) ap3
   read(u) lz
 
-  print *, infile,iyd,sec,f107a,f107,apday,ap3,lz
+  print *, 'msis_setup parameters: ', infile,iyd,sec,f107a,f107,apday,ap3,lz
+
+  call get_command_argument(3, buf, status=i)
+  if (lz<1) error stop 'lz must be positive'
+  if (i==0) then
+    read(buf,*) i
+    if (i/=lz) then
+      write(stderr,*) 'expected ',i,' grid points but read ',lz
+      error stop
+    endif
+  endif
 
   allocate(glat(lz),glon(lz),alt(lz))
 
@@ -55,18 +66,22 @@ block
     !! use binary to reduce file size and read times
 
   !> call to msis routine
-  do iz=1,lz
-    stl = sec/3600. + glon(iz)/15.
-    call gtd7(iyd, real(sec, sp),alt(iz),glat(iz),glon(iz),stl,f107a,f107,ap,mass,d,t)
-    write(u) alt(iz),d(1:9),t(2)
-  !  write(*,'(1f20.4, 9e20.8, 1f20.4)') alt(iz),d(1:9),t(2)
-  ! write(*,1000),alt(iz),d(1:9),t(2)
+  do i=1,lz
+    stl = sec/3600. + glon(i)/15.
+    call gtd7(iyd, real(sec, sp),alt(i),glat(i),glon(i),stl,f107a,f107,ap,mass,d,t)
+    write(u) alt(i),d(1:9),t(2)
+  !  write(*,'(1f20.4, 9e20.8, 1f20.4)') alt(i),d(1:9),t(2)
+  ! write(*,1000),alt(i),d(1:9),t(2)
   ! 1000 format(1F,9E,1F)
   end do
 
   close(u)
+
+  inquire(file=outfile, size=i)
+  print *,'msis_setup: wrote ',i,' bytes to ',outfile
+  if (i==0) error stop 'msis_setup failed to write file'
 end block
 
-deallocate(glat,glon,alt)
+
 
 end program
