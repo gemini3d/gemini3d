34fc8ded13a47712186856ef6cff43920b36ebc6
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-03 12:40:54 -0500

    h5fortran v2.7.1
    h5fortran >= 2.7 has optional ierr parameter. Omitting it makes error stop on failure.

--- cmake/h5fortran.cmake
+++ cmake/h5fortran.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(h5fortran_proj
   GIT_REPOSITORY https://github.com/scivision/h5fortran.git
-  GIT_TAG v2.6.3
+  GIT_TAG v2.7.1
 )
 
 FetchContent_MakeAvailable(h5fortran_proj)
--- io/reader_hdf5.f90
+++ io/reader_hdf5.f90
@@ -11,7 +11,6 @@ module procedure get_simsize2
 !! get x2 and x3 dimension sizes
 type(hdf5_file) :: hf
 character(:), allocatable :: fn
-integer :: ierr
 logical :: exists
 
 if (index(path, 'simsize.h5') /= 0) then
@@ -27,33 +26,30 @@ if (.not.exists) then
    error stop 77
 endif
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'get_simsize2: simsize.h5'
+call hf%initialize(fn, status='old',action='r')
 
 !> scripts can use variety of variable names
-if(hf%exist('/llat', ierr)) then
-  call hf%read('/llat', llat, ierr)
-elseif(hf%exist('/Nlat', ierr)) then
-  call hf%read('/Nlat', llat, ierr)
-elseif(hf%exist('/lx2', ierr)) then
-  call hf%read('/lx2', llat, ierr)
+if(hf%exist('/llat')) then
+  call hf%read('/llat', llat)
+elseif(hf%exist('/Nlat')) then
+  call hf%read('/Nlat', llat)
+elseif(hf%exist('/lx2')) then
+  call hf%read('/lx2', llat)
 else
   error stop 'reader_hdf5:get_simsize2: llat / lx2'
 endif
-if(ierr/=0) error stop 'reader_hdf5:get_simsize2: llat / lx2'
 
-if(hf%exist('/llon', ierr)) then
-  call hf%read('/llon', llon, ierr)
-elseif(hf%exist('/Nlat', ierr)) then
-  call hf%read('/Nlat', llon, ierr)
-elseif(hf%exist('/lx3', ierr)) then
-  call hf%read('/lx3', llon, ierr)
+if(hf%exist('/llon')) then
+  call hf%read('/llon', llon)
+elseif(hf%exist('/Nlat')) then
+  call hf%read('/Nlat', llon)
+elseif(hf%exist('/lx3')) then
+  call hf%read('/lx3', llon)
 else
   error stop 'reader_hdf5:get_simsize2: llon / lx3'
 endif
-if(ierr/=0) error stop 'reader_hdf5:get_simsize2: llon / lx3'
 
-call hf%finalize(ierr)
+call hf%finalize()
 
 end procedure get_simsize2
 
@@ -63,7 +59,6 @@ module procedure get_simsize3
 !! sizes include Ghost Cells
 type(hdf5_file) :: hf
 character(:), allocatable :: fn
-integer :: ierr
 logical :: exists
 
 if (index(path, 'simsize.h5') /= 0) then
@@ -79,38 +74,33 @@ if (.not.exists) then
    error stop 77
 endif
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'could not open simsize.h5'
+call hf%initialize(fn, status='old',action='r')
 
-if (hf%exist("/lx1", ierr)) then
-  call hf%read('/lx1', lx1, ierr)
-  if(ierr/=0) error stop 'get_simsize3: lx1'
-  call hf%read('/lx2', lx2all, ierr)
-  if(ierr/=0) error stop 'get_simsize3: lx2'
+if (hf%exist("/lx1")) then
+  call hf%read('/lx1', lx1)
+  call hf%read('/lx2', lx2all)
   if (present(lx3all)) then
-    call hf%read('/lx3', lx3all, ierr)
-    if(ierr/=0) error stop 'get_simsize3: lx3'
+    call hf%read('/lx3', lx3all)
   endif
-elseif (hf%exist("/lxs", ierr)) then
+elseif (hf%exist("/lxs")) then
   block
     integer :: lx(3)
-    call hf%read("/lxs", lx, ierr)
+    call hf%read("/lxs", lx)
     lx1 = lx(1)
     lx2all = lx(2)
     if (present(lx3all)) lx3all = lx(3)
   end block
-elseif (hf%exist("/lx", ierr)) then
+elseif (hf%exist("/lx")) then
   block
     integer :: lx(3)
-    call hf%read("/lx", lx, ierr)
+    call hf%read("/lx", lx)
     lx1 = lx(1)
     lx2all = lx(2)
     if (present(lx3all)) lx3all = lx(3)
   end block
 endif
 
-call hf%finalize(ierr)
-if(ierr/=0) error stop 'could not close simsize.h5'
+call hf%finalize()
 
 end procedure get_simsize3
 
@@ -118,7 +108,6 @@ end procedure get_simsize3
 module procedure get_grid2
 type(hdf5_file) :: hf
 character(:), allocatable :: fn
-integer :: ierr
 
 if (index(path, 'simgrid.h5') /= 0) then
   fn = path
@@ -127,13 +116,10 @@ else
 endif
 if (debug) print '(A,/,A)', 'READ 2D (B-perp, B-perp) grid:', fn
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'get_grid2: could not open simgrid.h5'
-call hf%read('/mlon', mlonp, ierr)
-if(ierr/=0) error stop 'get_grid2: mlon'
-call hf%read('/mlat', mlatp, ierr)
-if(ierr/=0) error stop 'get_grid2: mlat'
-call hf%finalize(ierr)
+call hf%initialize(fn, status='old',action='r')
+call hf%read('/mlon', mlonp)
+call hf%read('/mlat', mlatp)
+call hf%finalize()
 
 end procedure get_grid2
 
@@ -142,58 +128,38 @@ module procedure get_Efield
 type(hdf5_file) :: hf
 real(wp) :: flagtmp
 character(:), allocatable :: fn
-integer :: ierr
 
 fn = path // '.h5'
 if (debug) print *, 'READ electric field data from file:  ',fn
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'get_Efield: could not read Efield HDF5'
-call hf%read('/flagdirich', flagdirich, ierr)
-if(ierr/=0) error stop 'get_Efield: flagdirich'
+call hf%initialize(fn, status='old',action='r')
+call hf%read('/flagdirich', flagdirich)
 !! handle degenerate cases to avoid
 !! "Operating system error: Cannot allocate memory Memory allocation failed"
 if (size(E0xp, 1)==1) then
-  call hf%read('/Exit', E0xp(1,:), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Eyit', E0yp(1,:), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vminx1it', Vminx1p(1,:),ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vmaxx1it', Vmaxx1p(1,:), ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Exit', E0xp(1,:))
+  call hf%read('/Eyit', E0yp(1,:))
+  call hf%read('/Vminx1it', Vminx1p(1,:))
+  call hf%read('/Vmaxx1it', Vmaxx1p(1,:))
 elseif (size(E0xp, 2)==1) then
-  call hf%read('/Exit', E0xp(:,1), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Eyit', E0yp(:,1), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vminx1it', Vminx1p(:,1),ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vmaxx1it', Vmaxx1p(:,1), ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Exit', E0xp(:,1))
+  call hf%read('/Eyit', E0yp(:,1))
+  call hf%read('/Vminx1it', Vminx1p(:,1))
+  call hf%read('/Vmaxx1it', Vmaxx1p(:,1))
 else !< 3D
-  call hf%read('/Exit', E0xp, ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Eyit', E0yp, ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vminx1it', Vminx1p,ierr)
-  if(ierr/=0) error stop
-  call hf%read('/Vmaxx1it', Vmaxx1p, ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Exit', E0xp)
+  call hf%read('/Eyit', E0yp)
+  call hf%read('/Vminx1it', Vminx1p)
+  call hf%read('/Vmaxx1it', Vmaxx1p)
 endif
 !! background fields and top/bottom boundary conditions
-call hf%read('/Vminx2ist', Vminx2pslice,ierr)
-if(ierr/=0) error stop
-call hf%read('/Vmaxx2ist', Vmaxx2pslice, ierr)
-if(ierr/=0) error stop
+call hf%read('/Vminx2ist', Vminx2pslice)
+call hf%read('/Vmaxx2ist', Vmaxx2pslice)
 !! these only used for 3D simulations
-call hf%read('/Vminx3ist', Vminx3pslice,ierr)
-if(ierr/=0) error stop
-call hf%read('/Vmaxx3ist', Vmaxx3pslice, ierr)
-if(ierr/=0) error stop
+call hf%read('/Vminx3ist', Vminx3pslice)
+call hf%read('/Vmaxx3ist', Vmaxx3pslice)
 
-call hf%finalize(ierr)
-if(ierr/=0) error stop
+call hf%finalize()
 
 end procedure get_Efield
 
@@ -202,34 +168,25 @@ module procedure get_precip
 type(hdf5_file) :: hf
 real(wp) :: flagtmp
 character(:), allocatable :: fn
-integer :: ierr
 
 fn = path // '.h5'
 if (debug) print *, 'READ precipitation data from file:  ',fn
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'could not open precipitation HDF5'
+call hf%initialize(fn, status='old',action='r')
 !! handle degenerate cases to avoid
 !! "Operating system error: Cannot allocate memory Memory allocation failed"
 if (size(Qp, 1)==1) then
-  call hf%read('/Qp', Qp(1,:), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/E0p', E0p(1,:), ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Qp', Qp(1,:))
+  call hf%read('/E0p', E0p(1,:))
 elseif (size(Qp, 2)==1) then
-  call hf%read('/Qp', Qp(:,1), ierr)
-  if(ierr/=0) error stop
-  call hf%read('/E0p', E0p(:,1), ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Qp', Qp(:,1))
+  call hf%read('/E0p', E0p(:,1))
 else  !< 3-D
-  call hf%read('/Qp', Qp, ierr)
-  if(ierr/=0) error stop
-  call hf%read('/E0p', E0p, ierr)
-  if(ierr/=0) error stop
+  call hf%read('/Qp', Qp)
+  call hf%read('/E0p', E0p)
 endif
 
-call hf%finalize(ierr)
-if(ierr/=0) error stop
+call hf%finalize()
 
 end procedure get_precip
 
@@ -238,28 +195,20 @@ module procedure get_neutral2
 type(hdf5_file) :: hf
 real(wp) :: flagtmp
 character(:), allocatable :: fn
-integer :: ierr
 
 fn = path // '.h5'
 if (debug) print *, 'READ neutral 2D data from file:  ',fn
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'get_neutral2: could not open precipitation HDF5'
+call hf%initialize(fn, status='old',action='r')
 
-call hf%read('/dn0all', dnO, ierr)
-if(ierr/=0) error stop 'get_neutral2: dn0'
-call hf%read('/dnN2all', dnN2, ierr)
-if(ierr/=0) error stop 'get_neutral2: dnN2'
-call hf%read('/dnO2all', dnO2, ierr)
-if(ierr/=0) error stop 'get_neutral2: dnO2'
-call hf%read('/dvnrhoall', dvnrho, ierr)
-if(ierr/=0) error stop 'get_neutral2: dvnrho'
-call hf%read('/dvnzall', dvnz, ierr)
-if(ierr/=0) error stop 'get_neutral2: dvnz'
-call hf%read('/dTnall', dTn, ierr)
-if(ierr/=0) error stop 'get_neutral2: dTn'
+call hf%read('/dn0all', dnO)
+call hf%read('/dnN2all', dnN2)
+call hf%read('/dnO2all', dnO2)
+call hf%read('/dvnrhoall', dvnrho)
+call hf%read('/dvnzall', dvnz)
+call hf%read('/dTnall', dTn)
 
-call hf%finalize(ierr)
+call hf%finalize()
 
 end procedure get_neutral2
 
@@ -268,30 +217,21 @@ module procedure get_neutral3
 type(hdf5_file) :: hf
 real(wp) :: flagtmp
 character(:), allocatable :: fn
-integer :: ierr
 
 fn = path // '.h5'
 if (debug) print *, 'READ neutral 3D data from file:  ',fn
 
-call hf%initialize(fn, ierr, status='old',action='r')
-if(ierr/=0) error stop 'could not open precipitation HDF5'
+call hf%initialize(fn, status='old',action='r')
 
-call hf%read('/dn0all', dnOall, ierr)
-if(ierr/=0) error stop 'get_neutral3: dn0all'
-call hf%read('/dnN2all', dnN2all, ierr)
-if(ierr/=0) error stop 'get_neutral3: dnN2'
-call hf%read('/dnO2all', dnO2all, ierr)
-if(ierr/=0) error stop 'get_neutral3: dnO2all'
-call hf%read('/dvnxall', dvnxall, ierr)
-if(ierr/=0) error stop 'get_neutral3: dnvxall'
-call hf%read('/dvnrhoall', dvnrhoall, ierr)
-if(ierr/=0) error stop 'get_neutral3: dvnrhoall'
-call hf%read('/dvnzall', dvnzall, ierr)
-if(ierr/=0) error stop 'get_neutral3: dvnzall'
-call hf%read('/dTnall', dTnall, ierr)
-if(ierr/=0) error stop 'get_neutral3: dTnall'
+call hf%read('/dn0all', dnOall)
+call hf%read('/dnN2all', dnN2all)
+call hf%read('/dnO2all', dnO2all)
+call hf%read('/dvnxall', dvnxall)
+call hf%read('/dvnrhoall', dvnrhoall)
+call hf%read('/dvnzall', dvnzall)
+call hf%read('/dTnall', dTnall)
 
-call hf%finalize(ierr)
+call hf%finalize()
 
 end procedure get_neutral3
 
