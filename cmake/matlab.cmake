01672f185892f7d9943f9ede8e196f88d25e42b9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-08 22:49:20 -0400

    allow run_test if Python present, even w/o numpy/install
    cmake logic

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -1,18 +1,19 @@
 if(matlab)
+  if(NOT DEFINED matlab_disabled)
+    find_package(Matlab COMPONENTS MAIN_PROGRAM)
 
-if(DEFINED matlab_disabled)
-  return()
-endif()
+    # check if Matlab >= R2019 -batch is working
+    execute_process(COMMAND ${Matlab_MAIN_PROGRAM} -batch exit
+      OUTPUT_QUIET
+      RESULT_VARIABLE _ok
+      TIMEOUT 60)  # Matlab takes a long time to start with lots of toolboxes
 
-find_package(Matlab COMPONENTS MAIN_PROGRAM)
-
-# check if Matlab >= R2019 -batch is working
-execute_process(COMMAND ${Matlab_MAIN_PROGRAM} -batch exit
-  OUTPUT_QUIET
-  RESULT_VARIABLE matlab_disabled
-  TIMEOUT 60)  # Matlab takes a long time to start with lots of toolboxes
-
-set(matlab_disabled ${matlab_disabled} CACHE STRING "0: Matlab new enough for -batch")
+    if(_ok EQUAL 0)
+      set(matlab_disabled false CACHE BOOL "0: Matlab new enough for -batch")
+    else()
+      set(matlab_disabled true)
+    endif()
+  endif()
 else()
-set(matlab_disabled true CACHE BOOL "Matlab new enough for -batch")
-endif()
+  set(matlab_disabled true)
+endif(matlab)
