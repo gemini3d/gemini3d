7f87bf19805704b30448916886874768db6f16c7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-12 01:42:23 -0400

    mute nuisance build due to gfortran 9.2.0 bug

--- cmake/compiler_gnu.cmake
+++ cmake/compiler_gnu.cmake
@@ -12,6 +12,7 @@ string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -Werror=array-bounds -fcheck=all")
 string(APPEND CMAKE_Fortran_FLAGS " -Wno-unused-dummy-argument -Wno-unused-variable -Wno-unused-function")
 
 # enforce Fortran 2018 standard
-if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
+if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8 AND
+   NOT CMAKE_Fortran_COMPILER_VERSION VERSION_EQUAL 9.2.0)
   string(APPEND CMAKE_Fortran_FLAGS " -std=f2018")
 endif()
