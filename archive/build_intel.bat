d4c0f2fe0b4d113218436ec7a05368b9ae50d52d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-12 12:08:55 -0400

    move archive/ to gemini-archive/

--- archive/build_intel.bat
+++
@@ -1,4 +0,0 @@
-REM uses Intel compiler (ifort) on Windows.
-REM assumes you're in Intel compiler environment
-
-c:\miniconda3\python compile_prereqs_intel.py --buildsys=cmake -prefix="c:/lib_intel/mumps-5.2.1" -wipe
--- archive/meson_matlab_has_batch.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-import shutil
-
-matlab = shutil.which("matlab")
-if not matlab:
-    raise ImportError("Matlab not available")
-
-subprocess.check_call([matlab, "-batch", "exit"], timeout=60)
