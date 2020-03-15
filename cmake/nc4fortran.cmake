b892f70e42cf17e9d3b2c35c6f914e7cd4758002
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-14 23:56:27 -0400

    update h5fortran and nc4fortran api

--- cmake/nc4fortran.cmake
+++ cmake/nc4fortran.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 
 FetchContent_Declare(nc4fortran_proj
   GIT_REPOSITORY https://github.com/scivision/nc4fortran.git
-  GIT_TAG v0.3.0
+  GIT_TAG v0.4.0
 )
 
 FetchContent_MakeAvailable(nc4fortran_proj)
