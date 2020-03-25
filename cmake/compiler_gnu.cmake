62e9ea14c4b36aafdce6dfe95619e8833ecd881b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-24 22:06:05 -0400

    disable GNU Wno-subscript as it's known to have false positive

--- cmake/compiler_gnu.cmake
+++ cmake/compiler_gnu.cmake
@@ -7,6 +7,9 @@ string(APPEND CMAKE_Fortran_FLAGS " -Wall -Wextra -fimplicit-none")
 
 # -Wpedantic makes too many false positives, through Gfortran 9
 
+# Wdo-subscript is known to warn on obvious non-problems
+string(APPEND CMAKE_Fortran_FLAGS " -Wno-do-subscript")
+
 string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -Werror=array-bounds -fcheck=all")
   # string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=invalid,zero,overflow")#,underflow)
 string(APPEND CMAKE_Fortran_FLAGS " -Wno-unused-dummy-argument -Wno-unused-variable -Wno-unused-function")
