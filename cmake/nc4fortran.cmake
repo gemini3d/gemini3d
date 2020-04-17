b0c63f7fbd9c5a94cf7c0aa6465be4eda55ff297
Michael Hirsch <scivision@users.noreply.github.com>
2020-04-17 15:07:43 -0400

    improve netcdf link

--- cmake/nc4fortran.cmake
+++ cmake/nc4fortran.cmake
@@ -1,19 +1,21 @@
 if(netcdf)
-include(FetchContent)
+  include(FetchContent)
 
-FetchContent_Declare(nc4fortran_proj
-  GIT_REPOSITORY https://github.com/scivision/nc4fortran.git
-  GIT_TAG v0.4.2
-)
+  FetchContent_Declare(nc4fortran_proj
+    GIT_REPOSITORY https://github.com/scivision/nc4fortran.git
+    GIT_TAG v0.4.2)
 
-FetchContent_MakeAvailable(nc4fortran_proj)
-endif()
+  FetchContent_MakeAvailable(nc4fortran_proj)
 
-if(NOT netcdf OR NOT NCDFOK)
-  message(VERBOSE "NetCDF4 nc4fortran dummy library")
+  if(NOT NCDFOK)
+    message(FATAL_ERROR "NetCDF4 was requested but is not available.")
+
+  endif()
+else(netcdf)
+  message(VERBOSE " using nc4fortran dummy")
 
   add_library(nc4fortran ${CMAKE_CURRENT_SOURCE_DIR}/src/vendor/nc4fortran/dummy.f90)
   target_include_directories(nc4fortran INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/include)
   set_target_properties(nc4fortran PROPERTIES Fortran_MODULE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/include)
   add_library(nc4fortran::nc4fortran ALIAS nc4fortran)
-endif()
+endif(netcdf)
