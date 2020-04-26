70935a323bf8b29ba3041ba5fa46254f12fa87dd
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-04-26 16:24:43 -0400

    better qsub defaults

--- src/gemini3d/templates/qsub_template.sh
+++ src/gemini3d/templates/qsub_template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
-#$ -l h_rt=12:00:00
+#$ -l h_rt=24:00:00
 
 # this is total per-node RAM
 #$ -l mem_per_core=3G
 
 # MPI
-#$ -pe mpi_8_tasks_per_node 16
+#$ -pe omp 28
 
 echo "NSLOTS = $NSLOTS"
 
