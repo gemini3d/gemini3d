2bd6e73b84ad6b772cfb5efc3dca4229291bba33
Matthew Zettergren <mattzett@gmail.com>
2020-05-16 10:44:22 -0400

    started reorganizing gemini_cfg class

--- src/io/config.f90
+++ src/io/config.f90
@@ -11,22 +11,57 @@ public :: read_configfile, gemini_cfg, get_compiler_vendor
 
 type :: gemini_cfg
 
+!> basic simulation information
 integer :: ymd(3)
 real(wp) :: UTsec0, tdur, dtout
 real(wp) :: activ(3)
 real(wp) :: tcfl
 real(wp) :: Teinf
-integer :: potsolve, flagperiodic, flagoutput, flagcap,flagdneu,flagprecfile,interptype=0
-real(wp) :: sourcemlat=0,sourcemlon=0,dtneu=0, dxn=0,drhon=0,dzn=0
+integer :: potsolve,flagperiodic,flagoutput
 logical :: nooutput = .false.
 logical :: dryrun = .false.
-real(wp) :: dtprec=0
-character(:), allocatable :: infile, outdir, &
-  indatsize,indatgrid, indatfile, sourcedir, precdir, E0dir, out_format
-integer :: flagE0file
-real(wp) :: dtE0=0
-integer :: flagglow
-real(wp) :: dtglow, dtglowout
+character(:), allocatable :: infile,outdir,indatsize,indatgrid,indatfile,out_format
+
+!> neutral atmospheric perturbations
+integer :: flagdneu                           ! whether or not to include neutral perturbations from input files
+character(:), allocatable :: sourcedir        ! where the neutral input files are located
+integer :: interptype=0                       ! assumptions to be used when interpolating neutrals:  0-cartesian 2D, 1-axisymmetric 2D, 3 - cartesian 3D
+real(wp) :: sourcemlat=0,sourcemlon=0         ! source latitude and longitude
+real(wp) :: dtneu=0                           ! time step between neutral inputs
+real(wp) :: dxn=0,drhon=0,dzn=0               ! dx,dy (or drho),dz for neutral inputs
+
+!> preciptiation file inputs
+integer :: flagprecfile                       ! whether or not we have precipitation input from a file
+character(:), allocatable :: precdir          ! location of precipitation input data
+real(wp) :: dtprec=0                          ! time step between precipitation inputs
+
+!> electric field file inputs
+integer :: flagE0file                         ! whether or not to have electric field file input
+character(:), allocatable :: E0dir            ! location of electric field input data
+real(wp) :: dtE0=0                            ! time step between electric field inputs
+
+!> GLOW parameters
+integer :: flagglow              ! whether or not to use glow to compute impact ionization
+real(wp) :: dtglow, dtglowout    ! time step between GLOW updates and outputs for GLOW emissions
+
+!> equatorial ionization anomaly
+logical :: flagEIA=.false.       ! whether or not to include and equatorial ionization anomaly in simulation
+real(wp) :: v0equator=10._wp      ! max vertical drift of plasma at equator for EIA
+
+!> varying neutral atmosphere background
+logical :: flagneuBG=.false.                ! whether or not to allow MSIS to be called to update neutral background
+real(wp) :: dtneuBG=900._wp                  ! approximate time between MSIS calls
+
+!> background preciptation
+real(wp) :: PhiWBG=0.05                      ! background total energy flux in mW/m^2  
+real(wp) :: W0BG=3e3_wp                      ! background characteristic energy for precipitation
+
+!> parallel current calculations
+logical :: flagJpar=.true.                  ! whether or not to compute parallel current (some simulation setups will give really poor results)
+
+!> inertial capacitance
+integer :: flagcap               ! use inertial capacitance? 0 - set all to zero, 1 - use ionosphere to compute, 2 - add a magnetospheric part
+real(wp) :: magcap=30._wp        ! value of integrated magnetospheric capacitance to use 
 
 end type gemini_cfg
 
