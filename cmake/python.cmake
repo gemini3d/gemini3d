b39ad6678df6feadd6248c490c70a49eccab0130
Michael Hirsch <scivision@users.noreply.github.com>
2020-05-19 13:32:48 -0400

    warn more vigorously if PyGemini not installed

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -2,7 +2,7 @@
 find_package(Python3 COMPONENTS Interpreter)
 
 # Python3::Interpreter does NOT work, use ${Python3_EXECUTABLE}
-if(NOT DEFINED python_ok)
+if(NOT python_ok)
 
 # __path__ is always iterable: https://docs.python.org/3/reference/import.html#__path__
 execute_process(COMMAND ${Python3_EXECUTABLE} -c "import gemini3d; print(gemini3d.__path__[0])"
@@ -16,7 +16,7 @@ if(python_ok EQUAL 0)
   set(python_ok true CACHE BOOL "PyGemini OK")
 else()
   set(python_ok false)
-  message(STATUS "MISSING: PyGemini -> install by 'python3 setup.py --user develop'")
+  message(WARNING "MISSING: PyGemini -> install by 'python3 setup.py --user develop'")
 endif()
 
-endif(NOT DEFINED python_ok)
+endif(NOT python_ok)
