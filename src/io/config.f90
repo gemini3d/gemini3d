5ea30a249662a94a9ede045d4b24df057a011936
Michael Hirsch <scivision@users.noreply.github.com>
2020-05-26 14:36:57 -0400

    make config_nml.f90 submodule

--- src/io/config.f90
+++ src/io/config.f90
@@ -30,6 +30,14 @@ real(wp) :: dtglow, dtglowout
 
 end type gemini_cfg
 
+
+interface
+module subroutine read_nml(cfg)
+class(gemini_cfg), intent(inout) :: cfg
+end subroutine read_nml
+end interface
+
+
 character(:), allocatable :: compiler_vendor
 
 contains
@@ -55,126 +63,6 @@ endif
 
 end subroutine read_configfile
 
-
-subroutine read_nml(cfg)
-
-class(gemini_cfg), intent(inout) :: cfg
-
-integer :: u, i
-
-integer :: ymd(3)
-real(wp) :: UTsec0
-real(wp) :: tdur
-real(wp) :: dtout
-real(wp) :: activ(3)
-real(wp) :: tcfl
-real(wp) :: Teinf
-integer :: potsolve, flagperiodic=0, flagoutput, flagcap=0
-integer :: flagdneu=0
-integer :: interptype
-real(wp) :: sourcemlat,sourcemlon
-real(wp) :: dtneu
-real(wp) :: dxn,drhon,dzn
-integer :: flagprecfile=0
-real(wp) :: dtprec=0
-character(256) :: indat_size, indat_grid, indat_file, source_dir, prec_dir, E0_dir
-character(4) :: file_format=""  !< need to initialize blank or random invisible fouls len_trim>0
-integer :: flagE0file=0
-real(wp) :: dtE0=0
-integer :: flagglow=0
-real(wp) :: dtglow=0, dtglowout=0
-
-namelist /base/ ymd, UTsec0, tdur, dtout, activ, tcfl, Teinf
-namelist /files/ file_format, indat_size, indat_grid, indat_file
-namelist /flags/ potsolve, flagperiodic, flagoutput, flagcap, flagdneu, flagprecfile, flagE0file, flagglow
-namelist /neutral_perturb/ interptype, sourcemlat, sourcemlon, dtneu, dxn, drhon, dzn, source_dir
-namelist /precip/ dtprec, prec_dir
-namelist /efield/ dtE0, E0_dir
-namelist /glow/ dtglow, dtglowout
-
-open(newunit=u, file=cfg%infile, status='old', action='read')
-
-read(u, nml=base, iostat=i)
-call check_nml_io(i, cfg%infile, "base", compiler_vendor)
-cfg%ymd = ymd
-cfg%UTsec0 = UTsec0
-cfg%tdur = tdur
-cfg%dtout = dtout
-cfg%activ = activ
-cfg%tcfl = tcfl
-cfg%Teinf = Teinf
-
-read(u, nml=flags, iostat=i)
-call check_nml_io(i, cfg%infile, "flags", compiler_vendor)
-cfg%potsolve = potsolve
-cfg%flagperiodic = flagperiodic
-cfg%flagoutput = flagoutput
-cfg%flagcap = flagcap
-cfg%flagdneu = flagdneu
-cfg%flagprecfile = flagprecfile
-cfg%flagE0file = flagE0file
-cfg%flagglow = flagglow
-
-read(u, nml=files, iostat=i)
-call check_nml_io(i, cfg%infile, "files", compiler_vendor)
-
-!> auto file_format if not specified
-if (len_trim(file_format) > 0) then
-  cfg%out_format = trim(file_format)
-else
-  file_format = get_suffix(indat_size)
-  cfg%out_format = file_format(2:)
-endif
-
-cfg%indatsize = expanduser(indat_size)
-cfg%indatgrid = expanduser(indat_grid)
-cfg%indatfile = expanduser(indat_file)
-
-if (cfg%flagdneu == 1) then
-  read(u, nml=neutral_perturb, iostat=i)
-  call check_nml_io(i, cfg%infile, "neutral_perturb", compiler_vendor)
-  cfg%sourcedir = expanduser(source_dir)
-  cfg%interptype = interptype
-  cfg%sourcemlat = sourcemlat
-  cfg%sourcemlon = sourcemlon
-  cfg%dtneu = dtneu
-  cfg%drhon = drhon
-  cfg%dzn = dzn
-  cfg%dxn = dxn
-else
-  cfg%sourcedir = ""
-endif
-
-
-if (cfg%flagprecfile == 1) then
-  read(u, nml=precip, iostat=i)
-  call check_nml_io(i, cfg%infile, "precip", compiler_vendor)
-  cfg%precdir = expanduser(prec_dir)
-  cfg%dtprec = dtprec
-else
-  cfg%precdir = ""
-endif
-
-if (cfg%flagE0file == 1) then
-  read(u, nml=efield, iostat=i)
-  call check_nml_io(i, cfg%infile, "efield", compiler_vendor)
-  cfg%E0dir = expanduser(E0_dir)
-  cfg%dtE0 = dtE0
-else
-  cfg%E0dir = ""
-endif
-
-if (cfg%flagglow == 1) then
-  read(u, nml=glow, iostat=i)
-  call check_nml_io(i, cfg%infile, "glow", compiler_vendor)
-  cfg%dtglow = dtglow
-  cfg%dtglowout = dtglowout
-endif
-
-
-close(u)
-
-end subroutine read_nml
 
 
 subroutine read_ini(cfg)
@@ -230,7 +118,7 @@ if (cfg%flagprecfile==1) then
 !! get the location of the precipitation input files
   read(u,*, iostat=i) cfg%dtprec
   read(u,'(A256)', iostat=i) buf
-  call check_nml_io(i, cfg%infile)
+  call check_ini_io(i, cfg%infile)
   cfg%precdir = expanduser(buf)
 else
   cfg%precdir = ""
@@ -253,62 +141,12 @@ call check_ini_io(i, cfg%infile)
 if (cfg%flagglow==1) then
   read(u,*, iostat=i) cfg%dtglow
   read(u,*, iostat=i) cfg%dtglowout
-  call check_nml_io(i, cfg%infile)
+  call check_ini_io(i, cfg%infile)
 end if
 
 close(u)
 
 end subroutine read_ini
-
-
-subroutine check_nml_io(i, filename, group, vendor)
-!! checks for EOF and gives helpful error
-!! this accomodates non-Fortran 2018 error stop with variable character
-
-integer, intent(in) :: i
-character(*), intent(in) :: filename
-character(*), intent(in), optional :: group, vendor
-
-character(:), allocatable :: grp, msg
-
-if(i==0) return
-
-grp = ""
-if(present(group)) grp = group
-
-if (is_iostat_end(i)) then
-  write(stderr,*) 'ERROR: group ' // grp // ': ensure there is a trailing blank line in ' // filename
-  write(stderr,*) 'also, ensure that Namelist groups are read in order: base, flags, files etc'
-  error stop 5
-endif
-
-msg = ""
-if (present(vendor)) then
-  select case (vendor)
-  case ("Intel")
-    !! https://software.intel.com/en-us/fortran-compiler-developer-guide-and-reference-list-of-run-time-error-messages
-    select case (i)
-    case (19)
-      msg = "mismatch between variable names in namelist and Fortran code, or problem in variable specification in file"
-    case (623)
-      msg = "variable specified in Fortran code missing from Namelist file"
-    case (17,18,624,625,626,627,628,680,750,759)
-      msg = "namelist file format problem"
-    end select
-  case ("GCC")
-    select case (i)
-    case (5010)
-      msg = "mismatch between variable names in namelist and Fortran code, or problem in variable specification in file"
-    end select
-  end select
-endif
-
-if (len(msg)==0) write(stderr,*) "namelist read error code",i
-
-write(stderr,'(A,/,A)') 'ERROR: reading ' // grp // " " // filename, msg
-error stop 5
-
-end subroutine check_nml_io
 
 
 subroutine check_ini_io(i, filename)
