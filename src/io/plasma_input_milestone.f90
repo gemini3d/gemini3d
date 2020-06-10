1caca1a462bfac94b0681a57cff3c56697fd8a01
Matthew Zettergren <mattzett@gmail.com>
2020-06-10 14:09:22 -0400

    milestone search routines compiling

--- src/io/plasma_input_milestone.f90
+++ src/io/plasma_input_milestone.f90
@@ -1,24 +1,17 @@
 submodule (io:plasma_input) plasma_input_milestone
 
 use timeutils, only: date_filename,dateinc
+use h5fortran, only: hdf5_file 
 
 implicit none (type,external)   !! external procedures must be explicitly denoted thusly
 
 contains
 
-module procedure find_lastfile(ymd0,UTsec0,ymdtarget,UTsectarget,cadence,ymd,UTsec)
+module procedure find_lastfile
 
 !> Compute the last file before the target date based on a start date and cadence.  The
 !  file is assumed to exist and programmer needs to check for existence outside this 
 !  procedure.
-
-integer, dimension(3), intent(in) :: ymd0
-real(wp), intent(in) :: UTsec0
-integer, dimension(3), intent(:) :: ymdtarget
-real(wp), intent(in) :: UTsectarget
-real(wp), intent(in) :: cadence
-integer, dimension(3), intent(out) :: ymd
-real(wp), intent(out) :: UTsec
 
 integer, dimension(3) :: ymdnext
 real(wp) :: UTsecnext
@@ -41,44 +34,35 @@ end do
 end procedure find_lastfile
 
 
-module procedure find_milestone(path,suffix,ymd0,UTsec0,ymdmile,UTsecmile,filemile)
+module procedure find_milestone
 
 !> search path having output rate cadence (s) and find the
 !  last file that is a milestone.
-
-character, dimension(:), intent(in) :: path
-character, dimension(:), intent(in) :: suffix
-integer, dimension(3), intent(in) :: ymd0
-real(wp), intent(in) :: UTsec0
-
-integer, dimension(3), intent(out) :: ymdmile
-real(wp), intent(out) :: UTsecmeil
-character, dimension(:), allocatable, intent(out) :: filemile
 
 integer, dimension(3) :: ymd
 real(wp) :: UTsec
 character(:), allocatable :: fn
 type(hdf5_file) :: h5f
-logical flagend,flagmilestone
+logical flagexists,flagend,flagmilestone
 
 
 ymd=ymd0
 UTsec=UTsec0
 ymdmile=ymd0
 UTsecmile=UTsec0
-filemile=date_filename(ymd0,UTsec0,path)   !! This presumes the file output is a milestone
+filemile=date_filename(path,ymd0,UTsec0)   !! This presumes the file output is a milestone
 flagend=.false.
 do while ( .not.(flagend) )
   !! new filename
-  fn=date_filename(ymd,UTsec,path)
-  fn=fn//suffix
+  fn=date_filename(path,ymd,UTsec)
+  fn=fn // suffix
 
   !! is the file there
   inquire(file=fn, exist=flagexists)
   if ( flagexists ) then
     call h5f%initialize(fn, status='old', action='r')    ! init hdf5 object instance
-    flagmilestone=h5f%exists(fn,'/nsall')
-    call hdf5%finalize()    ! destruct hdf5 object instance so it can be reused
+    flagmilestone=h5f%exists('/nsall')
+    call h5f%finalize()    ! destruct hdf5 object instance so it can be reused
     if (flagmilestone) then
       ymdmile=ymd
       UTsecmile=UTsec
