dc9a2fe36846d35ebb3d0796cb54b5f7a3a512df
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-11 08:35:08 -0400

    compile_prereqs.py: HDF5: use Git instead of Tar.
    Also, give much better user feedback for getting HDF5 on Windows

--- scripts/compile_prereqs_gcc.py
+++ scripts/compile_prereqs_gcc.py
@@ -24,21 +24,20 @@ NETCDF_C_GIT = "https://github.com/Unidata/netcdf-c.git"
 NETCDF_FORTRAN = "4.5.2"
 NETCDF_FORTRAN_GIT = "https://github.com/Unidata/netcdf-fortran.git"
 
-HDF5VERSION = "1.12.0"
-HDF5URL = f"https://zenodo.org/record/3700903/files/hdf5-{HDF5VERSION}.tar.bz2?download=1"
-HDF5MD5 = "1fa68c4b11b6ef7a9d72ffa55995f898"
+HDF5_TAG = "1.12/master"
+HDF5_GIT = "https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git"
 
 MPIVERSION = "3.1.6"  # OpenMPI 4 needs Scalapack 2.1
 MPISHA1 = "bc4cd7fa0a7993d0ae05ead839e6056207e432d4"
 
-LAPACKGIT = "https://github.com/scivision/lapack.git"
-LAPACKDIR = "lapack"
+LAPACK_GIT = "https://github.com/scivision/lapack.git"
+LAPACK_DIR = "lapack"
 
-SCALAPACKGIT = "https://github.com/scivision/scalapack.git"
-SCALAPACKDIR = "scalapack"
+SCALAPACK_GIT = "https://github.com/scivision/scalapack.git"
+SCALAPACK_DIR = "scalapack"
 
-MUMPSGIT = "https://github.com/scivision/mumps.git"
-MUMPSDIR = "mumps"
+MUMPS_GIT = "https://github.com/scivision/mumps.git"
+MUMPS_DIR = "mumps"
 
 # ========= end of user parameters ================
 
@@ -56,7 +55,7 @@ def netcdf_c(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None, wipe: boo
     if not env:
         env = get_compilers()
 
-    update(source_dir, NETCDF_C_GIT, f"v{NETCDF_C}")
+    git_update(source_dir, NETCDF_C_GIT, f"v{NETCDF_C}")
 
     c_args = [
         f"-DCMAKE_INSTALL_PREFIX:PATH={install_dir}",
@@ -87,7 +86,7 @@ def netcdf_fortran(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None, wip
     if not env:
         env = get_compilers()
 
-    update(source_dir, NETCDF_FORTRAN_GIT, f"v{NETCDF_FORTRAN}")
+    git_update(source_dir, NETCDF_FORTRAN_GIT, f"v{NETCDF_FORTRAN}")
 
     # NetCDF-Fortran does not yet use NetCDF_ROO
     if sys.platform == "linux":
@@ -122,34 +121,52 @@ def netcdf_fortran(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None, wip
 
 
 def hdf5(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None):
-    """ build and install HDF5 """
-    if os.name == "nt":
-        raise NotImplementedError(
-            "Please use binaries from HDF Group for Windows appropriate for your compiler."
-        )
-
-    hdf5_dir = f"hdf5-{HDF5VERSION}"
-    install_dir = dirs["prefix"] / hdf5_dir
-    source_dir = dirs["workdir"] / hdf5_dir
-
-    tarfn = dirs["workdir"] / f"hdf5-{HDF5VERSION}.tar.bz2"
-    gemini3d.url_retrieve(HDF5URL, tarfn, ("md5", HDF5MD5))
-    gemini3d.extract_tar(tarfn, source_dir)
-
+    """ build and install HDF5
+    some systems have broken libz and so have trouble extracting tar.bz2 from Python.
+    To avoid this, we git clone the release instead.
+    """
     if not env:
         env = get_compilers()
 
-    cmd = nice + [
+    if os.name == "nt":
+        if "ifort" in env["FC"]:
+            msg = """
+For Windows with Intel compiler, use HDF5 binaries from HDF Group.
+https://www.hdfgroup.org/downloads/hdf5/
+look for filename like hdf5-1.12.0-Std-win10_64-vs14-Intel.zip
+            """
+        elif "gfortran" in env["FC"]:
+            msg = """
+For MSYS2 on Windows, just use MSYS2 HDF5.
+Install from the MSYS2 terminal like:
+pacman -S mingw-w64-x86_64-hdf5
+reference: https://packages.msys2.org/package/mingw-w64-x86_64-hdf5
+            """
+        else:
+            msg = """
+For Windows, use HDF5 binaries from HDF Group.
+https://www.hdfgroup.org/downloads/hdf5/
+Instead of this, it is generally best to use MSYS2 or Windows Subsystem for Linux
+            """
+        raise SystemExit(msg)
+
+    hdf5_name = "hdf5"
+    install_dir = dirs["prefix"] / hdf5_name
+    source_dir = dirs["workdir"] / hdf5_name
+
+    git_update(source_dir, HDF5_GIT, tag=HDF5_TAG)
+
+    cmd = [
         "./configure",
         f"--prefix={install_dir}",
         "--enable-fortran",
         "--enable-build-mode=production",
     ]
 
-    subprocess.check_call(cmd, cwd=source_dir, env=env)
+    subprocess.check_call(nice + cmd, cwd=source_dir, env=env)
 
-    cmd = nice + ["make", "-C", str(source_dir), "-j", str(NJOBS), "install"]
-    subprocess.check_call(cmd)
+    cmd = ["make", "-C", str(source_dir), "-j", str(NJOBS), "install"]
+    subprocess.check_call(nice + cmd)
 
 
 def openmpi(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None):
@@ -172,7 +189,7 @@ def openmpi(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None):
     if not env:
         env = get_compilers()
 
-    cmd = nice + [
+    cmd = [
         "./configure",
         f"--prefix={install_dir}",
         f"CC={env['CC']}",
@@ -180,19 +197,19 @@ def openmpi(dirs: T.Dict[str, Path], env: T.Mapping[str, str] = None):
         f"FC={env['FC']}",
     ]
 
-    subprocess.check_call(cmd, cwd=source_dir, env=env)
+    subprocess.check_call(nice + cmd, cwd=source_dir, env=env)
 
-    cmd = nice + ["make", "-C", str(source_dir), "-j", str(NJOBS), "install"]
-    subprocess.check_call(cmd)
+    cmd = ["make", "-C", str(source_dir), "-j", str(NJOBS), "install"]
+    subprocess.check_call(nice + cmd)
 
 
 def lapack(wipe: bool, dirs: T.Dict[str, Path], buildsys: str, env: T.Mapping[str, str] = None):
     """ build and insall Lapack """
-    install_dir = dirs["prefix"] / LAPACKDIR
-    source_dir = dirs["workdir"] / LAPACKDIR
+    install_dir = dirs["prefix"] / LAPACK_DIR
+    source_dir = dirs["workdir"] / LAPACK_DIR
     build_dir = source_dir / BUILDDIR
 
-    update(source_dir, LAPACKGIT)
+    git_update(source_dir, LAPACK_GIT)
 
     if not env:
         env = get_compilers()
@@ -209,18 +226,18 @@ def lapack(wipe: bool, dirs: T.Dict[str, Path], buildsys: str, env: T.Mapping[st
 
 def scalapack(wipe: bool, dirs: T.Dict[str, Path], buildsys: str, env: T.Mapping[str, str] = None):
     """ build and install Scalapack """
-    source_dir = dirs["workdir"] / SCALAPACKDIR
+    source_dir = dirs["workdir"] / SCALAPACK_DIR
     build_dir = source_dir / BUILDDIR
 
-    update(source_dir, SCALAPACKGIT)
+    git_update(source_dir, SCALAPACK_GIT)
 
-    lib_args = [f'-DLAPACK_ROOT={dirs["prefix"] / LAPACKDIR}']
+    lib_args = [f'-DLAPACK_ROOT={dirs["prefix"] / LAPACK_DIR}']
 
     if not env:
         env = get_compilers()
 
     if buildsys == "cmake":
-        args = [f"-DCMAKE_INSTALL_PREFIX:PATH={dirs['prefix'] / SCALAPACKDIR}"]
+        args = [f"-DCMAKE_INSTALL_PREFIX:PATH={dirs['prefix'] / SCALAPACK_DIR}"]
         cmake_build(args + lib_args, source_dir, build_dir, wipe, env=env)
     elif buildsys == "meson":
         args = [f"--prefix={dirs['prefix']}"]
@@ -231,14 +248,14 @@ def scalapack(wipe: bool, dirs: T.Dict[str, Path], buildsys: str, env: T.Mapping
 
 def mumps(wipe: bool, dirs: T.Dict[str, Path], buildsys: str, env: T.Mapping[str, str] = None):
     """ build and install Mumps """
-    install_dir = dirs["prefix"] / MUMPSDIR
-    source_dir = dirs["workdir"] / MUMPSDIR
+    install_dir = dirs["prefix"] / MUMPS_DIR
+    source_dir = dirs["workdir"] / MUMPS_DIR
     build_dir = source_dir / BUILDDIR
 
-    scalapack_lib = dirs["prefix"] / SCALAPACKDIR
-    lapack_lib = dirs["prefix"] / LAPACKDIR
+    scalapack_lib = dirs["prefix"] / SCALAPACK_DIR
+    lapack_lib = dirs["prefix"] / LAPACK_DIR
 
-    update(source_dir, MUMPSGIT)
+    git_update(source_dir, MUMPS_GIT)
 
     if env and env["FC"] == "ifort":
         lib_args = []
@@ -328,7 +345,7 @@ def cmake_minimum_version(min_version: str = None) -> str:
     return cmake
 
 
-def update(path: Path, repo: str, tag: str = None):
+def git_update(path: Path, repo: str, tag: str = None):
     """
     Use Git to update a local repo, or clone it if not already existing.
 
@@ -337,16 +354,29 @@ def update(path: Path, repo: str, tag: str = None):
     GITEXE = shutil.which("git")
 
     if not GITEXE:
-        logging.warning("Git not available, cannot check for library updates")
+        logging.error("Git not available.")
         return
 
     if path.is_dir():
-        subprocess.run([GITEXE, "-C", str(path), "pull"])
+        subprocess.check_call([GITEXE, "-C", str(path), "pull"])
     else:
-        subprocess.run([GITEXE, "clone", repo, str(path)])
-
-    if tag:
-        subprocess.run([GITEXE, "-C", str(path), "checkout", tag])
+        # shallow clone
+        if tag:
+            subprocess.check_call(
+                [
+                    GITEXE,
+                    "clone",
+                    repo,
+                    "--depth",
+                    "1",
+                    "--branch",
+                    tag,
+                    "--single-branch",
+                    str(path),
+                ]
+            )
+        else:
+            subprocess.check_call([GITEXE, "clone", repo, "--depth", "1", str(path)])
 
 
 def get_compilers() -> T.Mapping[str, str]:
