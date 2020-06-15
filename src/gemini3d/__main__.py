252d0a82f3fb7daddac5ee10879e4226fab6636a
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-15 02:35:06 -0400

    gemini_setup entry point

--- src/gemini3d/__main__.py
+++ src/gemini3d/__main__.py
@@ -9,6 +9,16 @@ from time import monotonic
 
 from .job import runner
 from .compare import compare_all
+from .model_setup import model_setup
+
+
+def gemini_setup():
+    p = argparse.ArgumentParser()
+    p.add_argument("config_file", help="path to config*.nml file")
+    p.add_argument("out_dir", help="simulation output directory")
+    P = p.parse_args()
+
+    model_setup(P.config_file, P.out_dir)
 
 
 def compare_run():
