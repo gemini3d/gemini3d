32d25400122c597f2cdc1c673bc54ccf8608e476
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-17 15:31:02 -0400

    pygemini: more feedback if install trouble

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -6,8 +6,6 @@ function(check_pygemini)
 
 # __path__ is always iterable: https://docs.python.org/3/reference/import.html#__path__
 
-find_package(Python3 COMPONENTS Interpreter)
-
 execute_process(COMMAND ${Python3_EXECUTABLE} -c "import gemini3d; print(gemini3d.__path__[0])"
   OUTPUT_VARIABLE PYGEMINI_DIR
   OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -15,13 +13,20 @@ execute_process(COMMAND ${Python3_EXECUTABLE} -c "import gemini3d; print(gemini3
   TIMEOUT 15)
 
 if(_ok EQUAL 0)
-  message(STATUS "PyGemini: ${PYGEMINI_DIR}")
+  message(STATUS "PyGemini found: ${PYGEMINI_DIR}")
   set(python_ok true CACHE BOOL "PyGemini OK")
 endif()
 
 endfunction()
 
 # --- script
+find_package(Python3 COMPONENTS Interpreter)
+if(NOT Python3_FOUND)
+  message(WARNING "Python not found, cannot use PyGemini.")
+  return()
+endif()
+# keep this in script so it's not scoped in function
+
 set(pygemini_dir "${PROJECT_SOURCE_DIR}/../pygemini/")
 set(pygemini_url "https://github.com/gemini3d/pygemini")
 
@@ -32,11 +37,22 @@ endif()
 if(NOT python_ok)
   clone_if_missing(${pygemini_dir} ${pygemini_url})
 
-  execute_process(COMMAND ${Python3_EXECUTABLE} setup.py --user develop
+  if(NOT EXISTS ${pygemini_dir}/setup.py)
+    message(WARNING "Could not find PyGemini setup.py in ${pygemini_dir}")
+    return()
+  endif()
+
+  execute_process(COMMAND ${Python3_EXECUTABLE} setup.py develop --user
     WORKING_DIRECTORY ${pygemini_dir}
+    OUTPUT_VARIABLE _log
+    ERROR_VARIABLE _log
     RESULT_VARIABLE _ok)
-  if(NOT _ok EQUAL 0)
-    message(WARNING "Problem installing PyGemini. Perhaps try doing this manually: ${pygemini_url}")
+
+  if(_ok EQUAL 0)
+    message(STATUS "Setup PyGemini in ${pygemini_dir}")
+  else()
+    message(STATUS "${_ok} ${_log}")
+    message(WARNING "Problem installing PyGemini with ${Python3_EXECUTABLE} in ${pygemini_dir}  Perhaps try doing this manually: ${pygemini_url}")
     return()
   endif()
 
