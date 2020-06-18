22e749688ea8c552a4f5ce6eca1edd6f5ace843d
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-17 23:14:26 -0400

    ctest: set dryrun properties

--- cmake/test_setup.cmake
+++ cmake/test_setup.cmake
@@ -7,8 +7,17 @@ set(_outdir ${CMAKE_CURRENT_BINARY_DIR}/test${TESTNAME})
 
 if(python_ok AND hdf5)
 
+  win32_hdf5_env()
+
   add_test(NAME gemini:hdf5:${TESTNAME}:dryrun
     COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${TESTNAME} ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir} -dryrun)
+
+  set_tests_properties(gemini:hdf5:${TESTNAME}:dryrun PROPERTIES
+    TIMEOUT 60
+    SKIP_RETURN_CODE 77
+    RUN_SERIAL true
+    FIXTURES_SETUP hdf5:${TESTNAME}:dryrun)
+
 
   add_test(NAME gemini:hdf5:${TESTNAME}
     COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${TESTNAME} ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir})
@@ -18,14 +27,19 @@ if(python_ok AND hdf5)
     TIMEOUT ${TIMEOUT}
     SKIP_RETURN_CODE 77
     RUN_SERIAL true
-    DEPENDS gemini:hdf5:${TESTNAME}:dryrun
-    FIXTURES_REQUIRED MPIMUMPS)
-  win32_env(gemini:${TESTNAME})
+    FIXTURES_REQUIRED "hdf5:${TESTNAME}:dryrun;MPIMUMPS")
+
 endif()
 
 if(python_ok AND netcdf)
   add_test(NAME gemini:netcdf:${TESTNAME}:dryrun
     COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${TESTNAME} ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir} -out_format nc -dryrun)
+
+  set_tests_properties(gemini:netcdf:${TESTNAME}:dryrun PROPERTIES
+    TIMEOUT 60
+    SKIP_RETURN_CODE 77
+    RUN_SERIAL true
+    FIXTURES_SETUP netcdf:${TESTNAME}:dryrun)
 
   add_test(NAME gemini:netcdf:${TESTNAME}
     COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${TESTNAME} ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir} -out_format nc)
@@ -34,8 +48,7 @@ if(python_ok AND netcdf)
     TIMEOUT ${TIMEOUT}
     SKIP_RETURN_CODE 77
     RUN_SERIAL true
-    DEPENDS gemini:netcdf:${TESTNAME}:dryrun
-    FIXTURES_REQUIRED MPIMUMPS)
+    FIXTURES_REQUIRED "netcdf:${TESTNAME}:dryrun;MPIMUMPS")
 endif()
 
 
