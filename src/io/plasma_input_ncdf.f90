c61b51751527d9458d0523df17ff0a141d1c7078
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-13 02:50:11 -0400

    phiall 1d or 2d

--- src/io/plasma_input_ncdf.f90
+++ src/io/plasma_input_ncdf.f90
@@ -14,7 +14,7 @@ character(:), allocatable :: filenamefull
 real(wp), dimension(:,:,:), allocatable :: J1all,J2all,J3all
 real(wp), dimension(:,:,:), allocatable :: tmpswap
 
-type(netcdf_file) :: h5
+type(netcdf_file) :: hf
 
 !>  CHECK TO MAKE SURE WE ACTUALLY HAVE THE DATA WE NEED TO DO THE MAG COMPUTATIONS.
 if (flagoutput==3) error stop 'Need current densities in the output to compute magnetic fields'
@@ -24,29 +24,29 @@ if (flagoutput==3) error stop 'Need current densities in the output to compute m
 filenamefull = date_filename(outdir,ymd,UTsec) // '.nc'
 print *, 'Input file name for current densities:  ', filenamefull
 
-call h5%initialize(filenamefull, status='old', action='r')
+call hf%initialize(filenamefull, status='old', action='r')
 
 !> LOAD THE DATA
 !> PERMUTE THE ARRAYS IF NECESSARY
 allocate(J1all(lx1,lx2all,lx3all),J2all(lx1,lx2all,lx3all),J3all(lx1,lx2all,lx3all))
 if (flagswap==1) then
   allocate(tmpswap(lx1,lx3all,lx2all))
-  call h5%read('J1all', tmpswap)
+  call hf%read('J1all', tmpswap)
   J1all = reshape(tmpswap,[lx1,lx2all,lx3all],order=[1,3,2])
-  call h5%read('J2all', tmpswap)
+  call hf%read('J2all', tmpswap)
   J2all = reshape(tmpswap,[lx1,lx2all,lx3all],order=[1,3,2])
-  call h5%read('J3all', tmpswap)
+  call hf%read('J3all', tmpswap)
   J3all = reshape(tmpswap,[lx1,lx2all,lx3all],order=[1,3,2])
+  deallocate(tmpswap)
 else
   !! no need to permute dimensions for 3D simulations
-  call h5%read('J1all', J1all)
-  call h5%read('J2all', J2all)
-  call h5%read('J3all', J3all)
+  call hf%read('J1all', J1all)
+  call hf%read('J2all', J2all)
+  call hf%read('J3all', J3all)
 end if
 print *, 'Min/max current data:  ',minval(J1all),maxval(J1all),minval(J2all),maxval(J2all),minval(J3all),maxval(J3all)
 
-call h5%finalize()
-
+call hf%finalize()
 
 !> DISTRIBUTE DATA TO WORKERS AND TAKE A PIECE FOR ROOT
 call bcast_send(J1all,tag%J1,J1)
@@ -64,16 +64,18 @@ module procedure input_root_mpi_nc4
 !! TO GFORTRAN BUG WHICH DISALLOWS 8 BYTE INTEGER RECORD
 !! LENGTHS.
 
-type(netcdf_file) :: h5
+type(netcdf_file) :: hf
 
 integer :: lx1,lx2,lx3,lx2all,lx3all,isp
+integer :: ix1
 
 real(wp), dimension(-1:size(x1,1)-2,-1:size(x2all,1)-2,-1:size(x3all,1)-2,1:lsp) :: nsall, vs1all, Tsall
 integer :: lx1in,lx2in,lx3in,u, utrace
 real(wp) :: tin
 real(wp), dimension(3) :: ymdtmp
-
 real(wp) :: tstart,tfin
+real(wp), dimension(:,:), allocatable :: Phislab
+real(wp), allocatable :: tmp(:,:,:,:)
 
 !> so that random values (including NaN) don't show up in Ghost cells
 nsall = 0
@@ -89,6 +91,10 @@ lx2=size(ns,2)-4
 lx3=size(ns,3)-4
 lx2all=size(x2all)-4
 lx3all=size(x3all)-4
+
+
+allocate(Phislab(1:lx2all,1:lx3all))  !space to store EFL potential
+
 
 !> READ IN FROM FILE, AS OF CURVILINEAR BRANCH THIS IS NOW THE ONLY INPUT OPTION
 call get_simsize3(indatsize, lx1in, lx2in, lx3in)
@@ -106,37 +112,43 @@ if (.not. (lx1==lx1in .and. lx2all==lx2in .and. lx3all==lx3in)) then
        '- use a script to interpolate up/down to the simulation grid'
 end if
 
-call h5%initialize(indatfile, status='old', action='r')
+call hf%initialize(indatfile, status='old', action='r')
 
 if (flagswap==1) then
-  block
-  !> NOTE: workaround for intel 2020 segfault--may be a compiler bug
-  !real(wp) :: tmp(lx1,lx3all,lx2all,lsp)
-  real(wp), allocatable :: tmp(:,:,:,:)
   allocate(tmp(lx1,lx3all,lx2all,lsp))
-  !! end workaround
 
-  call h5%read('ns', tmp)
+  call hf%read('nsall', tmp)
   nsall(1:lx1,1:lx2all,1:lx3all,1:lsp) = reshape(tmp,[lx1,lx2all,lx3all,lsp],order=[1,3,2,4])
-  call h5%read('vsx1', tmp)
+  call hf%read('vs1all', tmp)
   vs1all(1:lx1,1:lx2all,1:lx3all,1:lsp) = reshape(tmp,[lx1,lx2all,lx3all,lsp],order=[1,3,2,4])
-  call h5%read('Ts', tmp)
+  call hf%read('Tsall', tmp)
   Tsall(1:lx1,1:lx2all,1:lx3all,1:lsp) = reshape(tmp,[lx1,lx2all,lx3all,lsp],order=[1,3,2,4])
   !! permute the dimensions so that 2D runs are parallelized
-  end block
+  if (hf%exist('Phiall')) then
+    call hf%read('Phiall',tmpPhi)
+    Phislab=reshape(tmpPhi,[lx2all,lx3all],order=[2,1])
+  else
+    Phislab = 0
+  end if
+
+  deallocate(tmp)
 else
-  call h5%read('ns', nsall(1:lx1,1:lx2all,1:lx3all,1:lsp))
-  call h5%read('vsx1', vs1all(1:lx1,1:lx2all,1:lx3all,1:lsp))
-  call h5%read('Ts', Tsall(1:lx1,1:lx2all,1:lx3all,1:lsp))
+  call hf%read('nsall', nsall(1:lx1,1:lx2all,1:lx3all,1:lsp))
+  call hf%read('vs1all', vs1all(1:lx1,1:lx2all,1:lx3all,1:lsp))
+  call hf%read('Tsall', Tsall(1:lx1,1:lx2all,1:lx3all,1:lsp))
+  if (hf%exist('Phiall')) then
+    call hf%read('Phiall', Phislab)
+  else
+    Phislab = 0
+  end if
 end if
 
-call h5%finalize()
+call hf%finalize()
 
-
-!> Don't support restarting potential right now for nc4
-Phiall=0._wp
-Phi=0._wp
-
+!> Apply EFL approx to compute full grid potential
+do ix1=1,lx1
+  Phiall(ix1,1:lx2all,1:lx3all)=Phislab(1:lx2all,1:lx3all)
+end do
 
 !> ROOT BROADCASTS IC DATA TO WORKERS
 call cpu_time(tstart)
@@ -145,9 +157,10 @@ call bcast_send(vs1all,tag%vs1,vs1)
 call bcast_send(Tsall,tag%Ts,Ts)
 call bcast_send(Phiall,tag%Phi,Phi)
 call cpu_time(tfin)
-print '(A,ES15.6,A)', 'Sent ICs to workers in', tfin-tstart, ' seconds.'
+print '(A,ES12.3,A)', 'Sent ICs to workers in', tfin-tstart, ' seconds.'
+
+deallocate(Phislab)
 
 end procedure input_root_mpi_nc4
-
 
 end submodule plasma_input_nc4
