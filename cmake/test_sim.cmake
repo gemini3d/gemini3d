1b5efe54c614cc7228345aaabb57c14d55c026da
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-07-27 17:16:03 -0400

    add new test names to cmake

--- cmake/test_sim.cmake
+++ cmake/test_sim.cmake
@@ -1,11 +1,29 @@
-#== Fang
-setup_gemini_test(2d_fang 300)
+# this file defines simulation tests.
+# The names of these must match those in pygemini/src/gemini3d/tests/gemini3d_url.ini
+# we are trying to avoid multiple copies of that URL/MD5 data.
 
+# === Fang
+
+# North-South 2D
+setup_gemini_test(2dns_fang 300)
+
+# East-West 2D
+setup_gemini_test(2dew_fang 300)
+
+# 3D
 setup_gemini_test(3d_fang 600)
 
 # === glow
-if(glow)
-  setup_gemini_test(2d_glow 300)
 
+if(glow)
+
+  # North-South 2D
+  setup_gemini_test(2dns_glow 300)
+
+  # East-West 2D
+  setup_gemini_test(2dew_glow 300)
+
+  # 3D
   setup_gemini_test(3d_glow 1800)
+
 endif(glow)
