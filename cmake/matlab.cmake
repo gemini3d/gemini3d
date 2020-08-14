16b16edc21443e5003260e1b696da326b520e79c
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-14 14:44:52 -0400

    cmake: don't fail if Matlab installed in custom directory

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -6,8 +6,13 @@ function(check_matgemini)
 # * appdata\version.xml
 # * appdata\prodcontents.json
 # I have observed over the years that this directory name scheme is universally used.
-string(REGEX MATCH "R([0-9][0-9][0-9][0-9][a-z])" Matlab_RELEASE ${Matlab_ROOT_DIR})
-matlab_get_version_from_release_name(${Matlab_RELEASE} Matlab_VERSION)
+# NOTE: some do not put an "R" in front that is /opt/matlab/2020a/... instead of /opt/matlab/R2020a/...
+#
+set(Matlab_VERSION)
+string(REGEX MATCH "([0-9][0-9][0-9][0-9][a-z])" Matlab_RELEASE ${Matlab_ROOT_DIR})
+if(Matlab_RELEASE) # don't fail if installed in custom directory
+  matlab_get_version_from_release_name(${Matlab_RELEASE} Matlab_VERSION)
+endif()
 
 if(Matlab_VERSION AND Matlab_VERSION VERSION_LESS 9.6)
   message(STATUS "Matlab >= R2019a required for -batch mode")
