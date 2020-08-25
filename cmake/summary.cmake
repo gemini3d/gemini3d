353b74b3d28028df3797af829783f1e558c19099
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-25 13:16:11 -0400

    cmake: change no_mpi to mpi variable name

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -5,7 +5,7 @@ set_package_properties(MPI PROPERTIES
     DESCRIPTION "OpenMPI, IntelMPI, MPICH and MS-MPI are known to work with GEMINI"
     PURPOSE "MPI is essential to GEMINI for massively parallel computation.")
 
-add_feature_info(WithoutMPI no_mpi "partial build of unit tests only")
+add_feature_info(UseMPI mpi "Use MPI parallelization")
 
 add_feature_info(GLOW glow "airglow / aurora model")
 add_feature_info(HDF5 HDF5OK "file read / write")
