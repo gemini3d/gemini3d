d5c3311d643c2ac6415cc96eb40db92a253b5a1f
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-25 13:55:32 -0400

    cmake: simplify / clarify test if() logic

--- cmake/test_compare.cmake
+++ cmake/test_compare.cmake
@@ -3,11 +3,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/matlab.cmake)
 
 function(matlab_compare outdir refdir testname)
 
-if(NOT matlab_ok)
-  return()
-endif()
-
-
 add_test(NAME gemini:compare:hdf5:${testname}:matlab
   COMMAND ${Matlab_MAIN_PROGRAM} -batch "setup; gemini3d.compare_all('${outdir}', '${refdir}')"
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
@@ -23,7 +18,7 @@ endfunction(matlab_compare)
 
 function(python_compare outdir refdir testname)
 
-if(python_ok AND hdf5)
+if(hdf5)
   add_test(NAME gemini:compare:hdf5:${testname}:python
   COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/scripts/gemini_compare.py ${outdir} ${refdir} -file_format h5
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
@@ -33,9 +28,9 @@ if(python_ok AND hdf5)
   FIXTURES_REQUIRED hdf5:${testname}
   REQUIRED_FILES ${outdir}/inputs/config.nml
   SKIP_RETURN_CODE 77)
-endif()
+endif(hdf5)
 
-if(python_ok AND netcdf)
+if(netcdf)
   add_test(NAME gemini:compare:netcdf:${testname}:python
   COMMAND ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/scripts/gemini_compare.py ${outdir} ${refdir} -file_format nc)
 
@@ -44,15 +39,22 @@ if(python_ok AND netcdf)
   FIXTURES_REQUIRED netcdf:${testname}
   REQUIRED_FILES ${outdir}/inputs/config.nml
   SKIP_RETURN_CODE 77)
-endif()
+endif(netcdf)
 
 endfunction(python_compare)
 
 
 function(compare_gemini_output testname)
-  set(outdir ${PROJECT_BINARY_DIR}/test${testname})
-  set(refdir ${PROJECT_SOURCE_DIR}/tests/data/test${testname})
 
+set(outdir ${PROJECT_BINARY_DIR}/test${testname})
+set(refdir ${PROJECT_SOURCE_DIR}/tests/data/test${testname})
+
+if(matlab_ok)
   matlab_compare(${outdir} ${refdir} ${testname})
+endif()
+
+if(python_ok)
   python_compare(${outdir} ${refdir} ${testname})
+endif()
+
 endfunction(compare_gemini_output)
--- cmake/test_setup.cmake
+++ cmake/test_setup.cmake
@@ -5,64 +5,67 @@ function(setup_gemini_test testname TIMEOUT)
 # --- setup test
 set(_outdir ${CMAKE_CURRENT_BINARY_DIR}/test${testname})
 
-if(python_ok AND hdf5)
+if(python_ok)
 
-  win32_hdf5_env()
+if(mpi)
+  set(_cmd ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${testname} -mpiexec ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir})
+else()
+  set(_cmd ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${testname} $<TARGET_FILE:gemini.bin> ${_outdir})
+endif(mpi)
 
-  if(mpi)
-    set(_cmd ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${testname} -mpiexec ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir})
-  else()
-    set(_cmd ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/scripts/run_test.py ${testname} $<TARGET_FILE:gemini.bin> ${_outdir})
-  endif()
+if(hdf5)
 
-  add_test(NAME gemini:hdf5:${testname}:dryrun
-    COMMAND ${_cmd} -dryrun
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-    # NOTE: Working_Diretory is NECESSARY for Windows + Intel + HDF5
+win32_hdf5_env()
 
-  set_tests_properties(gemini:hdf5:${testname}:dryrun PROPERTIES
-    TIMEOUT 60
-    SKIP_RETURN_CODE 77
-    RUN_SERIAL true
-    FIXTURES_SETUP hdf5:${testname}:dryrun)
+add_test(NAME gemini:hdf5:${testname}:dryrun
+  COMMAND ${_cmd} -dryrun
+  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+  # NOTE: Working_Diretory is NECESSARY for Windows + Intel + HDF5
+
+set_tests_properties(gemini:hdf5:${testname}:dryrun PROPERTIES
+  TIMEOUT 60
+  SKIP_RETURN_CODE 77
+  RUN_SERIAL true
+  FIXTURES_SETUP hdf5:${testname}:dryrun)
 
 
-  add_test(NAME gemini:hdf5:${testname}
-    COMMAND ${_cmd}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-    # NOTE: Working_Diretory is NECESSARY for Windows + Intel + HDF5
+add_test(NAME gemini:hdf5:${testname}
+  COMMAND ${_cmd}
+  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+  # NOTE: Working_Diretory is NECESSARY for Windows + Intel + HDF5
 
-  # NOTE: don't use REQUIRED_FILES because it won't let file download if not present.
-  set_tests_properties(gemini:hdf5:${testname} PROPERTIES
-    TIMEOUT ${TIMEOUT}
-    SKIP_RETURN_CODE 77
-    RUN_SERIAL true
-    FIXTURES_REQUIRED "hdf5:${testname}:dryrun;mumps_fixture"
-    FIXTURES_SETUP hdf5:${testname})
+# NOTE: don't use REQUIRED_FILES because it won't let file download if not present.
+set_tests_properties(gemini:hdf5:${testname} PROPERTIES
+  TIMEOUT ${TIMEOUT}
+  SKIP_RETURN_CODE 77
+  RUN_SERIAL true
+  FIXTURES_REQUIRED "hdf5:${testname}:dryrun;mumps_fixture"
+  FIXTURES_SETUP hdf5:${testname})
 
-endif()
+endif(hdf5)
 
-if(python_ok AND netcdf)
-  add_test(NAME gemini:netcdf:${testname}:dryrun
-    COMMAND ${_cmd} -out_format nc -dryrun)
+if(netcdf)
+add_test(NAME gemini:netcdf:${testname}:dryrun
+  COMMAND ${_cmd} -out_format nc -dryrun)
 
-  set_tests_properties(gemini:netcdf:${testname}:dryrun PROPERTIES
-    TIMEOUT 60
-    SKIP_RETURN_CODE 77
-    RUN_SERIAL true
-    FIXTURES_SETUP netcdf:${testname}:dryrun)
+set_tests_properties(gemini:netcdf:${testname}:dryrun PROPERTIES
+  TIMEOUT 60
+  SKIP_RETURN_CODE 77
+  RUN_SERIAL true
+  FIXTURES_SETUP netcdf:${testname}:dryrun)
 
-  add_test(NAME gemini:netcdf:${testname}
-    COMMAND ${_cmd} -out_format nc)
+add_test(NAME gemini:netcdf:${testname}
+  COMMAND ${_cmd} -out_format nc)
 
-  set_tests_properties(gemini:netcdf:${testname} PROPERTIES
-    TIMEOUT ${TIMEOUT}
-    SKIP_RETURN_CODE 77
-    RUN_SERIAL true
-    FIXTURES_REQUIRED "netcdf:${testname}:dryrun;mumps_fixture"
-    FIXTURES_SETUP netcdf:${testname})
-endif()
+set_tests_properties(gemini:netcdf:${testname} PROPERTIES
+  TIMEOUT ${TIMEOUT}
+  SKIP_RETURN_CODE 77
+  RUN_SERIAL true
+  FIXTURES_REQUIRED "netcdf:${testname}:dryrun;mumps_fixture"
+  FIXTURES_SETUP netcdf:${testname})
+endif(netcdf)
 
+endif(python_ok)
 
 compare_gemini_output(${testname})
 
