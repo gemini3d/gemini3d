1cb0b418be343442568b79b5dc322a34102f851a
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-25 15:04:03 -0400

    mumps v5.3.3.6

--- cmake/mumps_external.cmake
+++ cmake/mumps_external.cmake
@@ -1,11 +1,14 @@
 set(mumps_external true CACHE BOOL "autobuild Mumps")
 
+# necessary since CMAKE_ARGS is broken in general
+set(parallel ${mpi} CACHE BOOL "Mumps parallel == Gemini mpi")
+
 include(FetchContent)
 
 FetchContent_Declare(MUMPS_proj
   GIT_REPOSITORY https://github.com/scivision/mumps.git
-  GIT_TAG v5.3.3.5
-  CMAKE_ARGS "-Darith=${arith}" "-Dparallel=true" "-Dmetis=${metis}" "-Dscotch=${scotch}" "-Dopenmp=false"
+  GIT_TAG v5.3.3.6
+  CMAKE_ARGS -Darith=${arith} -Dmetis:BOOL=${metis} -Dscotch:BOOL=${scotch} -Dopenmp:BOOL=false
 )
 
 FetchContent_MakeAvailable(MUMPS_proj)
