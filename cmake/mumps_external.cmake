84fb73188dfba0e303ec529d173f8330713ffa35
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-26 16:06:01 -0400

    mumps v5.3.3.7

--- cmake/mumps_external.cmake
+++ cmake/mumps_external.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 
 FetchContent_Declare(MUMPS_proj
   GIT_REPOSITORY https://github.com/scivision/mumps.git
-  GIT_TAG v5.3.3.6
+  GIT_TAG v5.3.3.7
   CMAKE_ARGS -Darith=${arith} -Dmetis:BOOL=${metis} -Dscotch:BOOL=${scotch} -Dopenmp:BOOL=false
 )
 
