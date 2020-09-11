4a1374e8cee8f17fcdbfda37afcae9b6dd5859d1
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-11 18:21:31 -0400

    h5fortran v3.1.0

--- cmake/h5fortran.cmake
+++ cmake/h5fortran.cmake
@@ -9,7 +9,7 @@ if(hdf5)
     include(FetchContent)
     FetchContent_Declare(h5fortran_proj
       GIT_REPOSITORY https://github.com/geospace-code/h5fortran.git
-      GIT_TAG v3.0.3)
+      GIT_TAG v3.1.0)
 
     FetchContent_MakeAvailable(h5fortran_proj)
   endif()
