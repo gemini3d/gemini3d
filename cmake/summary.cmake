3cf0e8055d35068dd172484baf5f718421e8d31d
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-08 01:38:37 -0400

    report HDF5 status more accurately

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -8,12 +8,17 @@ set_package_properties(MPI PROPERTIES
 add_feature_info(UseMPI mpi "Use MPI parallelization")
 
 add_feature_info(GLOW glow "airglow / aurora model")
-add_feature_info(HDF5 HDF5OK "file read / write")
 add_feature_info(NetCDF4 NCDFOK "file read / write")
 
 add_feature_info(Python python_ok "simulation generation, HPC script generator and plotting")
 
+if(hdf5_external)
+  add_feature_info(HDF5 true "file read / write")
+else()
+  add_feature_info(HDF5 HDF5OK "file read / write")
+endif()
 add_feature_info(AutoHDF5 hdf5_external "auto-build HDF5")
+
 add_feature_info(AutoMumps mumps_external "auto-build Mumps")
 add_feature_info(AutoScalapack scalapack_external "auto-build Scalapack")
 add_feature_info(AutoLapack lapack_external "auto-build Lapack")
