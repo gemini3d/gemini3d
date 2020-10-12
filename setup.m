eb58b184f5fc0fa2682c81de284d27b472a20c5a
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-12 14:59:08 -0400

    clone mat_gemini submodules, too

--- setup.m
+++ setup.m
@@ -7,7 +7,7 @@ matgemini_dir_name = "mat_gemini";
 cwd = fileparts(mfilename('fullpath'));
 gemini_matlab = fullfile(cwd, "..", matgemini_dir_name);
 if ~isfolder(gemini_matlab)
-  cmd = "git -C " + fullfile(cwd, "..") + " clone " + matgemini_git + " " + matgemini_dir_name;
+  cmd = "git -C " + fullfile(cwd, "..") + " clone --recursive " + matgemini_git + " " + matgemini_dir_name;
   ret = system(cmd);
   assert(ret==0, "problem downloading Gemini Matlab functions")
 end
