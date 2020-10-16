9457c6f0e62f5b8756fd005e0158a1166780504f
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-16 09:44:41 -0400

    matlab: enable compare test for netcdf and hdf5

--- cmake/test_compare.cmake
+++ cmake/test_compare.cmake
@@ -1,13 +1,13 @@
 
 function(matlab_compare outdir refdir testname)
 
-add_test(NAME gemini:compare:hdf5:${testname}:matlab
+add_test(NAME gemini:compare:${testname}:matlab
   COMMAND ${Matlab_MAIN_PROGRAM} -batch "setup; gemini3d.compare_all('${outdir}', '${refdir}')"
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 
-set_tests_properties(gemini:compare:hdf5:${testname}:matlab PROPERTIES
+set_tests_properties(gemini:compare:${testname}:matlab PROPERTIES
 TIMEOUT 120
-FIXTURES_REQUIRED hdf5:${testname}
+FIXTURES_REQUIRED "hdf5:${testname};netcdf:${testname}"
 REQUIRED_FILES ${outdir}/inputs/config.nml
 SKIP_RETURN_CODE 77)
 
@@ -17,26 +17,26 @@ endfunction(matlab_compare)
 function(python_compare outdir refdir testname)
 
 if(hdf5)
-  add_test(NAME gemini:compare:hdf5:${testname}:python
-  COMMAND ${Python3_EXECUTABLE} -m gemini3d.compare ${outdir} ${refdir} -file_format h5
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+add_test(NAME gemini:compare:hdf5:${testname}:python
+COMMAND ${Python3_EXECUTABLE} -m gemini3d.compare ${outdir} ${refdir} -file_format h5
+WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 
-  set_tests_properties(gemini:compare:hdf5:${testname}:python PROPERTIES
-  TIMEOUT 60
-  FIXTURES_REQUIRED hdf5:${testname}
-  REQUIRED_FILES ${outdir}/inputs/config.nml
-  SKIP_RETURN_CODE 77)
+set_tests_properties(gemini:compare:hdf5:${testname}:python PROPERTIES
+TIMEOUT 60
+FIXTURES_REQUIRED hdf5:${testname}
+REQUIRED_FILES ${outdir}/inputs/config.nml
+SKIP_RETURN_CODE 77)
 endif(hdf5)
 
 if(netcdf)
-  add_test(NAME gemini:compare:netcdf:${testname}:python
-  COMMAND ${Python3_EXECUTABLE} -m gemini3d.compare ${outdir} ${refdir} -file_format nc)
+add_test(NAME gemini:compare:netcdf:${testname}:python
+COMMAND ${Python3_EXECUTABLE} -m gemini3d.compare ${outdir} ${refdir} -file_format nc)
 
-  set_tests_properties(gemini:compare:netcdf:${testname}:python PROPERTIES
-  TIMEOUT 60
-  FIXTURES_REQUIRED netcdf:${testname}
-  REQUIRED_FILES ${outdir}/inputs/config.nml
-  SKIP_RETURN_CODE 77)
+set_tests_properties(gemini:compare:netcdf:${testname}:python PROPERTIES
+TIMEOUT 60
+FIXTURES_REQUIRED netcdf:${testname}
+REQUIRED_FILES ${outdir}/inputs/config.nml
+SKIP_RETURN_CODE 77)
 endif(netcdf)
 
 endfunction(python_compare)
