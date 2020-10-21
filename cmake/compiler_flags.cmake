181e345b89114c3a414cbdb8199804bd9de37991
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-10-21 12:27:49 -0400

    cleanup intel options

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,7 +1,11 @@
 # === compiler setup
 # feel free to add more compiler_*.cmake
 if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
+  if(WIN32)
+  include(${CMAKE_CURRENT_LIST_DIR}/compiler_intel_win.cmake)
+  else(WIN32)
   include(${CMAKE_CURRENT_LIST_DIR}/compiler_intel.cmake)
+  endif(WIN32)
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
   include(${CMAKE_CURRENT_LIST_DIR}/compiler_gnu.cmake)
 endif()
---
+++ cmake/compiler_intel_win.cmake
@@ -0,0 +1,12 @@
+# workaround: /nologo /libs:dll are set by CMake in cmake_fortran_flags
+string(APPEND CMAKE_Fortran_FLAGS " /warn:declarations /traceback")
+string(APPEND CMAKE_Fortran_FLAGS " /Qopenmp")
+string(APPEND CMAKE_Fortran_FLAGS " /heap-arrays")  # necessary for stack overflow avoid, even for 2D
+string(APPEND CMAKE_Fortran_FLAGS_DEBUG " /check:bounds")
+string(APPEND CMAKE_Fortran_FLAGS " /warn:nounused /Qdiag-disable:5268 /Qdiag-disable:7712")
+#add_link_options(/Qparallel)
+
+# enforce Fortran 2018 standard
+if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 19)
+string(APPEND CMAKE_Fortran_FLAGS " /stand:f18")
+endif()
