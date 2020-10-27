a9d3662c46cc72b125008774edf9fcc58c512665
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-27 01:10:25 -0400

    compare: use explicit reference path

--- cmake/test_setup.cmake
+++ cmake/test_setup.cmake
@@ -4,13 +4,14 @@ function(setup_gemini_test testname TIMEOUT)
 
 # --- setup test
 set(_outdir ${CMAKE_CURRENT_BINARY_DIR}/test${testname})
+set(_refdir ${PROJECT_SOURCE_DIR}/tests/data)
 
 if(python_ok)
 
 if(mpi)
-  set(_cmd ${Python3_EXECUTABLE} -m gemini3d.run_test ${testname} -mpiexec ${MPIEXEC_EXECUTABLE} $<TARGET_FILE:gemini.bin> ${_outdir})
+  set(_cmd ${Python3_EXECUTABLE} -m gemini3d.run_test -mpiexec ${MPIEXEC_EXECUTABLE} ${testname} $<TARGET_FILE:gemini.bin> ${_outdir} ${_refdir})
 else()
-  set(_cmd ${Python3_EXECUTABLE} -m gemini3d.run_test ${testname} $<TARGET_FILE:gemini.bin> ${_outdir})
+  set(_cmd ${Python3_EXECUTABLE} -m gemini3d.run_test ${testname} $<TARGET_FILE:gemini.bin> ${_outdir} ${_refdir})
 endif(mpi)
 
 if(hdf5)
