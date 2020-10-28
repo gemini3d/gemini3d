89204cd1a3d19e42b2ec9711de86747e0dab4fcb
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-10-28 00:44:44 -0400

    neutral:interp submodule

--- src/neutral/proj.f90
+++ src/neutral/proj.f90
@@ -540,4 +540,148 @@ end if
 end procedure gridproj_dneu3D
 
 
+subroutine slabrange(maxzn,ximat,yimat,zimat,sourcemlat,xnrange,ynrange)
+
+!takes in a subgrid and the max altitude of interest for neutral interpolation and then computes
+!what the maximum xn and yn will be for that slab
+! ZZZ - also this is specific to dipole grids right now...
+
+real(wp), intent(in) :: maxzn
+real(wp), dimension(:,:,:), intent(in) :: ximat,yimat,zimat
+real(wp), intent(in) :: sourcemlat
+real(wp), dimension(2), intent(out) :: xnrange,ynrange     !for min and max
+
+real(wp), dimension(:,:,:), allocatable :: xitmp,yitmp,zitmp
+integer :: lx1tmp
+integer, dimension(size(ximat,2),size(ximat,3)) :: ix1stmp
+integer :: ix1tmp
+logical :: flagSH
+integer :: ix1
+
+
+!in what hemisphere is our source?
+if (sourcemlat<=0) then
+  flagSH=.true.
+else
+  flagSH=.false.
+end if
+
+
+!peel the grid in half (source hemisphere if closed dipole)
+if (gridflag==0) then    !closed dipole grid
+
+  ix1=maxloc(pack(zimat(:,1,1),.true.),1)    !apex is by definition the highest altitude along a given field line
+  if (flagSH) then
+    lx1tmp=ix1                  !first piece of arrays
+  else
+    lx1tmp=lx1-ix1    !second (end) piece of arrays
+  end if
+  allocate(xitmp(lx1tmp,lx2,lx3),yitmp(lx1tmp,lx2,lx3),zitmp(lx1tmp,lx2,lx3))   !could this be done more less wastefully with pointers???
+
+  if(flagSH) then    !southern hemisphere
+    xitmp=ximat(1:ix1,1:lx2,1:lx3)          !select beginning of the array - the southern half
+    yitmp=yimat(1:ix1,1:lx2,1:lx3)
+    zitmp=zimat(1:ix1,1:lx2,1:lx3)
+  else               !northern hemisphere
+      xitmp=ximat(ix1+1:lx1,1:lx2,1:lx3)    !select end half of the array
+      yitmp=yimat(ix1+1:lx1,1:lx2,1:lx3)
+      zitmp=zimat(ix1+1:lx1,1:lx2,1:lx3)
+  end if
+else     !this is not an interhemispheric grid so our approach is to just use all of the data
+  lx1tmp=lx1
+  allocate(xitmp(lx1tmp,lx2,lx3),yitmp(lx1tmp,lx2,lx3),zitmp(lx1tmp,lx2,lx3))   !could this be done more less wastefully with pointers?
+  xitmp=ximat(1:lx1,1:lx2,1:lx3)
+  yitmp=yimat(1:lx1,1:lx2,1:lx3)
+  zitmp=zimat(1:lx1,1:lx2,1:lx3)
+!  flagSH=.true.    !treat is as southern, doesn't really matter in this case...
+end if
+
+
+!the min and max x are simply determined by longitude...
+xnrange(1)=minval(xitmp)
+xnrange(2)=maxval(xitmp)
+
+
+!situation is more complicated for latitude due to dipole grid, need to determine by L-shell
+if (flagSH) then
+  ix1=minloc(zitmp(:,1,1)-maxzn,1,zitmp(:,1,1)-maxzn>0._wp)    !find the min distance from maxzn subject to constraint that it is > 0, just use the first longitude slice since they will all have the same L-shell-field line relations
+  ynrange(2)=yitmp(ix1,1,1)
+  ix1=minloc(zitmp(:,lx2,1),1,zitmp(:,lx2,1)<0._wp)
+  ynrange(1)=yitmp(ix1,lx2,1)
+else    !things are swapped around in NH
+  ix1=minloc(zitmp(:,1,1)-maxzn,1,zitmp(:,1,1)-maxzn>0._wp)    !find the min distance from maxzn subject to constraint that it is > 0; this is the southernmost edge of the neutral slab we need
+  ynrange(1)=yitmp(ix1,1,1)
+  ix1=minloc(zitmp(:,lx2,1),1,zitmp(:,lx2,1)<0._wp)
+  ynrange(2)=yitmp(ix1,lx2,1)
+end if
+
+deallocate(xitmp,yitmp,zitmp)
+
+end subroutine slabrange
+
+
+subroutine range2inds(ranges,zn,xnall,ynall,indices)
+
+!! determine where the slab described by ranges falls within the global neutral grid
+
+real(wp), dimension(6), intent(in) :: ranges
+real(wp), dimension(:), intent(in) :: zn,xnall,ynall
+integer, dimension(6), intent(out) :: indices
+
+real(wp) :: minzn,maxzn,minxn,maxxn,minyn,maxyn
+integer :: ixn,iyn
+
+
+!for clarity
+minzn=ranges(1)
+maxzn=ranges(2)
+minxn=ranges(3)
+maxxn=ranges(4)
+minyn=ranges(5)
+maxyn=ranges(6)
+
+
+!always use the full z-range
+indices(1)=1
+indices(2)=lzn
+
+
+!x-range
+ixn=1
+do while (ixn<lxnall .and. xnall(ixn)<minxn)
+  ixn=ixn+1
+end do
+indices(3)=max(ixn-1,1)    !just to be sure go back one index so that we cover the min range, don't let index fall below zero
+do while (ixn<lxnall .and. xnall(ixn)<maxxn)
+  ixn=ixn+1
+end do
+indices(4)=ixn
+
+
+!y-range
+iyn=1
+do while (iyn<lynall .and. ynall(iyn)<minyn)
+  iyn=iyn+1
+end do
+indices(5)=max(iyn-1,1)    !just to be sure go back one index so that we cover the min range
+do while (iyn<lynall .and. ynall(iyn)<maxyn)
+  iyn=iyn+1
+end do
+indices(6)=iyn
+
+print*, '!!!!!!!!!!!!!!!!!'
+print*, myid
+print*, ranges
+print*, indices
+print*, lxnall,lynall
+print*, xnall(indices(3)),xnall(indices(4))
+print*, ynall(indices(5)),ynall(indices(6))
+print*, '!!!!!!!!!!!!!!!!!'
+
+
+!corner cases - range is not at all within the neutral grid...  Manifests as both indices being either 1 or lxi, interpolation should zero these out...
+
+end subroutine range2inds
+
+
 end submodule proj
