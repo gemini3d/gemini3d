253d4f3a4b29213ee681747df885ab327130f20c
Michael Hirsch <scivision@users.noreply.github.com>
2020-11-02 15:32:54 -0500

    cmake: correct option printout

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -9,15 +9,11 @@ add_feature_info(UseMPI mpi "Use MPI parallelization")
 
 add_feature_info(GLOW glow "airglow / aurora model")
 
-add_feature_info(NetCDF4 nc4fortran_FOUND "file read / write")
+add_feature_info(NetCDF4 netcdf "file read / write")
 
 add_feature_info(Python python_ok "simulation generation, HPC script generator and plotting")
 
-if(hdf5_external)
-  add_feature_info(HDF5 true "file read / write")
-else()
-  add_feature_info(HDF5 h5fortran_FOUND "file read / write")
-endif()
+add_feature_info(HDF5 hdf5 "file read / write")
 add_feature_info(AutoHDF5 hdf5_external "auto-build HDF5")
 
 add_feature_info(AutoMumps mumps_external "auto-build Mumps")
