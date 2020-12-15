c993763279c8cec73e319e576e42d3f65b567665
Michael Hirsch <scivision@users.noreply.github.com>
2020-12-14 21:02:31 -0500

    lapack95 vendor: make real{64,32} agnostic, and not require unused lapack kind

--- src/vendor/lapack95/gbsv.in.f90
+++ src/vendor/lapack95/gbsv.in.f90
@@ -6,11 +6,16 @@ implicit none (type, external)
 private
 public :: gbsv
 
+interface gbsv
+  procedure :: gbsv32, gbsv64
+end interface gbsv
+
 contains
 
-subroutine gbsv(A,B,KL,IPIV,INFO)
-real(real64), dimension(:,:), intent(inout) :: A
-real(real64), dimension(:), intent(inout) :: B
+
+subroutine gbsv32(A,B,KL,IPIV,INFO)
+real(real32), dimension(:,:), intent(inout) :: A
+real(real32), dimension(:), intent(inout) :: B
 integer, intent(in), optional :: KL
 integer, dimension(:), intent(out), optional, target :: IPIV
 integer, intent(out), optional :: INFO
@@ -18,7 +23,7 @@ integer :: LINFO, ISTAT, SIPIV, LDA, N, NRHS, LKL, KU
 integer, dimension(:), pointer :: LPIV
 intrinsic :: size, present
 
-external :: sgbsv, dgbsv
+external :: sgbsv
 
 LINFO = 0; ISTAT = 0;
 LDA = size(A,1); N = size(A,2); NRHS = 1;
@@ -40,22 +45,55 @@ end if
 if ( ISTAT == 0 ) then
   KU = LDA - 2*LKL - 1
 
-  ! select type (A)
-  !   type is (real(real32))
-  !     @sgbsv@
-  !   type is (real(real64))
-  !     call dgbsv(N,LKL,KU,NRHS,A,LDA,LPIV,B,N,LINFO)
-  !   class default
-  !     error stop "unhandled kind"
-  ! end select
-  ! FIXME: above comments are workaround for GCC10
-  call dgbsv(N,LKL,KU,NRHS,A,LDA,LPIV,B,N,LINFO)
+  @sgbsv@
 endif
 
 if(present(info)) info = linfo
 
 if ( .NOT.present(IPIV) ) deallocate(LPIV)
 
-end subroutine gbsv
+end subroutine gbsv32
+
+
+subroutine gbsv64(A,B,KL,IPIV,INFO)
+real(real64), dimension(:,:), intent(inout) :: A
+real(real64), dimension(:), intent(inout) :: B
+integer, intent(in), optional :: KL
+integer, dimension(:), intent(out), optional, target :: IPIV
+integer, intent(out), optional :: INFO
+integer :: LINFO, ISTAT, SIPIV, LDA, N, NRHS, LKL, KU
+integer, dimension(:), pointer :: LPIV
+intrinsic :: size, present
+
+external :: dgbsv
+
+LINFO = 0; ISTAT = 0;
+LDA = size(A,1); N = size(A,2); NRHS = 1;
+
+if ( present(KL) ) then
+  LKL = KL
+else
+  LKL = (LDA-1)/3
+end if
+
+if ( present(IPIV) ) then
+  SIPIV = size(IPIV)
+  LPIV => IPIV
+else
+  SIPIV = N
+  allocate(LPIV(N))
+end if
+
+if ( ISTAT == 0 ) then
+  KU = LDA - 2*LKL - 1
+
+  @dgbsv@
+endif
+
+if(present(info)) info = linfo
+
+if ( .NOT.present(IPIV) ) deallocate(LPIV)
+
+end subroutine gbsv64
 
 end module vendor_lapack95
