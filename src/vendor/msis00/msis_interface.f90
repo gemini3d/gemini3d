cabdd147a5fe38669bacd96223be1c148ad65dfc
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-15 13:06:35 -0500

    msis linkage: msis_ifc to prepare generic interface

---
+++ src/vendor/msis00/msis_interface.f90
@@ -0,0 +1,8 @@
+module msis_interface
+!! this module allows selecting MSISE00 or MSIS 2.0
+!! it is a thin abstraction of the MSIS routines
+!! MSISE00 is Fortran 66 style, while MSIS 2.0 is Fortran 90 style
+!!
+!! We assume MSISE00 is always available, which MSIS 2.0 might not be available.
+
+end module
