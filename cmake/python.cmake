4ca933b108175ce71c615ae5cead5086df6857d9
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-21 15:40:28 -0500

    use fetchcontent to get PyGemini, more PyGemini checks

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,10 +1,21 @@
-include(${CMAKE_CURRENT_LIST_DIR}/git.cmake)
+include(FetchContent)
+
 
 function(check_pygemini)
 
 # Python3::Interpreter does NOT work, use ${Python3_EXECUTABLE}
 
 # __path__ is always iterable: https://docs.python.org/3/reference/import.html#__path__
+
+# Numpy conflicts are a general source of trouble
+execute_process(COMMAND ${Python3_EXECUTABLE} -c "import numpy,sys; print(f'Python {sys.version}  Numpy {numpy.__version__}')"
+  RESULT_VARIABLE _ok
+  TIMEOUT 15)
+
+if(NOT _ok EQUAL 0)
+  message(STATUS "Problem with Python Numpy, cannot use PyGemini")
+  return()
+endif()
 
 execute_process(COMMAND ${Python3_EXECUTABLE} -c "import gemini3d; import gemini3d.run_test; print(gemini3d.__path__[0])"
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}  # help avoid Intel Windows false import error due to hdf5 .dll in build dir
@@ -15,7 +26,7 @@ execute_process(COMMAND ${Python3_EXECUTABLE} -c "import gemini3d; import gemini
 
 if(_ok EQUAL 0)
   message(STATUS "PyGemini found: ${PYGEMINI_DIR}")
-  set(python_ok true CACHE BOOL "PyGemini OK")
+  set(PYGEMINI_DIR ${PYGEMINI_DIR} CACHE STRING "PyGemini path")
 endif()
 
 endfunction()
@@ -23,51 +34,45 @@ endfunction()
 # --- script
 find_package(Python3 COMPONENTS Interpreter)
 if(NOT Python3_FOUND)
-  message(WARNING "Python not found, cannot use PyGemini.")
   return()
 endif()
 # keep this in script so it's not scoped in function
 
-set(pygemini_dir "${PROJECT_SOURCE_DIR}/../pygemini/")
-
-if(NOT python_ok)
+if(NOT PYGEMINI_DIR)
   check_pygemini()
 endif()
 
-if(NOT python_ok)
-  if(NOT IS_DIRECTORY ${pygemini_dir})
-    git_download(${pygemini_dir} ${pygemini_git} ${pygemini_tag})
-  endif()
-  # detect virtualenv
-  # this is how CMake itself works for FindPython3 in Modules/FindPython/Support.cmake
-  if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
-    set(_pip_args)
-  else()
-    set(_pip_args "--user")
-  endif()
+if(NOT PYGEMINI_DIR)
 
-  if(NOT EXISTS ${pygemini_dir}/setup.py)
-    message(WARNING "Could not find PyGemini setup.py in ${pygemini_dir}")
-    return()
-  endif()
+FetchContent_Declare(pygemini_proj
+  GIT_REPOSITORY ${pygemini_git}
+  GIT_TAG ${pygemini_tag}
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}/../pygemini)
 
-  execute_process(COMMAND ${Python3_EXECUTABLE} -m pip install -e. ${_pip_args}
-    WORKING_DIRECTORY ${pygemini_dir}
-    OUTPUT_VARIABLE _log
-    ERROR_VARIABLE _log
-    RESULT_VARIABLE _ok)
+FetchContent_MakeAvailable(pygemini_proj)
 
-  if(_ok EQUAL 0)
-    message(STATUS "Setup PyGemini in ${pygemini_dir}")
-  else()
-    message(STATUS "${_ok} ${_log}")
-    message(WARNING "Problem installing PyGemini with ${Python3_EXECUTABLE} in ${pygemini_dir}  Perhaps try doing this manually: ${gemini_pygemini_url}")
-    return()
-  endif()
-
-  check_pygemini()
+# detect virtualenv
+# this is how CMake itself works for FindPython3 in Modules/FindPython/Support.cmake
+if(DEFINED ENV{VIRTUAL_ENV}) # OR DEFINED ENV{CONDA_PREFIX})  # intel python HPC admin install
+  set(_pip_args)
+else()
+  set(_pip_args "--user")
 endif()
 
-if(NOT python_ok)
-  message(WARNING "MISSING: PyGemini ${gemini_pygemini_url} many self-tests will not work.")
+execute_process(COMMAND ${Python3_EXECUTABLE} -m pip install -e ${pygemini_proj_SOURCE_DIR} ${_pip_args}
+  RESULT_VARIABLE _ok)
+
+if(_ok EQUAL 0)
+  message(STATUS "Setup PyGemini in ${pygemini_proj_SOURCE_DIR}")
+else()
+  message(STATUS "Problem installing PyGemini with ${Python3_EXECUTABLE}")
+  return()
+endif()
+
+check_pygemini()
+
+endif(NOT PYGEMINI_DIR)
+
+if(NOT PYGEMINI_DIR)
+  message(STATUS "MISSING: PyGemini ${pygemini_git}   Many self-tests will not run.")
 endif()
