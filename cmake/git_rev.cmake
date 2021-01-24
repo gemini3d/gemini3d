58ff63e60f9fe3e2042a89b594e3dd5cbeb4236d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-24 11:18:41 -0500

    handle git more canonically

--- cmake/git_rev.cmake
+++ cmake/git_rev.cmake
@@ -7,9 +7,13 @@
 find_package(Git)
 
 set(_max_len 80) # arbitrary limit, so as not to exceed maximum 132 character Fortran line length.
+set(git_branch)
+set(git_rev)
+set(git_porcelain .false.)
 
-
-if(GIT_FOUND)
+if(NOT GIT_FOUND)
+  return()
+endif()
 
 set(git_version ${GIT_VERSION_STRING})
 string(SUBSTRING ${git_version} 0 ${_max_len} git_version)
@@ -17,10 +21,14 @@ string(SUBSTRING ${git_version} 0 ${_max_len} git_version)
 execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
   OUTPUT_VARIABLE git_branch
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-if(git_branch)
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE _err)
+if(_err EQUAL 0)
   string(SUBSTRING ${git_branch} 0 ${_max_len} git_branch)
-endif(git_branch)
+else()
+  set(git_branch)
+endif()
+
 
 # git describe --tags can make CI error fatal: No names found, cannot describe anything.
 execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags
@@ -28,33 +36,31 @@ execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags
   OUTPUT_VARIABLE git_rev
   OUTPUT_STRIP_TRAILING_WHITESPACE
   RESULT_VARIABLE _err)
-
 if(NOT _err EQUAL 0)
   execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE git_rev
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    RESULT_VARIABLE _err)
 endif()
-
-if(git_rev)
+if(_err EQUAL 0)
   string(SUBSTRING ${git_rev} 0 ${_max_len} git_rev)
-endif(git_rev)
-
+else()
+  set(git_rev)
+endif()
 string(APPEND git_rev " ${PROJECT_VERSION}")
 
 set(git_porcelain .true.)
 execute_process(COMMAND ${GIT_EXECUTABLE} status --porcelain
   WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
   OUTPUT_VARIABLE _porcelain
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE _err)
 if(_porcelain)
   set(git_porcelain .false.)
 endif(_porcelain)
-
-else(GIT_FOUND)
-  set(git_branch)
-  set(git_rev)
+if(NOT _err EQUAL 0)
   set(git_porcelain .false.)
-endif(GIT_FOUND)
+endif()
 
 message(STATUS "${PROJECT_NAME}  git revision: ${git_rev}  git_branch: ${git_branch}  git_porcelain: ${git_porcelain}")
