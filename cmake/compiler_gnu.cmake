2b9202b117ae7ef3e0ad5eac379617623a32e5f3
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-24 23:56:42 -0500

    gcc less chatty warning for non-dev

--- cmake/compiler_gnu.cmake
+++ cmake/compiler_gnu.cmake
@@ -1,11 +1,16 @@
 # NOTE: don't use -march=native as GCC doesn't support all CPU arches with that option.
 add_compile_options(-mtune=native)
 
-# keep Wall and Wextra in cmake_fortran_flags so FetchContent packages can override
+# keep Wall in cmake_fortran_flags so FetchContent packages can override
 # and thereby avoid useless megabytes of other project warnings
-string(APPEND CMAKE_Fortran_FLAGS " -Wall -Wextra -fimplicit-none")
+string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none")
+if(dev)
+  string(APPEND CMAKE_Fortran_FLAGS " -Wall -Wextra")
+  # -Wpedantic makes too many false positives
+else(dev)
+  string(APPEND CMAKE_Fortran_FLAGS " -Wno-unused-dummy-argument -Wno-unused-variable -Wno-unused-function")
+endif(dev)
 
-# -Wpedantic makes too many false positives, through Gfortran 9
 
 # Wdo-subscript is known to warn on obvious non-problems
 check_fortran_compiler_flag(-Wdo-subscript dosubflag)
@@ -15,7 +20,6 @@ endif()
 
 string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -Werror=array-bounds -fcheck=all")
   # string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=invalid,zero,overflow")#,underflow)
-string(APPEND CMAKE_Fortran_FLAGS " -Wno-unused-dummy-argument -Wno-unused-variable -Wno-unused-function")
 
 # enforce Fortran 2018 standard
 # this flag is buggy at least through GCC 9, causing fake "Common block" warnings
