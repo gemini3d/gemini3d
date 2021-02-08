971435a1dd78111daa13b026bd734f6a99225ef5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-02-07 21:54:37 -0500

    add msis unit test

---
+++ src/vendor/nrl_msis/test_msis.f90
@@ -0,0 +1,79 @@
+program test_msis
+
+use, intrinsic :: iso_fortran_env, only : real32
+use h5fortran, only : hdf5_file
+
+implicit none (type, external)
+
+integer :: argc, i
+character(1000) :: argv
+character(:), allocatable :: fnew, fref
+real(real32) :: Dnew(9), Dref(9), Tnew(2), Tref(2), altnew, altref
+
+argc = command_argument_count()
+if(argc /= 2) error stop "./test_msis new_file ref_file"
+
+call get_command_argument(1, argv)
+fnew = trim(argv)
+
+call get_command_argument(2, argv)
+fref = trim(argv)
+
+call reader(fnew, altnew, Dnew, Tnew)
+call reader(fref, altref, Dref, Tref)
+
+do i = 1,size(Tnew)
+  if (abs(Tnew(i) - Tref(i)) > 0.01) error stop "mismatch: Tn"
+enddo
+
+do i = 1,size(Dnew)
+  if (abs(Dnew(i) - Dref(i)) > 0.001*Dref(i)) error stop "mismatch: Dn"
+enddo
+
+contains
+
+
+subroutine reader(file, alt, Dn, Tn)
+
+character(*), intent(in) :: file
+real(real32), intent(out) :: alt, Dn(9), Tn(2)
+
+real(real32) :: buf(1,1,1)  !< a priori for test file
+type(hdf5_file) :: hf
+
+call hf%initialize(file, status="old", action="read")
+
+call hf%read("/alt", buf)
+alt = buf(1,1,1)
+
+call hf%read("/nHe", buf)
+Dn(1) = buf(1,1,1)
+call hf%read("/nO", buf)
+Dn(2) = buf(1,1,1)
+call hf%read("/nN2", buf)
+Dn(3) = buf(1,1,1)
+call hf%read("/nO2", buf)
+Dn(4) = buf(1,1,1)
+call hf%read("/nAr", buf)
+Dn(5) = buf(1,1,1)
+call hf%read("/TotalMassDensity", buf)
+Dn(6) = buf(1,1,1)
+call hf%read("/nH", buf)
+Dn(7) = buf(1,1,1)
+call hf%read("/nN", buf)
+Dn(8) = buf(1,1,1)
+call hf%read("/nOana", buf)
+Dn(9) = buf(1,1,1)
+
+call hf%read("/Tn", buf)
+Tn(2) = buf(1,1,1)
+call hf%read("/Texo", buf)
+Tn(1) = buf(1,1,1)
+
+
+call hf%finalize()
+
+end subroutine reader
+
+
+end program
