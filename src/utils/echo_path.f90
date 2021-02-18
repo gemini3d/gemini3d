1349da5f4211890de6f621a6193228bb8be3d4cc
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-18 11:17:41 -0500

    add environment print test
    this can be useful when diagnosing seemingly bizarre problems
    when you're not sure if the shell / frontend is passing needed
    environment variables to the final executable.
    
    For example, perhaps a Matlab/Python script calls the gemini3d.run
    front-end that invokes gemini.bin. We want to be sure the final
    Fortran executable has the several environment variables it needs,
    particularly for MPI etc.

---
+++ src/utils/echo_path.f90
@@ -0,0 +1,25 @@
+program echo_path
+!! tell environment variable.
+!! can help establish if run frameworks like CTest are passing along variables properly.
+
+implicit none (type, external)
+
+character(:), allocatable :: var
+character(4096) :: buf
+integer :: i,L
+
+if (command_argument_count() == 0) then
+  var = "PATH"
+else
+  call get_command_argument(1, buf)
+  var = trim(buf)
+endif
+
+call get_environment_variable(var, buf, status=i)
+if (i==1) error stop var // " is not an environment variable."
+if (i==-1) error stop "buffer is too small for " // var
+if (i/=0) error stop "problem reading " // var
+
+print *, trim(buf)
+
+end program
