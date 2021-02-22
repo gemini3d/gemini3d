d52d5b7e8956fc59d8d2f53b768826fe122630f1
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-22 17:47:07 -0500

    note that sanity_check internally ignores ghost cells

--- src/gemini.f90
+++ src/gemini.f90
@@ -155,7 +155,7 @@ if (cfg%flagglow /= 0) then
 end if
 
 
-!> FIXME: Zero out all state variables here - the corner ghost cells otherwise never get set and could contain garbage whicgh may make the sanity check fail since it does look at ghost cells, as well.  
+!> FIXME: Zero out all state variables here - the corner ghost cells otherwise never get set and could contain garbage whicgh may make the sanity check fail since it does look at ghost cells, as well.
 !ns=0; vs1=0; Ts=0;
 ! oddly this doesn't seem to help our issue...
 
@@ -359,11 +359,8 @@ main : do while (t < tdur)
   endif
 
   !> Sanity check key variables before advancing
-  !call check_finite_output(t, mpi_cfg%myid, vs2,vs3,ns,vs1,Ts, Phi,J1,J2,J3)
   ! FIXME: for whatever reason, it is just a fact that vs1 has trash in ghost cells after fluid_adv; I don't know why...
-  call check_finite_output(t, mpi_cfg%myid, vs2(1:lx1,1:lx2,1:lx3,1:lsp),vs3(1:lx1,1:lx2,1:lx3,1:lsp), &
-                              ns(1:lx1,1:lx2,1:lx3,1:lsp),vs1(1:lx1,1:lx2,1:lx3,1:lsp), &
-                              Ts(1:lx1,1:lx2,1:lx3,1:lsp), Phi,J1,J2,J3)
+  call check_finite_output(t, mpi_cfg%myid, vs2,vs3,ns,vs1,Ts, Phi,J1,J2,J3)
 
   !> NOW OUR SOLUTION IS FULLY UPDATED SO UPDATE TIME VARIABLES TO MATCH...
   it = it + 1
