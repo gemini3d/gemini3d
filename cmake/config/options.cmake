da0e81e97ad27ed5f8fed4f71c5f2863fd3a6ef6
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-24 14:51:51 -0500

    initial pass at Gemini cmake --install package

--- cmake/config/options.cmake
+++ cmake/config/options.cmake
@@ -90,3 +90,10 @@ endif()
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
   file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
 endif()
+
+# --- default install directory under build/local
+# users can specify like "cmake -B build -DCMAKE_INSTALL_PREFIX=~/mydir"
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  # will not take effect without FORCE
+  set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "Install top-level directory" FORCE)
+endif()
---
+++ cmake/package/install.cmake
@@ -0,0 +1,55 @@
+# --- BOILERPLATE: install / packaging
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  INSTALL_DESTINATION lib)
+
+write_basic_package_version_file(
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  VERSION ${${PROJECT_NAME}_VERSION}
+  COMPATIBILITY SameMinorVersion
+)
+
+install(EXPORT ${PROJECT_NAME}Targets
+  FILE ${PROJECT_NAME}Targets.cmake
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION lib/cmake/${PROJECT_NAME}
+   )
+
+install(FILES
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  DESTINATION lib/cmake/${PROJECT_NAME})
+
+export(EXPORT ${PROJECT_NAME}Targets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Targets.cmake"
+  NAMESPACE ${PROJECT_NAME}::
+)
+
+# --- CPack
+
+set(_fmt TGZ)
+if(WIN32)
+  set(_fmt ZIP)
+endif()
+set(CPACK_GENERATOR ${_fmt})
+set(CPACK_SOURCE_GENERATOR ${_fmt})
+set(CPACK_PACKAGE_VENDOR "Michael Hirsch")
+set(CPACK_PACKAGE_CONTACT "Michael Hirsch")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libhdf5-dev (>=1.10) liblapack-dev")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE.txt")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/Readme.md")
+set(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/package")
+set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+
+# not .gitignore as its regex syntax is more advanced than CMake
+file(READ ${CMAKE_CURRENT_LIST_DIR}/.cpack_ignore _cpack_ignore)
+string(REGEX REPLACE "\n" ";" _cpack_ignore ${_cpack_ignore})
+set(CPACK_SOURCE_IGNORE_FILES "${_cpack_ignore}")
+
+install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE}
+  DESTINATION share/docs/${PROJECT_NAME})
+
+include(CPack)
