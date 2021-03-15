286efcd1f50cbbca1bf63e38823e7b49bea2e6bd
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-15 12:37:32 -0400

    set target props per binary dir

---
+++ cmake/SetTargProps.cmake
@@ -0,0 +1,13 @@
+function(set_targ_props)
+
+foreach(t ${ARGV})
+
+  get_target_property(_bin ${t} BINARY_DIR)
+
+  target_include_directories(${t} INTERFACE
+  $<BUILD_INTERFACE:${_bin}/include>
+  $<INSTALL_INTERFACE:include>)
+  set_target_properties(${t} PROPERTIES Fortran_MODULE_DIRECTORY ${_bin}/include)
+endforeach()
+
+endfunction(set_targ_props)
