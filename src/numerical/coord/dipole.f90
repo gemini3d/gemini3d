653444a3dc396eaf30e63eb103c1824b110379f8
Matthew D. Zettergren <mattzett@gmail.com>
2021-03-19 11:10:46 -0400

    basid dipole setup compiling

--- src/numerical/coord/dipole.f90
+++ src/numerical/coord/dipole.f90
@@ -5,22 +5,30 @@ module dipole
 ! uses
 use, intrinsic :: ISO_Fortran_env,  only : wp=>real64
 use newton, only : newtopts,newton_exact,objfun,objfun_deriv
+
+implicit none (type, external)
+
 ! use phys_consts, only : wp,Re,pi
 real(wp), parameter :: Re=6370e3
 real(wp), parameter :: pi=3.141592
-
 
 ! magnetic pole location
 real(wp), private, parameter :: thetan=11*pi/180
 real(wp), private, parameter :: phin=289*pi/180
 
 !! procedure declarations needed???
-interface
-  module procedure(objfun) :: rpoly
-end interface
-interface
-  module procedure(objfun_derv) :: rpoly_deriv
-end interface
+!interface
+!  real(wp) module procedure(objfun) :: rpoly(x,parms) result(fval)
+!    real(wp), intent(in) :: x
+!    real(wp), dimension(:), intent(in) :: parms
+!  end procedure rpoly
+!end interface
+!interface
+!  real(wp) module procedure(objfun_derv) :: rpoly_deriv(x,parms) result(fval_deriv)
+!    real(wp), intent(in) :: x
+!    real(wp), dimension(:), intent(in) :: parms
+!  end procedure rpoly
+!end interface
 
 contains
 
@@ -34,7 +42,7 @@ subroutine qp2rtheta(q,p,r,theta)
   procedure(objfun_deriv), pointer :: fprime
   integer :: maxrestart, maxr, r0step
   type(newtopts) :: newtparms
-  integer :: it
+  integer :: it,ir0
   logical :: converged
 
 
@@ -54,38 +62,38 @@ subroutine qp2rtheta(q,p,r,theta)
   r=0; converged=.false.; ir0=1;
   do while (.not. converged .and. ir0<maxrestart .and. (r<=0 .or. r>maxr))
     r0=(ir0-1)*(r0step)    ! change starting point in increments of 0.25 Re until we get a "good" answer
-    call newton_exact(f,fprime,r0,parms,newtparms,root,it,converged)
+    call newton_exact(f,fprime,r0,parms,newtparms,r,it,converged)
     ir0=ir0+1
   end do
 
 end subroutine qp2rtheta
 
 
-elemental real(8) function qr2theta(q,r) result(theta)
+elemental real(wp) function qr2theta(q,r) result(theta)
   real(wp), intent(in) :: q,r
 
   theta=acos(q*(r/Re)**2)
 end function qr2theta
 
 
-!subroutine rtheta2qp(r,theta,q,p)
-!  real(wp), intent(in) :: r,theta
-!  real(wp), intent(out) :: q,p
-!
-!
-!end subroutine rtheta2qp
+real(wp) function rpoly(x,parms) result(fval)
+  real(wp), intent(in) :: x
+  real(wp), dimension(:), intent(in) :: parms
+  real(wp) ::  q,p
 
-
-module procedure rpoly
   q=parms(1); p=parms(2);
   fval=q**2*(x/Re)**4 + 1/p*(x/Re) - 1
-end procedure rpoly
+end function rpoly
 
 
-module procedure rpoly_deriv
+real(wp) function rpoly_deriv(x,parms) result(fval_deriv)
+  real(wp), intent(in) :: x
+  real(wp), dimension(:), intent(in) :: parms
+  real(wp) :: q,p
+
   q=parms(1); p=parms(2);
-  fprimeval=4/Re*q**2*(x/Re)**3 + 1/p/Re
-end procedure rpoly_deriv
+  fval_deriv=4/Re*q**2*(x/Re)**3 + 1/p/Re
+end function rpoly_deriv
 
 
 end module dipole
