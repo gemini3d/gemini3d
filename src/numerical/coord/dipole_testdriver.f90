02d268c914d0d60e867b38970bf8ccfd844a7f2d
Matthew D. Zettergren <mattzett@gmail.com>
2021-03-25 16:44:40 -0400

    updating coordinate conversion tests

--- src/numerical/coord/dipole_testdriver.f90
+++ src/numerical/coord/dipole_testdriver.f90
@@ -1,18 +1,18 @@
 program newton_testdriver
 
 use, intrinsic :: ISO_Fortran_env,  only : wp=>real64
-use dipole, only : qp2rtheta,rtheta2qp
+use dipole, only : get_rtheta_2D,get_qp_2D
 
 implicit none
 
-integer, parameter :: lq=16,lp=16
+integer, parameter :: lq=1024,lp=1024
 real(wp), dimension(lq) :: q
 real(wp), dimension(lp) :: p
 real(wp), dimension(2) :: qlims=[-0.5851937,0.5851937]
 real(wp), dimension(2) :: plims=[1.2053761,1.5820779]
 real(wp), dimension(lq,lp) :: r,theta,qtest,ptest,errq,errp
 real(wp) :: maxerrq,maxerrp
-integer :: iq,ip
+integer :: iq,ip,irepeat
 
 
 ! define a grid
@@ -21,18 +21,12 @@ p=[(plims(1) + (plims(2)-plims(1)/lp*(ip-1)),ip=1,lp)]
 
 
 ! test the conversion of a set of grid points
-do iq=1,lq
-  do ip=1,lp
-    call qp2rtheta(q(iq),p(ip),r(iq,ip),theta(iq,ip))
-  end do
+do irepeat=1,3    ! do mulitple times as would be require for a full grid generation (to compare times against scripts)
+  call get_rtheta_2D(q,p,r,theta)
 end do
 
 ! now convert back to dipole coords so we can check
-do iq=1,lq
-  do ip=1,lp
-    call rtheta2qp(r(iq,ip),theta(iq,ip),qtest(iq,ip),ptest(iq,ip))
-  end do
-end do
+call get_qp_2D(r,theta,qtest,ptest)
 
 ! now compute errors for each conversion that has been done
 do iq=1,lq
@@ -45,7 +39,8 @@ end do
 ! provide some indication to the call program as to whether this test passed or not
 maxerrq=maxval(abs(errq))
 maxerrp=maxval(abs(errp))
+print*, ' Test system size:  '
 print*, ' Maximum error in computed q and p:  ',maxerrq,maxerrp
-if (maxerrq>1e-4_wp .or. maxerrp>1e-4_wp) error stop ' Excessive error in coordinate conversion!'
+if (maxerrq>1e-6_wp .or. maxerrp>1e-6_wp) error stop ' Excessive error in coordinate conversion!'
 
 end program newton_testdriver
