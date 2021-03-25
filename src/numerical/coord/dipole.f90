20c2ffc323bfdb8e7ab012d26dd18ab852b831ca
Matthew D. Zettergren <mattzett@gmail.com>
2021-03-25 18:04:23 -0400

    added metric factors and unit vector to dipole mesh lib

--- src/numerical/coord/dipole.f90
+++ src/numerical/coord/dipole.f90
@@ -1,6 +1,8 @@
 module dipole
 
 !> Contains data and subroutines for converting between dipole coordinates and other coordinate systems
+! Notes:
+! 1) may want to overload the calculation procedures (e.g. for metric factors) so that can accept an index or a spherical coordiante input.
 
 ! uses
 use, intrinsic :: ISO_Fortran_env,  only : wp=>real64
@@ -18,10 +20,158 @@ real(wp), private, parameter :: phin=289*pi/180
 
 type(newtopts), public :: newtparms
 
-public :: get_rtheta_2D,get_qp_2D,qp2rtheta,rtheta2qp,rpoly,rpoly_deriv
-private :: qr2theta
+!> these to be dealt with later
+!public :: get_rtheta_2D,get_qp_2D,qp2rtheta,rtheta2qp,rpoly,rpoly_deriv,hq
+!private :: qr2theta
+
+!> overload interfaces for unit vectors since we can't handle these with elementals...
+interface er
+  module procedure er_scalar
+  module procedure er_rank3
+end interface
+interface etheta
+  module procedure etheta_scalar
+  module procedure etheta_rank3
+end interface etheta
+interface ephi
+  module procedure ephi_scalar
+  module procedure ephi_rank3
+end interface ephi
+interface eq
+  module procedure eq_scalar
+  module procedure eq_rank3
+end interface eq
+interface ep
+  module procedure ep_scalar
+  module procedure ep_rank3
+end interface ep
 
 contains
+
+!> compute a metric factor for q corresponding to a given r,theta,phi ordered triple
+elemental real(wp) function hq(r,theta,phi)
+  real(wp), intent(in) :: r,theta,phi
+
+  hq=r**3/Re**2/(sqrt(1+3*cos(theta)**2))
+end function hq
+
+!> compute p metric factor
+elemental real(wp) function hp(r,theta,phi)
+  real(wp), intent(in) :: r,theta,phi
+
+  hp=Re*sin(theta)**3/(sqrt(1+3*cos(theta)**2))
+end function hp
+
+!> compute phi metric factor
+elemental real(wp) function hphi(r,theta,phi)
+  real(wp), intent(in) :: r,theta,phi
+
+  hphi=r*sin(theta)
+end function hphi
+
+
+!> radial unit vector (expressed in ECEF cartesian coodinates, permuted as ix,iy,iz)
+function er_scalar(r,theta,phi) result(er)
+  real(wp), intent(in) :: r,theta,phi
+  real(wp), dimension(3) :: er
+
+  er(1)=sin(theta)*cos(phi)
+  er(2)=sin(theta)*sin(phi)
+  er(3)=cos(theta)
+end function er_scalar
+function er_rank3(r,theta,phi) result(er)
+  real(wp), dimension(:,:,:), intent(in) :: r,theta,phi
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3),3) :: er
+
+  er(:,:,:,1)=sin(theta)*cos(phi)
+  er(:,:,:,2)=sin(theta)*sin(phi)
+  er(:,:,:,3)=cos(theta)
+end function er_rank3
+
+
+!> zenith angle unit vector (expressed in ECEF cartesian coodinates
+function etheta_scalar(r,theta,phi) result(etheta)
+  real(wp), intent(in) :: r,theta,phi
+  real(wp), dimension(3) :: etheta
+
+  etheta(1)=cos(theta)*cos(phi)
+  etheta(2)=cos(theta)*sin(phi)
+  etheta(3)=-sin(theta)
+end function etheta_scalar
+function etheta_rank3(r,theta,phi) result(etheta)
+  real(wp), dimension(:,:,:), intent(in) :: r,theta,phi
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3),3) :: etheta
+
+  etheta(:,:,:,1)=cos(theta)*cos(phi)
+  etheta(:,:,:,2)=cos(theta)*sin(phi)
+  etheta(:,:,:,3)=-sin(theta)
+end function etheta_rank3
+
+
+!> azimuth angle unit vector (ECEF cart.)
+function ephi_scalar(r,theta,phi) result(ephi)
+  real(wp), intent(in) :: r,theta,phi
+  real(wp), dimension(3) :: ephi
+
+  ephi(1)=-sin(phi)
+  ephi(2)=cos(phi)
+  ephi(3)=0
+end function ephi_scalar
+function ephi_rank3(r,theta,phi) result(ephi)
+  real(wp), dimension(:,:,:), intent(in) :: r,theta,phi
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3),3) :: ephi
+
+  ephi(:,:,:,1)=-sin(phi)
+  ephi(:,:,:,2)=cos(phi)
+  ephi(:,:,:,3)=0
+end function ephi_rank3
+
+
+!> unit vector in the q direction
+function eq_scalar(r,theta,phi) result(eq)
+  real(wp), intent(in) :: r,theta,phi
+  real(wp), dimension(3) :: eq
+  real(wp) :: denom  
+
+  denom=sqrt(1+3*cos(theta)**2)
+  eq(1)=-3*cos(theta)*sin(theta)*cos(phi)/denom
+  eq(2)=-3*cos(theta)*sin(theta)*sin(phi)/denom
+  eq(3)=(1-3*cos(theta)**2)/denom   !simplify?
+end function eq_scalar
+function eq_rank3(r,theta,phi) result(eq)
+  real(wp), dimension(:,:,:), intent(in) :: r,theta,phi
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3),3) :: eq
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3)) :: denom  
+
+  denom=sqrt(1+3*cos(theta)**2)
+  eq(:,:,:,1)=-3*cos(theta)*sin(theta)*cos(phi)/denom
+  eq(:,:,:,2)=-3*cos(theta)*sin(theta)*sin(phi)/denom
+  eq(:,:,:,3)=(1-3*cos(theta)**2)/denom   !simplify?
+end function eq_rank3
+
+
+!> unit vector in the p direction
+function ep_scalar(r,theta,phi) result(ep)
+  real(wp), intent(in) :: r,theta,phi
+  real(wp), dimension(3) :: ep
+  real(wp) :: denom  
+
+  denom=sqrt(1+3*cos(theta)**2)
+  ep(1)=(1-3*cos(theta)**2)*cos(phi)/denom
+  ep(2)=(1-3*cos(theta)**2)*sin(phi)/denom
+  ep(3)=3*cos(theta)*sin(theta)/denom
+end function ep_scalar
+function ep_rank3(r,theta,phi) result(ep)
+  real(wp), dimension(:,:,:), intent(in) :: r,theta,phi
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3),3) :: ep
+  real(wp), dimension(1:size(r,1),1:size(r,2),1:size(r,3)) :: denom  
+
+  denom=sqrt(1+3*cos(theta)**2)
+  ep(:,:,:,1)=(1-3*cos(theta)**2)*cos(phi)/denom
+  ep(:,:,:,2)=(1-3*cos(theta)**2)*sin(phi)/denom
+  ep(:,:,:,3)=3*cos(theta)*sin(theta)/denom
+end function ep_rank3
+
 
 !> convert a 1D arrays of q,p (assumed to define a 2D grid) into r,theta on a 2D mesh
 subroutine get_rtheta_2D(q,p,r,theta)
@@ -72,7 +222,6 @@ subroutine qp2rtheta(q,p,r,theta)
   integer :: it,ir0
   logical :: converged
 
-
   ! Set parameters of the restart and Newton iterations
   maxrestart=400
   maxr=100*Re
@@ -109,12 +258,14 @@ subroutine rtheta2qp(r,theta,q,p)
 
 end subroutine rtheta2qp
 
+
 !> find theta given q,r
 elemental real(wp) function qr2theta(q,r) result(theta)
   real(wp), intent(in) :: q,r
 
   theta=acos(q*(r/Re)**2)
 end function qr2theta
+
 
 !> objective function for newton iterations for solutions of roots for r
 real(wp) function rpoly(x,parms) result(fval)
@@ -125,6 +276,7 @@ real(wp) function rpoly(x,parms) result(fval)
   q=parms(1); p=parms(2);
   fval=q**2*(x/Re)**4 + 1/p*(x/Re) - 1
 end function rpoly
+
 
 !> derivative objective function for newton iterations for roots of r
 real(wp) function rpoly_deriv(x,parms) result(fval_deriv)
