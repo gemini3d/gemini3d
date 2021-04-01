08fabf2f18f05b31783480f5a3cae698ec8f4849
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-01 01:56:16 -0400

    glow: externalProject

--- cmake/ext_libs/glow.cmake
+++ cmake/ext_libs/glow.cmake
@@ -6,16 +6,30 @@ if(glow_FOUND)
   return()
 endif()
 
-FetchContent_Declare(GLOW
-  GIT_REPOSITORY ${glow_git}
-  GIT_TAG ${glow_tag})
-
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.14)
-  FetchContent_MakeAvailable(GLOW)
-elseif(NOT glow_POPULATED)
-  FetchContent_Populate(GLOW)
-  add_subdirectory(${glow_SOURCE_DIR} ${glow_BINARY_DIR})
+if(NOT DEFINED GLOW_ROOT)
+  set(GLOW_ROOT ${PROJECT_BINARY_DIR}/glow)
 endif()
 
-file(MAKE_DIRECTORY ${glow_BINARY_DIR}/include)
+set(GLOW_LIBRARIES
+${GLOW_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}glow${CMAKE_STATIC_LIBRARY_SUFFIX}
+${GLOW_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}msis00${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(GLOW_INCLUDE_DIRS ${GLOW_ROOT}/include)
+
+ExternalProject_Add(GLOW
+GIT_REPOSITORY ${glow_git}
+GIT_TAG ${glow_tag}
+INACTIVITY_TIMEOUT 30
+CONFIGURE_HANDLED_BY_BUILD ON
+CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${GLOW_ROOT} -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING:BOOL=false
+BUILD_BYPRODUCTS ${GLOW_LIBRARIES}
+)
+
+file(MAKE_DIRECTORY ${GLOW_INCLUDE_DIRS})
 # avoid generate race condition
+
+add_library(glow::glow INTERFACE IMPORTED)
+target_link_libraries(glow::glow INTERFACE ${GLOW_LIBRARIES})
+target_include_directories(glow::glow INTERFACE ${GLOW_INCLUDE_DIRS})
+target_compile_definitions(glow::glow INTERFACE DATADIR="${GLOW_ROOT}/share/data/glow/")
+
+add_dependencies(glow::glow GLOW)
