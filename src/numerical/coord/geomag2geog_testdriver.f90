1dc688cebecbabf0d742471b37f4e5a2faf31d0a
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2021-04-08 15:01:22 -0400

    added test for geographic conversions

---
+++ src/numerical/coord/geomag2geog_testdriver.f90
@@ -0,0 +1,64 @@
+program geomag2geog_testdriver
+
+use phys_consts, only: wp,Re,pi
+use meshobj_dipole, only : geomag2geog,geog2geomag,r2alt,alt2r
+
+implicit none
+
+integer, parameter :: lr=128,ltheta=192,lphi=96
+integer :: ir,itheta,iphi
+real(wp), dimension(2) :: rlims, thetalims, philims
+real(wp), dimension(lr) :: rarr
+real(wp), dimension(ltheta) :: thetaarr
+real(wp), dimension(lphi) :: phiarr
+real(wp), dimension(lr,ltheta,lphi) :: r,theta,phi
+real(wp), dimension(lr,ltheta,lphi) :: glon,glat,alt
+real(wp), dimension(lr,ltheta,lphi) :: rtest,thetatest,phitest
+real(wp) :: r0,theta0,phi0,glon0,glat0,alt0
+real(wp) :: r0test,theta0test,phi0test
+real(wp) :: maxerrr,maxerrtheta,maxerrphi
+
+!> define points of interest
+r0=Re+300e3
+theta0=pi/2
+phi0=pi
+rlims=[r0,r0+300e3]
+thetalims=[theta0,theta0+pi/6]
+philims=[phi0,phi0+pi/4]
+rarr=[(r0 + (rlims(2)-rlims(1))/lr*(ir-1),ir=1,lr)]
+thetaarr=[(theta0 + (thetalims(2)-thetalims(1))/ltheta*(itheta-1),itheta=1,ltheta)]
+phiarr=[(phi0 + (philims(2)-philims(1))/lphi*(iphi-1),iphi=1,lphi)]
+do ir=1,lr
+  do itheta=1,ltheta
+    do iphi=1,lphi
+      r(ir,itheta,iphi)=rarr(ir)
+      theta(ir,itheta,iphi)=thetaarr(itheta)
+      phi(ir,itheta,iphi)=phiarr(iphi)
+    end do
+  end do
+end do
+
+!> test the conversion of a single point
+call geomag2geog(phi0,theta0,glon0,glat0)
+alt0=r2alt(r0)
+call geog2geomag(glon0,glat0,phi0test,theta0test)
+r0test=alt2r(alt0)
+print*, ' phi,theta coordinate diff:  ',abs(phi0-phi0test),abs(theta0-theta0test),abs(r0-r0test)
+if (abs(phi0-phi0test)>1e-6 .or. abs(theta0-theta0test)>1e-6 .or. abs(r0-r0test)>1e-6) then
+  error stop ' excessive error in single point coordinate conversion'
+end if
+
+!> test a 3D array conversion
+call geomag2geog(phi,theta,glon,glat)
+alt=r2alt(r)
+call geog2geomag(glon,glat,phitest,thetatest)
+rtest=alt2r(alt)
+maxerrr=maxval(abs(rtest-r))
+maxerrtheta=maxval(abs(thetatest-theta))
+maxerrphi=maxval(abs(phitest-phi))
+
+if ( any([maxerrr,maxerrtheta,maxerrphi]>1e-6) ) then
+  error stop ' Excessive error in array coordinate conversion!'
+end if
+
+end program geomag2geog_testdriver
