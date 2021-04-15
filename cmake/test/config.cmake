8e2afb910be83d0c7df6048e8de2aa050b7e3fa8
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-15 11:04:17 -0400

    fixture name homogenize

--- cmake/test/config.cmake
+++ cmake/test/config.cmake
@@ -9,10 +9,10 @@ set(refroot ${PROJECT_SOURCE_DIR}/test_data)
 set(refdir ${refroot}/test${name})
 
 add_test(NAME gemini:${name}:setup
-  COMMAND ${CMAKE_COMMAND} -Dname=${name} -Doutdir:PATH=${outdir} -Drefroot:PATH=${refroot} -P ${CMAKE_CURRENT_LIST_DIR}/download.cmake)
+  COMMAND ${CMAKE_COMMAND} -Dname=${name} -Doutdir:PATH=${outdir} -Drefroot:PATH=${refroot} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/download.cmake)
 set_tests_properties(gemini:${name}:setup PROPERTIES
-  FIXTURES_SETUP ${name}_setup
-  FIXTURES_REQUIRED gemini_exe_fix
+  FIXTURES_SETUP ${name}:setup
+  FIXTURES_REQUIRED gemini_exe_fxt
   LABELS setup
   TIMEOUT 180)
 
@@ -40,7 +40,7 @@ add_test(NAME gemini:hdf5:${name}:dryrun
 set_tests_properties(gemini:hdf5:${name}:dryrun PROPERTIES
   TIMEOUT 60
   RESOURCE_LOCK cpu_mpi
-  FIXTURES_REQUIRED ${name}_setup
+  FIXTURES_REQUIRED ${name}:setup
   FIXTURES_SETUP hdf5:${name}:dryrun
   REQUIRED_FILES ${outdir}/inputs/config.nml
   LABELS core)
@@ -65,7 +65,7 @@ add_test(NAME gemini:netcdf:${name}:dryrun
 set_tests_properties(gemini:netcdf:${name}:dryrun PROPERTIES
   TIMEOUT 60
   RESOURCE_LOCK cpu_mpi
-  FIXTURES_REQUIRED "mumps_fixture;${name}_setup"
+  FIXTURES_REQUIRED "mumps_fxt;${name}:setup"
   FIXTURES_SETUP netcdf:${name}:dryrun
   REQUIRED_FILES ${outdir}/inputs/config.nml
   LABELS core)
