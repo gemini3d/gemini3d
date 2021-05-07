8620251a8c836d60984e3924af4e9faee0e1951e
Michael Hirsch <scivision@users.noreply.github.com>
2021-05-07 16:56:37 -0400

    netcdf external project

--- cmake/ext_libs/nc4fortran.cmake
+++ cmake/ext_libs/nc4fortran.cmake
@@ -1,20 +1,49 @@
 # Leave nc4fortran as FetchContent as we use wrangle NetCDF library distinctions there
-include(FetchContent)
+include(ExternalProject)
 
 if(netcdf)
-  set(nc4fortran_BUILD_TESTING false CACHE BOOL "h5fortran no test")
-
   find_package(nc4fortran CONFIG)
   if(nc4fortran_FOUND)
-    find_package(NetCDF COMPONENTS Fortran REQUIRED)
     return()
   endif()
 
-  FetchContent_Declare(NC4FORTRAN
-  GIT_REPOSITORY ${nc4fortran_git}
-  GIT_TAG ${nc4fortran_tag})
+  find_package(NetCDF REQUIRED COMPONENTS Fortran)
 
-  FetchContent_MakeAvailable(NC4FORTRAN)
+  if(NOT nc4fortran_ROOT)
+    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+        set(nc4fortran_ROOT ${PROJECT_BINARY_DIR})
+      else()
+        set(nc4fortran_ROOT ${CMAKE_INSTALL_PREFIX})
+    endif()
+  endif()
+
+  if(NOT DEFINED NetCDF_ROOT)
+    set(NetCDF_ROOT ${NetCDF_C_INCLUDE_DIRS}/..)
+  endif()
+  message(VERBOSE "NetCDF_ROOT: ${NetCDF_ROOT}")
+
+  set(nc4fortran_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/include)
+  set(nc4fortran_LIBRARIES ${nc4fortran_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}nc4fortran${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+  ExternalProject_Add(NC4FORTRAN
+    GIT_REPOSITORY ${nc4fortran_git}
+    GIT_TAG ${nc4fortran_tag}
+    CMAKE_ARGS -DNetCDF_ROOT:PATH=${NetCDF_ROOT} -DCMAKE_INSTALL_PREFIX:PATH=${nc4fortran_ROOT} -DBUILD_SHARED_LIBS:BOOL=false -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING:BOOL=false
+    BUILD_BYPRODUCTS ${nc4fortran_LIBRARIES}
+    INACTIVITY_TIMEOUT 30
+    CONFIGURE_HANDLED_BY_BUILD ON
+    )
+
+  file(MAKE_DIRECTORY ${nc4fortran_INCLUDE_DIRS})
+
+  add_library(nc4fortran::nc4fortran INTERFACE IMPORTED)
+  target_link_libraries(nc4fortran::nc4fortran INTERFACE ${nc4fortran_LIBRARIES})
+  target_include_directories(nc4fortran::nc4fortran INTERFACE ${nc4fortran_INCLUDE_DIRS})
+
+  # race condition for linking without this
+  add_dependencies(nc4fortran::nc4fortran NC4FORTRAN)
+
+  target_link_libraries(nc4fortran::nc4fortran INTERFACE NetCDF::NetCDF_Fortran)
 
 else(netcdf)
   message(VERBOSE " using nc4fortran dummy")
