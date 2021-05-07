c4509cfc687d1d1a55cdc2b4e2ed615107dd7139
Michael Hirsch <scivision@users.noreply.github.com>
2021-05-07 18:32:13 -0400

    use sha256 msis20

--- cmake/config/libraries.cmake
+++ cmake/config/libraries.cmake
@@ -4,7 +4,7 @@ list(APPEND names h5fortran hdf5 zlib glow lapack hwm14 msis2 mumps nc4fortran s
 file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json _libj)
 
 foreach(n ${names})
-  foreach(t url git tag zip sha1 sha256)
+  foreach(t url git tag zip sha256)
     string(JSON m ERROR_VARIABLE e GET ${_libj} ${n} ${t})
     if(m)
       set(${n}_${t} ${m})
