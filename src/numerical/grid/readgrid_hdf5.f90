31ab6ca669c0d0a9f58b993ce644517b6933d85e
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-04 16:41:26 -0400

    read_grid: error stop if glatctr,glonctr not present

--- src/numerical/grid/readgrid_hdf5.f90
+++ src/numerical/grid/readgrid_hdf5.f90
@@ -10,33 +10,33 @@ contains
 
 module procedure get_grid3_coords_hdf5
 
-  type(hdf5_file) :: hout
+  type(hdf5_file) :: hf
   character(:), allocatable :: fn
-  
+
   if (index(path, 'simgrid.h5') /= 0) then
     fn = path
   else
     fn = path // '/simgrid.h5'
   endif
   if (debug) print '(A,/,A)', 'READ 3D (B-parallel, B-perp, B-perp) grid:', fn
-  
-  call hout%initialize(fn, status='old',action='r')
-  
-  !> reads common to 2D and 3D
-  
-  !> 1-D variables
-  call hout%read('/x1', x1)
-  call hout%read('/x2', x2all)
-  call hout%read('/x3', x3all)
 
-  if (hout%exist('/glonctr')) then
-    call hout%read('/glonctr',glonctr)
-    call hout%read('/glatctr',glatctr)
-  else
-    print*, ' !!!!!!!!!!!!!!!!!!!!!!!  Default center grid, this is probably bad !!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    glonctr=0._wp
-    glatctr=0._wp
-  end if
+  call hf % initialize(fn, status='old',action='r')
+
+  !> reads common to 2D and 3D
+
+  !> 1-D variables
+  call hf%read('/x1', x1)
+  call hf%read('/x2', x2all)
+  call hf%read('/x3', x3all)
+
+  if (.not. (hf%exist('/glonctr') .and. &
+             hf%exist('/glatctr'))) error stop "please specify /glonctr and /glatctr in " // fn
+
+  call hf%read('/glonctr',glonctr)
+  call hf%read('/glatctr',glatctr)
+
+  call hf % finalize()
+
 end procedure get_grid3_coords_hdf5
 
 
@@ -44,81 +44,81 @@ end procedure get_grid3_coords_hdf5
 !
 !  type(hdf5_file) :: hout
 !  character(:), allocatable :: fn
-!  
+!
 !  if (index(path, 'simgrid.h5') /= 0) then
 !    fn = path
 !  else
 !    fn = path // '/simgrid.h5'
 !  endif
 !  if (debug) print '(A,/,A)', 'READ 3D (B-parallel, B-perp, B-perp) grid:', fn
-!  
+!
 !  call hout%initialize(fn, status='old',action='r')
-!  
+!
 !  !> reads common to 2D and 3D
-!  
+!
 !  !> 1-D variables
 !  call hout%read('/x1', x%x1)
 !  call hout%read('/x1i', x%x1i)
 !  call hout%read('/dx1b', x%dx1)
 !  call hout%read('/dx1h', x%dx1i)
-!  
+!
 !  if (flagswap/=1) then
 !    !! normal (i.e. full 3D) grid ordering, or a 2D grid with 1 element naturally in the second dimension
-!  
+!
 !    call hout%read('/x2', x%x2all)
 !    call hout%read('/x2i', x%x2iall)
 !    call hout%read('/dx2b', x%dx2all)
 !    call hout%read('/dx2h', x%dx2iall)
-!  
+!
 !    call hout%read('/x3', x%x3all)
 !    call hout%read('/x3i', x%x3iall)
 !    call hout%read('/dx3b', x%dx3all)
 !    call hout%read('/dx3h', x%dx3iall)
-!  
+!
 !    !> 3D variables
 !    call hout%read('/h1', x%h1all)
 !    call hout%read('/h2', x%h2all)
 !    call hout%read('/h3', x%h3all)
-!  
+!
 !    call hout%read('/h1x1i', x%h1x1iall)
 !    call hout%read('/h2x1i', x%h2x1iall)
 !    call hout%read('/h3x1i', x%h3x1iall)
-!  
+!
 !    call hout%read('/h1x2i', x%h1x2iall)
 !    call hout%read('/h2x2i', x%h2x2iall)
 !    call hout%read('/h3x2i', x%h3x2iall)
-!  
+!
 !    call hout%read('/h1x3i', x%h1x3iall)
 !    call hout%read('/h2x3i', x%h2x3iall)
 !    call hout%read('/h3x3i', x%h3x3iall)
-!  
+!
 !    call hout%read('/gx1', g1all)
 !    call hout%read('/gx2', g2all)
 !    call hout%read('/gx3', g3all)
-!  
+!
 !    call hout%read('/alt', x%altall)
 !    call hout%read('/glat', glatall)
 !    call hout%read('/glon', x%glonall)
-!  
+!
 !    call hout%read('/Bmag', x%Bmagall)
 !    call hout%read('/I', Incall)
 !    call hout%read('/nullpts', nullptsall)
-!  
+!
 !    call hout%read('/e1', e1all)
 !    call hout%read('/e2', e2all)
 !    call hout%read('/e3', e3all)
-!  
+!
 !    call hout%read('/er', erall)
 !    call hout%read('/etheta', ethetaall)
 !    call hout%read('/ephi', ephiall)
-!  
+!
 !    call hout%read('/r', rall)
 !    call hout%read('/theta', thetaall)
 !    call hout%read('/phi', phiall)
-!  
+!
 !  else
 !    !! 2D with swapped axes
-!  
+!
 !    call hout%read('/x2', x%x3all)
 !    call hout%read('/x2i', x%x3iall)
 !    call hout%read('/dx2b', x%dx3all)
@@ -129,7 +129,7 @@ end procedure get_grid3_coords_hdf5
 !    call hout%read('/dx3b', x%dx2all)
 !    call hout%read('/dx3h', x%dx2iall)
 !    !! formerly x3, now x2
-!  
+!
 !    block
 !    !> NOTE: workaround for Intel 2020, may not really be a bug
 !    !> notice this is the only one with negative indices
@@ -137,7 +137,7 @@ end procedure get_grid3_coords_hdf5
 !    real(wp), allocatable :: htmp(:,:,:)
 !    allocate(htmp(-1:lx1+2,-1:lx3all+2,-1:lx2all+2))
 !    !! end workaround
-!  
+!
 !    call hout%read('/h1', htmp)
 !    x%h1all = reshape(htmp, [lx1+4,lx2all+4,lx3all+4], order=[1,3,2])
 !    call hout%read('/h2', htmp)
@@ -148,7 +148,7 @@ end procedure get_grid3_coords_hdf5
 !    !! this would be h3, but with the input structure shape
 !    x%h2all = reshape(htmp, [lx1+4,lx2all+4,lx3all+4], order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(1:lx1+1,1:lx3all, 1) :: htmp
@@ -161,7 +161,7 @@ end procedure get_grid3_coords_hdf5
 !    call hout%read('/h3x1i', htmp)
 !    x%h2x1iall = reshape(htmp,[lx1+1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(1:lx1,1:lx3all+1, 1) :: htmp
@@ -173,7 +173,7 @@ end procedure get_grid3_coords_hdf5
 !    call hout%read('/h3x2i', htmp)
 !    x%h2x3iall = reshape(htmp,[lx1,lx2all,lx3all+1],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(1:lx1,1:lx3all,1:lx2all+1) :: htmp
 !    call hout%read('/h1x3i', htmp)
@@ -183,7 +183,7 @@ end procedure get_grid3_coords_hdf5
 !    call hout%read('/h3x3i', htmp)
 !    x%h2x2iall = reshape(htmp,[lx1,lx2all+1,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(lx1, lx3all, 1) :: htmp
@@ -193,37 +193,37 @@ end procedure get_grid3_coords_hdf5
 !    g3all = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    call hout%read('/gx3', htmp)
 !    g2all = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
-!  
+!
 !    call hout%read('/alt', htmp)
 !    x%altall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    call hout%read('/glat', htmp)
 !    glatall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    call hout%read('/glon', htmp)
 !    x%glonall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
-!  
+!
 !    call hout%read('/Bmag', htmp)
 !    x%Bmagall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(lx3all, 1) :: htmp
 !    call hout%read('/I', htmp)
 !    Incall = reshape(htmp,[lx2all,lx3all],order=[2,1])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(lx1, lx3all, 1) :: htmp
 !    call hout%read('/nullpts', htmp)
 !    nullptsall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(lx1,lx3all,lx2all,3) :: htmp
 !    call hout%read('/e1', htmp)
 !    e1all = reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
-!  
+!
 !    !> swap the x2/x3 unit vectors
 !    call hout%read('/e2', htmp)
 !    e3all = reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
@@ -236,7 +236,7 @@ end procedure get_grid3_coords_hdf5
 !    call hout%read('/ephi', htmp)
 !    ephiall = reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
 !    end block
-!  
+!
 !    block
 !    !! htmp includes degenerate dimension
 !    real(wp), dimension(lx1, lx3all, 1) :: htmp
@@ -248,9 +248,9 @@ end procedure get_grid3_coords_hdf5
 !    phiall = reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
 !  endif
-!  
+!
 !  call hout%finalize()
-!  
+!
 !  x%rall = rall
 !  x%thetaall = thetaall
 !  x%phiall = phiall
--- src/numerical/grid/readgrid_raw.f90
+++ src/numerical/grid/readgrid_raw.f90
@@ -11,7 +11,7 @@ module procedure get_grid3_coords_raw
 
   integer :: u
   character(:), allocatable :: fn
-  integer :: stat  
+  integer :: stat
 
   if (index(path, 'simgrid.dat') /= 0) then
     fn = path
@@ -19,9 +19,9 @@ module procedure get_grid3_coords_raw
     fn = path // '/simgrid.dat'
   endif
   if (debug) print '(A,/,A)', 'READ 3D (B-parallel, B-perp, B-perp) grid:', fn
-  
+
   open(newunit=u, file=fn, status='old', form='unformatted', access='stream', action='read')
-  
+
   !! normal (i.e. full 3D) grid ordering, or a 2D grid with 1 element naturally in the second dimension
   read(u) x1
   read(u) x2all
@@ -29,13 +29,11 @@ module procedure get_grid3_coords_raw
 
   !! check whether center location is include in file
   read(u,iostat=stat) glonctr
-  if (stat<0) then
-    read(u) glatctr
-  else
-    print*, 'Default center grid'
-    glonctr=0._wp
-    glatctr=0._wp 
-  end if
+  if (stat/=0) error stop "please specify /glonctr and /glatctr in " // fn
+  read(u) glatctr
+
+  close(u)
+
 end procedure get_grid3_coords_raw
 
 
@@ -43,16 +41,16 @@ end procedure get_grid3_coords_raw
 !
 !  integer :: u
 !  character(:), allocatable :: fn
-!  
+!
 !  if (index(path, 'simgrid.dat') /= 0) then
 !    fn = path
 !  else
 !    fn = path // '/simgrid.dat'
 !  endif
 !  if (debug) print '(A,/,A)', 'READ 3D (B-parallel, B-perp, B-perp) grid:', fn
-!  
+!
 !  open(newunit=u, file=fn, status='old', form='unformatted', access='stream', action='read')
-!  
+!
 !  if (flagswap/=1) then
 !    !! normal (i.e. full 3D) grid ordering, or a 2D grid with 1 element naturally in the second dimension
 !    read(u) x%x1,x%x1i,x%dx1,x%dx1i
@@ -62,7 +60,7 @@ end procedure get_grid3_coords_raw
 !    read(u) x%h1x1iall,x%h2x1iall,x%h3x1iall
 !    read(u) x%h1x2iall,x%h2x2iall,x%h3x2iall
 !    read(u) x%h1x3iall,x%h2x3iall,x%h3x3iall
-!  
+!
 !    read(u) g1all,g2all,g3all
 !    read(u) x%altall
 !    read(u) glatall,x%glonall
@@ -75,7 +73,7 @@ end procedure get_grid3_coords_raw
 !    read(u) erall
 !    read(u) ethetaall
 !    read(u) ephiall
-!  
+!
 !    read(u) rall
 !    read(u) thetaall
 !    read(u) phiall
@@ -83,7 +81,7 @@ end procedure get_grid3_coords_raw
 !    read(u) x%x1,x%x1i,x%dx1,x%dx1i                !< x1 untouched
 !    read(u) x%x3all,x%x3iall,x%dx3all,x%dx3iall    !< for a 3D grid this is x2, but now considered x3(all)
 !    read(u) x%x2all,x%x2iall,x%dx2all,x%dx2iall    !< formerly x3, now x2
-!  
+!
 !    block
 !    real(wp), dimension(-1:lx1+2,-1:lx3all+2,-1:lx2all+2) :: htmp
 !    !! this stores the input metric factors which are swapped x2/x3 vs. what this simulation will use
@@ -94,7 +92,7 @@ end procedure get_grid3_coords_raw
 !    read(u) htmp        !< this would be h3, but with the input structure shape
 !    x%h2all=reshape(htmp,[lx1+4,lx2all+4,lx3all+4],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(1:lx1+1,1:lx3all,1:lx2all) :: htmp
 !    !! input 2 vs. 3 dimensions swapped from this program
@@ -105,7 +103,7 @@ end procedure get_grid3_coords_raw
 !    read(u) htmp
 !    x%h2x1iall=reshape(htmp,[lx1+1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(1:lx1,1:lx3all+1,1:lx2all) :: htmp
 !    read(u) htmp
@@ -115,7 +113,7 @@ end procedure get_grid3_coords_raw
 !    read(u) htmp
 !    x%h2x3iall=reshape(htmp,[lx1,lx2all,lx3all+1],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(1:lx1,1:lx3all,1:lx2all+1) :: htmp
 !    read(u) htmp
@@ -125,7 +123,7 @@ end procedure get_grid3_coords_raw
 !    read(u) htmp
 !    x%h2x2iall=reshape(htmp,[lx1,lx2all+1,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(lx1,lx3all,lx2all) :: htmp
 !    read(u) htmp
@@ -134,24 +132,24 @@ end procedure get_grid3_coords_raw
 !    g3all=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    read(u) htmp
 !    g2all=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
-!  
+!
 !    read(u) htmp
 !    x%altall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    read(u) htmp
 !    glatall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    read(u) htmp
 !    x%glonall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
-!  
+!
 !    read(u) htmp
 !    x%Bmagall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(lx3all,lx2all) :: htmp
 !    read(u) htmp
 !    Incall=reshape(htmp,[lx2all,lx3all],order=[2,1])
 !    end block
-!  
+!
 !      !inquire(u, pos=itell)
 !    !print *,'file pos before read',itell
 !    !read(u) nullptsall
@@ -163,22 +161,22 @@ end procedure get_grid3_coords_raw
 !    !allocate(nullptsall(lx1,lx2,lx3all))
 !    !print *,shape(nullptsall)
 !    !stop
-!  
+!
 !    ! FIXME would be like this, but this doesn't work.
 !    !allocate(nullptsall(lx1,lx2,lx3all))
 !    !read(u) nullptsall
-!  
+!
 !    block
 !    real(wp), dimension(lx1,lx3all,lx2all) :: htmp
 !    read(u) htmp
 !    nullptsall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(lx1,lx3all,lx2all,3) :: htmp
 !    read(u) htmp
 !    e1all=reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
-!  
+!
 !    !! swap the x2/x3 unit vectors
 !    read(u) htmp
 !    e3all=reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
@@ -191,7 +189,7 @@ end procedure get_grid3_coords_raw
 !    read(u) htmp
 !    ephiall=reshape(htmp,[lx1,lx2all,lx3all,3],order=[1,3,2,4])
 !    end block
-!  
+!
 !    block
 !    real(wp), dimension(lx1,lx3all,lx2all) :: htmp
 !    read(u) htmp
@@ -202,12 +200,12 @@ end procedure get_grid3_coords_raw
 !    phiall=reshape(htmp,[lx1,lx2all,lx3all],order=[1,3,2])
 !    end block
 !  endif
-!  
+!
 !  close(u)
-!  
+!
 !  x%rall=rall
 !  x%thetaall=thetaall
 !  x%phiall=phiall
-!  
+!
 !end procedure get_grid3_raw
 end submodule readgrid_raw
