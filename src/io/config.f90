cde696be5f563ba4cd00f96dbb9c3752b4db5f86
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-06-14 10:50:07 -0400

    expand_envvar: more correct indexing to avoid triggering gcc 7.5 bug

--- src/io/config.f90
+++ src/io/config.f90
@@ -170,11 +170,9 @@ integer :: i, L, istat
 character(1000) :: buf
 
 expanded = expanduser(path)
-!! in case no @var@, and to sanitize fixed width config namelist
 
 i = index(path, "@")
 if (i < 1) return
-!! nothing to expand
 
 substr = "@" // envvar // "@"
 i = index(path, substr)
@@ -185,7 +183,7 @@ if (i < 1) return
 call get_environment_variable(envvar, buf, length=L, status=istat)
 if (istat /= 0 .or. L < 1) error stop "config:expand_envvar: environment variable empty or not defined: " // envvar
 
-expanded = path(1:i - 1) // trim(adjustl(buf)) // path(i + len(substr):len(expanded))
+expanded = path(1:i - 1) // trim(adjustl(buf)) // path(i + len(substr):len(path))
 
 end function expand_envvar
 
