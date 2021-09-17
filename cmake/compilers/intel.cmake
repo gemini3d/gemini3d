ffdb61daa35d4a927b8c07847c8681a3e135612a
Michael Hirsch <scivision@users.noreply.github.com>
2021-09-17 14:48:30 -0400

    gemini_c.bin: add intel compiler options needed

--- cmake/compilers/intel.cmake
+++ cmake/compilers/intel.cmake
@@ -1,7 +1,10 @@
 add_compile_options(-xHost)  # like -march=native
 
 string(APPEND CMAKE_Fortran_FLAGS " -warn declarations -traceback")  # -warn all or -warn gets mixed with -qopenmp with CMake 3.14.2
+
 string(APPEND CMAKE_Fortran_FLAGS " -qopenmp")  # undefined reference to `omp_get_max_threads'
+string(APPEND CMAKE_C_FLAGS " -fiopenmp")  # undefined reference to `__kmpc_global_thread_num' and more similar
+
 string(APPEND CMAKE_Fortran_FLAGS " -heap-arrays")  # (is this needed on Linux?) stack overflow avoid
 #string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -check all")
 #string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -debug extended -check all -fpe0 -fp-stack-check")
