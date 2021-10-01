a93b6d50e0defcec4f3bcf19800d63b65a596eca
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-01 10:04:13 -0400

    c cli: correct manual_grid parsing

--- src/gemini_main.c
+++ src/gemini_main.c
@@ -15,16 +15,24 @@ struct params s;
 int ierr = MPI_Init(&argc, &argv);
 
 if (argc < 2) {
-  perror("please give simulation output directory e.g. ~/data/my_sim");
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3D: please give simulation output directory e.g. ~/data/my_sim\n");
   return 1;
 }
 
 int L = strlen(argv[1]);
 if(L > LMAX) {
-  fprintf(stderr, "Gemini3D simulation output directory: path length > %d", LMAX);
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3D simulation output directory: path length > %d\n", LMAX);
   return 1;
 }
 L++; // for null terminator
+
+if(strcmp(argv[1], "-") == 0) {
+  MPI_Finalize();
+  fprintf(stderr, "Gemini3d: first argument must be output directory\n");
+  return 1;
+}
 
 strcpy(s.out_dir, argv[1]);
 
@@ -38,21 +46,19 @@ for (int i = 2; i < argc; i++) {
   if (strcmp(argv[i], "-dryrun") == 0) s.dryrun = true;
   if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-help") == 0) {
     MPI_Finalize();
-    printf("Gemini3D - C-frontend for ionospheric 3D simulation\n");
+    printf("Gemini3D: C-frontend for ionospheric 3D simulation\n");
     return 0;
   }
   if (strcmp(argv[i], "-manual_grid") == 0) {
-    lid2in = atoi(argv[i]);
     if (argc < i+1) {
-      perror("-manual_grid lid2in lid3in");
+      MPI_Finalize();
+      fprintf(stderr, "-manual_grid lid2in lid3in\n");
       return 1;
     }
+    lid2in = atoi(argv[i]);
+    lid3in = atoi(argv[i+1]);
   }
 }
-
-
-
-
 
 gemini_main(s, &lid2in, &lid3in);
 
