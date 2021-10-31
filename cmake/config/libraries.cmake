a3413cddfec8794cb8400657bac8d4438321d35c
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-31 12:31:33 -0400

    autobuild: allow requested HDF5 version

--- cmake/config/libraries.cmake
+++ cmake/config/libraries.cmake
@@ -1,7 +1,7 @@
 set(_names glow hwm14 msis2
 lapack mumps scalapack
 matgemini
-nc4fortran h5fortran hdf5)
+nc4fortran h5fortran)
 
 file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json _libj)
 
@@ -14,7 +14,7 @@ foreach(n ${_names})
   endforeach()
 endforeach()
 
-# Zlib special case to allow fallback
+# --- Zlib
 if(zlib_legacy)
   string(JSON zlib_url GET ${_libj} zlib1 url)
   string(JSON zlib_sha256 GET ${_libj} zlib1 sha256)
@@ -22,3 +22,11 @@ else()
   string(JSON zlib_url GET ${_libj} zlib2 url)
   string(JSON zlib_sha256 GET ${_libj} zlib2 sha256)
 endif()
+
+# --- HDF5
+if(NOT HDF5_VERSION)
+  set(HDF5_VERSION 1.12.1 CACHE STRING "HDF5 version built")
+endif()
+
+string(JSON hdf5_url GET ${_libj} hdf5 ${HDF5_VERSION} url)
+string(JSON hdf5_sha256 GET ${_libj} hdf5 ${HDF5_VERSION} sha256)
