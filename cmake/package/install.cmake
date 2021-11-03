ae6f3c4b69136575d7f739f36ea510fd8a585d53
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-03 16:51:44 -0400

    modernize/flatten install

--- cmake/package/install.cmake
+++ cmake/package/install.cmake
@@ -2,24 +2,26 @@
 
 include(CMakePackageConfigHelpers)
 
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
-  INSTALL_DESTINATION lib)
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/config.cmake.in
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}-config.cmake
+INSTALL_DESTINATION cmake
+)
 
 write_basic_package_version_file(
-  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
-  VERSION ${${PROJECT_NAME}_VERSION}
-  COMPATIBILITY SameMinorVersion)
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}-config-version.cmake
+COMPATIBILITY SameMinorVersion
+)
 
-install(EXPORT ${PROJECT_NAME}Targets
-  FILE ${PROJECT_NAME}Targets.cmake
-  NAMESPACE ${PROJECT_NAME}::
-  DESTINATION lib/cmake/${PROJECT_NAME})
+install(EXPORT ${PROJECT_NAME}-targets
+NAMESPACE ${PROJECT_NAME}::
+DESTINATION cmake
+)
 
 install(FILES
-  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
-  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
-  DESTINATION lib/cmake/${PROJECT_NAME})
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}-config.cmake
+${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}-config-version.cmake
+DESTINATION cmake
+)
 
 # --- CPack
 
@@ -38,6 +40,8 @@ file(READ ${CMAKE_CURRENT_LIST_DIR}/.cpack_ignore _cpack_ignore)
 string(REGEX REPLACE "\n" ";" _cpack_ignore ${_cpack_ignore})
 set(CPACK_SOURCE_IGNORE_FILES "${_cpack_ignore}")
 
-install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE} TYPE DOC)
+install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE}
+  DESTINATION share/docs/${PROJECT_NAME}
+)
 
 include(CPack)
