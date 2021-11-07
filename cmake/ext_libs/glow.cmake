557b7889ff5da3f04f1b263d1f2d5d99e425371b
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-07 14:37:00 -0500

    link objects for robustness

--- cmake/ext_libs/glow.cmake
+++ cmake/ext_libs/glow.cmake
@@ -1,7 +1,9 @@
 include(ExternalProject)
 
 find_package(glow CONFIG)
+
 if(glow_FOUND)
+  message(STATUS "GLOW found: ${glow_DIR}")
   return()
 endif()
 
@@ -13,11 +15,13 @@ endif()
 if(BUILD_SHARED_LIBS)
   set(GLOW_LIBRARIES
   ${GLOW_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}glow${CMAKE_SHARED_LIBRARY_SUFFIX}
-  ${GLOW_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}msis00${CMAKE_SHARED_LIBRARY_SUFFIX})
+  ${GLOW_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}msis00${CMAKE_SHARED_LIBRARY_SUFFIX}
+  )
 else()
   set(GLOW_LIBRARIES
   ${GLOW_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}glow${CMAKE_STATIC_LIBRARY_SUFFIX}
-  ${GLOW_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}msis00${CMAKE_STATIC_LIBRARY_SUFFIX})
+  ${GLOW_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}msis00${CMAKE_STATIC_LIBRARY_SUFFIX}
+  )
 endif()
 
 set(GLOW_INCLUDE_DIRS ${GLOW_ROOT}/include)
@@ -36,7 +40,8 @@ CMAKE_ARGS ${glow_cmake_args}
 CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
 BUILD_BYPRODUCTS ${GLOW_LIBRARIES}
 INACTIVITY_TIMEOUT 15
-CONFIGURE_HANDLED_BY_BUILD true)
+CONFIGURE_HANDLED_BY_BUILD true
+)
 
 file(MAKE_DIRECTORY ${GLOW_INCLUDE_DIRS})
 # avoid generate race condition
@@ -45,5 +50,8 @@ add_library(glow::glow INTERFACE IMPORTED)
 target_link_libraries(glow::glow INTERFACE ${GLOW_LIBRARIES})
 target_include_directories(glow::glow INTERFACE ${GLOW_INCLUDE_DIRS})
 target_compile_definitions(glow::glow INTERFACE DATADIR="${GLOW_ROOT}/share/data/glow/")
+# DATADIR comes from glow project BUILD_INTERFACE COMPILE_DEFINITIONS
+# this does not seem to work with get_target_property()
+# so preprocess instead of configure_file()
 
 add_dependencies(glow::glow GLOW)
