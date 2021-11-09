c3de5821d72dbdd0b232b54a0599f9b8d7e54676
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-09 12:12:04 -0500

    bugfix inputdata_ESFmods: subroutine declare

--- src/neutral/neutral.f90
+++ src/neutral/neutral.f90
@@ -77,14 +77,14 @@ contains
     !! intent(out)
     real(wp), dimension(:,:,:), intent(inout) :: vn1,vn2,vn3
     !! intent(out)
-  
+
     integer, dimension(3) :: ymdtmp
     real(wp) :: UTsectmp
     real(wp) :: tstart,tfin
-    
+
     !! allocation neutral module scope variables so there is space to store all the file input and do interpolations
     call make_dneu()
-    
+
     !! call msis to get an initial neutral background atmosphere
     if (mpi_cfg%myid == 0) call cpu_time(tstart)
     call neutral_atmos(cfg%ymd0,cfg%UTsec0,x%glat,x%glon,x%alt,cfg%activ,nn,Tn,cfg%msis_version,x%flagper)
@@ -92,7 +92,7 @@ contains
       call cpu_time(tfin)
       print *, 'Initial neutral density and temperature (from MSIS) at time:  ',ymd,UTsec,' calculated in time:  ',tfin-tstart
     end if
-    
+
     !> Horizontal wind model initialization/background
     if (mpi_cfg%myid == 0) call cpu_time(tstart)
     call neutral_winds(cfg%ymd0, cfg%UTsec0, Ap=cfg%activ(3), x=x, v2grid=v2grid,v3grid=v3grid,vn1=vn1, vn2=vn2, vn3=vn3)
@@ -102,13 +102,13 @@ contains
       call cpu_time(tfin)
       print *, 'Initial neutral winds (from HWM) at time:  ',ymd,UTsec,' calculated in time:  ',tfin-tstart
     end if
-  
-  
+
+
     !! perform an initialization for the perturbation quantities
     if (cfg%flagdneu==1) then
       ! set flag denoted neutral perturbations
       flagneuperturb=.true.
-  
+
       ! allocate correct type, FIXME: eventuallly no shunt to 3D
       select case (cfg%interptype)
       case (0)
@@ -120,7 +120,7 @@ contains
       case default
         error stop 'non-standard neutral interpolation type chosen in config.nml...'
       end select
-  
+
       ! call object init procedure
       call atmosperturb%init(cfg,cfg%sourcedir,x,dt,cfg%dtneu,ymd,UTsec)
     end if
@@ -128,7 +128,7 @@ contains
 
 
   !> update neutral perturbations and add to main neutral arrays
-  module subroutine neutral_perturb(cfg,dt,dtneu,t,ymd,UTsec,x,v2grid,v3grid,nn,Tn,vn1,vn2,vn3)
+  subroutine neutral_perturb(cfg,dt,dtneu,t,ymd,UTsec,x,v2grid,v3grid,nn,Tn,vn1,vn2,vn3)
     type(gemini_cfg), intent(in) :: cfg
     real(wp), intent(in) :: dt,dtneu
     real(wp), intent(in) :: t
@@ -151,34 +151,34 @@ contains
     !Add interpolated perturbations to module reference atmosphere arrays
     call neutral_update(nn,Tn,vn1,vn2,vn3,v2grid,v3grid)
   end subroutine neutral_perturb
-  
-  
+
+
   !> update density, temperature, and winds
   subroutine neutral_update(nn,Tn,vn1,vn2,vn3,v2grid,v3grid)
     !! adds stored base and perturbation neutral atmospheric parameters
     !!  these are module-scope parameters so not needed as input
     real(wp), dimension(:,:,:,:), intent(inout) :: nn
-    !! intent(out)  
+    !! intent(out)
     real(wp), dimension(:,:,:), intent(inout) :: Tn
     !! intent(out)
     real(wp), dimension(:,:,:), intent(inout) :: vn1,vn2,vn3
     !! intent(out)
     real(wp) :: v2grid,v3grid
-  
+
     call neutral_denstemp_update(nn,Tn)
     call neutral_wind_update(vn1,vn2,vn3,v2grid,v3grid)
   end subroutine neutral_update
-  
-  
+
+
   !> Adds stored base (viz. background) and perturbation neutral atmospheric density
   subroutine neutral_denstemp_update(nn,Tn)
     real(wp), dimension(:,:,:,:), intent(out) :: nn
     real(wp), dimension(:,:,:), intent(out) :: Tn
-    
+
     !> background neutral parameters
     nn=nnmsis
     Tn=Tnmsis
-    
+
     !> add perturbations, if used
     if (flagneuperturb) then
       nn(:,:,:,1)=nn(:,:,:,1)+atmosperturb%dnOinow
@@ -189,36 +189,36 @@ contains
       nn(:,:,:,3)=max(nn(:,:,:,3),1._wp)
       !! note we are not adjusting derived densities like NO since it's not clear how they may be related to major
       !! species perturbations.
-    
+
       Tn=Tn+atmosperturb%dTninow
       Tn=max(Tn,50._wp)
     end if
   end subroutine neutral_denstemp_update
-  
-  
+
+
   !> update wind variables with background and perturbation quantities
   subroutine neutral_wind_update(vn1,vn2,vn3,v2grid,v3grid)
     real(wp), dimension(:,:,:), intent(out) :: vn1,vn2,vn3
     real(wp) :: v2grid,v3grid
-    
+
     !> background neutral parameters
     vn1=vn1base
     vn2=vn2base
     vn3=vn3base
-    
+
     !> perturbations, if used
     if (flagneuperturb) then
       vn1=vn1+atmosperturb%dvn1inow
       vn2=vn2+atmosperturb%dvn2inow
       vn3=vn3+atmosperturb%dvn3inow
     end if
-    
+
     !> subtract off grid drift speed (needs to be set to zero if not lagrangian grid)
     vn2=vn2-v2grid
     vn3=vn3-v3grid
   end subroutine neutral_wind_update
-  
-  
+
+
   !> rotate winds from geographic to model native coordinate system (x1,x2,x3)
   subroutine rotate_geo2native(vnalt,vnglat,vnglon,x,vn1,vn2,vn3)
     real(wp), dimension(:,:,:), intent(in) :: vnalt,vnglat,vnglon
@@ -226,33 +226,33 @@ contains
     real(wp), dimension(:,:,:), intent(out) :: vn1,vn2,vn3
     real(wp), dimension(1:size(vnalt,1),1:size(vnalt,2),1:size(vnalt,3),3) :: ealt,eglat,eglon
     integer :: lx1,lx2,lx3
-  
+
     !> if first time called then allocate space for projections and compute
     if (.not. allocated(proj_ealt_e1)) then
       call x%calc_unitvec_geo(ealt,eglon,eglat)
       call store_geo2native_projections(x,ealt,eglon,eglat)
     end if
-  
+
     !> rotate vectors into model native coordinate system
     vn1=vnalt*proj_ealt_e1+vnglat*proj_eglat_e1+vnglon*proj_eglon_e1
     vn2=vnalt*proj_ealt_e2+vnglat*proj_eglat_e2+vnglon*proj_eglon_e2
     vn3=vnalt*proj_ealt_e3+vnglat*proj_eglat_e3+vnglon*proj_eglon_e3
   end subroutine rotate_geo2native
-  
-  
+
+
   !> compute projections for rotating winds geographic to native coordinate system
   subroutine store_geo2native_projections(x,ealt,eglon,eglat,rotmat)
     class(curvmesh), intent(in) :: x
     real(wp), dimension(:,:,:,:), intent(in) :: ealt,eglon,eglat
     real(wp), dimension(:,:,:,:,:), intent(out), optional :: rotmat    ! for debugging purposes
     integer :: ix1,ix2,ix3,lx1,lx2,lx3
-  
+
     !! allocate module-scope space for the projection factors
     lx1=size(ealt,1); lx2=size(ealt,2); lx3=size(ealt,3);
     allocate(proj_ealt_e1(lx1,lx2,lx3),proj_eglat_e1(lx1,lx2,lx3),proj_eglon_e1(lx1,lx2,lx3))
     allocate(proj_ealt_e2(lx1,lx2,lx3),proj_eglat_e2(lx1,lx2,lx3),proj_eglon_e2(lx1,lx2,lx3))
     allocate(proj_ealt_e3(lx1,lx2,lx3),proj_eglat_e3(lx1,lx2,lx3),proj_eglon_e3(lx1,lx2,lx3))
-  
+
     !! compute projections (dot products of unit vectors)
     proj_ealt_e1=sum(ealt*x%e1,4)
     proj_eglat_e1=sum(eglat*x%e1,4)
@@ -263,7 +263,7 @@ contains
     proj_ealt_e3=sum(ealt*x%e3,4)
     proj_eglat_e3=sum(eglat*x%e3,4)
     proj_eglon_e3=sum(eglon*x%e3,4)
-  
+
     !! store the rotation matrix to convert geo to native if the user wants it
     if (present(rotmat)) then
       do ix3=1,lx3
@@ -277,12 +277,12 @@ contains
       end do
     end if
   end subroutine store_geo2native_projections
-  
-  
+
+
   subroutine make_dneu()
     !allocate and compute plasma grid z,rho locations and space to save neutral perturbation variables and projection factors
     allocate(nnmsis(lx1,lx2,lx3,lnchem),Tnmsis(lx1,lx2,lx3),vn1base(lx1,lx2,lx3),vn2base(lx1,lx2,lx3),vn3base(lx1,lx2,lx3))
-    
+
     !start everyone out at zero
     nnmsis = 0
     Tnmsis = 0
@@ -290,14 +290,14 @@ contains
     vn2base = 0
     vn3base = 0
   end subroutine make_dneu
-  
-  
+
+
   subroutine clear_dneu
     ! stuff allocated at beginning of program
     deallocate(nnmsis,Tnmsis,vn1base,vn2base,vn3base)
-    
+
     if (allocated(atmosperturb)) deallocate(atmosperturb)
-    
+
     ! rotations of neutral winds
     if (allocated(proj_ealt_e1)) then
       deallocate(proj_ealt_e1,proj_eglat_e1,proj_eglon_e1)
