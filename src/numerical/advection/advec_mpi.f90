4897e696188932b0775cd3edc292ef010466fe05
Matthew D. Zettergren <mattzett@gmail.com>
2021-12-15 20:01:47 -0500

    working on refactoring advection for forestclaw

--- src/numerical/advection/advec_mpi.f90
+++ src/numerical/advection/advec_mpi.f90
@@ -202,6 +202,179 @@ subroutine advec_prep_mpi_23(isp,isperiodic,ns,rhovs1,vs1,vs2,vs3,rhoes,v1i,v2i,
 end subroutine advec_prep_mpi_23
 
 
+!> do haloing and averaging to compute cell interface velocities
+subroutine interface_vels(isp,isperiodic,vs1,vs2,vs3,v1i,v2i,v3i)
+  integer, intent(in) :: isp
+  logical, intent(in) :: isperiodic
+  real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: vs1,vs2,vs3
+  real(wp), dimension(1:size(vs1,1)-3,1:size(vs1,2)-4,1:size(vs1,3)-4), intent(inout) :: v1i
+  !! intent(out)
+  real(wp), dimension(1:size(vs1,1)-4,1:size(vs1,2)-3,1:size(vs1,3)-4), intent(inout) :: v2i
+  !! intent(out)
+  real(wp), dimension(1:size(vs1,1)-4,1:size(vs1,2)-4,1:size(vs1,3)-3), intent(inout) :: v3i
+  integer :: lx1,lx2,lx3
+  real(wp), dimension(-1:size(vs3,1)-2,-1:size(vs3,2)-2,-1:size(vs3,3)-2) :: param
+
+  lx1=size(vs1,1)-4
+  lx2=size(vs1,2)-4
+  lx3=size(vs1,3)-4
+  
+  !COMPUTE INTERFACE VELCOTIES AND APPLY LIMITING, IF NEEDED
+  v1i(2:lx1,:,:)=0.5*(vs1(1:lx1-1,1:lx2,1:lx3,isp)+vs1(2:lx1,1:lx2,1:lx3,isp))   !first the interior points
+  if (gridflag==0) then          !closed dipole grid
+    v1i(1,:,:)=vs1(1,1:lx2,1:lx3,isp)
+    v1i(lx1+1,:,:)=vs1(lx1,1:lx2,1:lx3,isp)
+  else if (gridflag==1) then     !inverted grid (assumes northern hemisphere???)
+    v1i(lx1+1,:,:)=vs1(lx1,1:lx2,1:lx3,isp)
+    !! lowest alt on grid.
+    v1i(1,:,:) = min(v1i(2,1:lx2,1:lx3), 0._wp)
+    !! highest alt; interesting that this is not vs1...
+  else                           !some type of non-inverted grid
+    v1i(1,:,:) = vs1(1,1:lx2,1:lx3,isp)
+  !!    v1i(lx1+1,:,:)=v1i(lx1,:,:)    !avoids issues with top boundary velocity spikes which may arise
+    v1i(lx1+1,:,:) = max(v1i(lx1,1:lx2,1:lx3),0._wp)
+    !! NOTE: interesting that this is not vs1...
+  end if
+
+  ! THIS TYPE OF LIMITING MAY BE NEEDED FOR VERY HIGH-ALTITUDE SIMULATIONS...
+  !    if (isp<lsp-1) then
+  !      v1i(lx1+1,:,:)=max(vs1(lx1+1,:,:,isp),-1*vellim)    !limit inflow
+  !    else if (isp==6) then
+  !      v1i(lx1+1,:,:)=min(vs1(lx1+1,:,:,isp),10.0*vellim)      !limit outflow
+  !      v1i(lx1+1,:,:)=max(v1i(lx1+1,:,:),0.0)                 !limit inflow
+  !!        vadvx1(1,:,6)=mean(vadvx1(1,:,6))                          !for eq sims
+  !    else
+  !      v1i(lx1+1,:,:)=2.0*v1i(lx1,:,:)-v1i(lx1-1,:,:)      !cleans up large current situations
+  !    end if
+
+  !> NEED TO ALSO PASS THE X2 VELOCITIES SO WE CAN COMPUTE INTERFACE VALUES
+  param=vs2(:,:,:,isp)
+  call halo(param,1,tag%vs2BC,isperiodic)
+  !! we only need one ghost cell to compute interface velocities
+  vs2(:,:,:,isp)=param
+
+  !> PASS X3 VELOCITY BOUNDARY CONDITIONS WITH GENERIC HALOING ROUTINES
+  param=vs3(:,:,:,isp)
+  call halo(param,1,tag%vs3BC,isperiodic)
+  !! we only need one ghost cell to compute interface velocities
+  vs3(:,:,:,isp)=param
+
+  !> AFTER HALOING CAN COMPUTE THE X3 INTERFACE VELOCITIES NORMALLY
+  v2i(:,1:lx2+1,:)=0.5_wp*(vs2(1:lx1,0:lx2,1:lx3,isp)+vs2(1:lx1,1:lx2+1,1:lx3,isp))
+  v3i(:,:,1:lx3+1)=0.5_wp*(vs3(1:lx1,1:lx2,0:lx3,isp)+vs3(1:lx1,1:lx2,1:lx3+1,isp))
+end subroutine interface_vels
+
+
+!> set values in global boundary ghost cells based on extrapolation, can be done by each worker
+!    without input from the root process
+subroutine set_global_boundaries(isp,isperiodic,ns,rhovs1,vs1,vs2,vs3,rhoes,v1i)
+  integer, intent(in) :: isp
+  logical, intent(in) :: isperiodic
+  real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: ns,rhovs1,vs1,vs2,vs3,rhoes
+  real(wp), dimension(1:size(vs1,1)-3,1:size(vs1,2)-4,1:size(vs1,3)-4), intent(in) :: v1i
+  !    real(wp), parameter :: vellim=2000.0
+  !    real(wp), parameter :: vellim=0.0
+  real(wp) :: coeff
+  integer :: ix2,ix3,lx1,lx2,lx3
+  integer :: idleft,idright,idup,iddown
+  real(wp), dimension(-1:size(vs3,1)-2,-1:size(vs3,2)-2,-1:size(vs3,3)-2) :: param,param2,param3,param4
+  real(wp) :: tstart,tfini
+  
+  lx1=size(vs1,1)-4
+  lx2=size(vs1,2)-4
+  lx3=size(vs1,3)-4
+  
+  !> GHOST CELL VALUES FOR DENSITY (these need to be done last to avoid being overwritten by send/recv's!!!)
+  do ix3=1,lx3
+    do ix2=1,lx2
+      !> logical bottom
+      coeff=ns(2,ix2,ix3,isp)/ns(3,ix2,ix3,isp)
+      ns(0,ix2,ix3,isp)=min(coeff*ns(1,ix2,ix3,isp),ns(1,ix2,ix3,isp))
+      ns(-1,ix2,ix3,isp)=min(coeff*ns(0,ix2,ix3,isp),ns(0,ix2,ix3,isp))
+  
+      !> logical top
+      coeff=ns(lx1-1,ix2,ix3,isp)/ns(lx1-2,ix2,ix3,isp)
+      ns(lx1+1,ix2,ix3,isp)=min(coeff*ns(lx1,ix2,ix3,isp),ns(lx1,ix2,ix3,isp))
+      ns(lx1+2,ix2,ix3,isp)=min(coeff*ns(lx1+1,ix2,ix3,isp),ns(lx1+1,ix2,ix3,isp))
+    end do
+  end do
+  
+  !FOR X1 MOMENTUM DENSITY
+  rhovs1(0,1:lx2,1:lx3,isp)=2*v1i(1,:,:)-vs1(1,1:lx2,1:lx3,isp)
+  !! initially these are velocities.  Also loose definition of 'conformable'.  Also corners never get set, but I suppose they aren't really used anyway.
+  rhovs1(-1,:,:,isp)=rhovs1(0,:,:,isp)+rhovs1(0,:,:,isp)-vs1(1,:,:,isp)
+  rhovs1(lx1+1,1:lx2,1:lx3,isp)=2*v1i(lx1+1,:,:)-vs1(lx1,1:lx2,1:lx3,isp)
+  rhovs1(lx1+2,:,:,isp)=rhovs1(lx1+1,:,:,isp)+rhovs1(lx1+1,:,:,isp)-vs1(lx1,:,:,isp)
+  
+  rhovs1(-1:0,:,:,isp)=rhovs1(-1:0,:,:,isp)*ns(-1:0,:,:,isp)*ms(isp)
+  !! now convert to momentum density
+  rhovs1(lx1+1:lx1+2,:,:,isp)=rhovs1(lx1+1:lx1+2,:,:,isp)*ns(lx1+1:lx1+2,:,:,isp)*ms(isp)
+  
+  !> FOR INTERNAL ENERGY
+  rhoes(0,:,:,isp)=rhoes(1,:,:,isp)
+  rhoes(-1,:,:,isp)=rhoes(1,:,:,isp)
+  rhoes(lx1+1,:,:,isp)=rhoes(lx1,:,:,isp)
+  rhoes(lx1+2,:,:,isp)=rhoes(lx1,:,:,isp)
+  
+  !MZ - collect the x2 boundary conditions here - these are no longer global
+  iddown=mpi_cfg%myid2-1
+  idup=mpi_cfg%myid2+1
+  
+  !> SET THE GLOBAL X2 BOUNDARY CELLS AND ASSUME HALOING WON'T OVERWRITE.
+  !> THIS DIMENSION IS ASSUMED TO NEVER BE PEREIODIC
+  if (iddown==-1) then
+    vs2(:,0,:,isp)=vs2(:,1,:,isp)
+  
+    ns(:,0,:,isp)=ns(:,1,:,isp)
+    ns(:,-1,:,isp)=ns(:,1,:,isp)
+    rhovs1(:,0,:,isp)=rhovs1(:,1,:,isp)
+    rhovs1(:,-1,:,isp)=rhovs1(:,1,:,isp)
+    rhoes(:,0,:,isp)=rhoes(:,1,:,isp)
+    rhoes(:,-1,:,isp)=rhoes(:,1,:,isp)
+  end if
+  if (idup==mpi_cfg%lid2) then
+    vs2(:,lx2+1,:,isp)=vs2(:,lx2,:,isp)
+  
+    ns(:,lx2+1,:,isp)=ns(:,lx2,:,isp)
+    ns(:,lx2+2,:,isp)=ns(:,lx2,:,isp)
+    rhovs1(:,lx2+1,:,isp)=rhovs1(:,lx2,:,isp)
+    rhovs1(:,lx2+2,:,isp)=rhovs1(:,lx2,:,isp)
+    rhoes(:,lx2+1,:,isp)=rhoes(:,lx2,:,isp)
+    rhoes(:,lx2+2,:,isp)=rhoes(:,lx2,:,isp)
+  end if
+  
+  !> NOW DEAL WITH ADVECTION ALONG X3; FIRST IDENTIFY MY NEIGHBORS
+  idleft=mpi_cfg%myid3-1; idright=mpi_cfg%myid3+1
+  
+  !> SET THE GLOBAL x3 BOUNDARY CELLS AND ASSUME THAT HALOING WON'T OVERWRITE...
+  if (.not. isperiodic) then
+    if (idleft==-1) then
+      !! left side is at global boundary, assume haloing won't overwrite
+      vs3(:,:,0,isp)=vs3(:,:,1,isp)
+      !! copy first cell to first ghost (vs3 not advected so only need only ghost)
+  
+      ns(:,:,0,isp)=ns(:,:,1,isp)
+      ns(:,:,-1,isp)=ns(:,:,1,isp)
+      rhovs1(:,:,0,isp)=rhovs1(:,:,1,isp)
+      rhovs1(:,:,-1,isp)=rhovs1(:,:,1,isp)
+      rhoes(:,:,0,isp)=rhoes(:,:,1,isp)
+      rhoes(:,:,-1,isp)=rhoes(:,:,1,isp)
+    end if
+    if (idright==mpi_cfg%lid3) then    !my right boundary is the global boundary, assume haloing won't overwrite
+      vs3(:,:,lx3+1,isp)=vs3(:,:,lx3,isp)    !copy last cell to first ghost (all that's needed since vs3 not advected)
+  
+      ns(:,:,lx3+1,isp)=ns(:,:,lx3,isp)
+      ns(:,:,lx3+2,isp)=ns(:,:,lx3,isp)
+      rhovs1(:,:,lx3+1,isp)=rhovs1(:,:,lx3,isp)
+      rhovs1(:,:,lx3+2,isp)=rhovs1(:,:,lx3,isp)
+      rhoes(:,:,lx3+1,isp)=rhoes(:,:,lx3,isp)
+      rhoes(:,:,lx3+2,isp)=rhoes(:,:,lx3,isp)
+    end if
+  end if
+end subroutine set_global_boundaries
+
+
+!> 3D advection subroutine
 function advec3D_MC_mpi_curv_23(f,v1i,v2i,v3i,dt,x,frank,tagf)
 !------------------------------------------------------------
 !-------ADVECT A VARIABLE IN 3D FOR AN MPI SIMULATION
@@ -248,7 +421,55 @@ function advec3D_MC_mpi_curv_23(f,v1i,v2i,v3i,dt,x,frank,tagf)
 end function advec3D_MC_mpi_curv_23
 
 
-!> dimensionally split advection along the 1-axis
+!> sweep all species along the 1 axis
+subroutine sweep1_allspec(fs,vs1i,dt,x,lsp)
+  real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: fs    !fs includes ghost cells and all species
+  real(wp), dimension(:,:,:,:), intent(in) :: vs1i           ! includes all species velocities
+  real(wp), intent(in) :: dt
+  class(curvmesh), intent(in) :: x
+  integer, intent(in) :: lsp                                 ! sweep the first "lsp" species only
+  integer :: isp
+
+  if (lsp>size(fs,4)) error stop 'number of swept species must be less than or equal to total species number'
+  do isp=1,lsp
+    call sweep1(fs(:,:,:,isp),vs1i(:,:,:,isp),dt,x)
+  end do
+end subroutine sweep1_allspec
+
+
+!> 2-dimensionally split transport for all species
+subroutine sweep2_allspec(fs,vs2i,dt,x,frank)
+  real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: fs    !fs includes ghost cells and all species
+  real(wp), dimension(:,:,:,:), intent(in) :: vs2i           ! includes all species velocities
+  real(wp), intent(in) :: dt
+  class(curvmesh), intent(in) :: x
+  integer, intent(in) :: frank
+  integer :: isp,lsp
+
+  if (lsp>size(fs,4)) error stop 'number of swept species must be less than or equal to total species number'
+  do isp=1,lsp
+    call sweep2(fs(:,:,:,isp),vs2i(:,:,:,isp),dt,x,frank)
+  end do
+end subroutine sweep2_allspec
+
+
+!> 3-dimensionally split transport for all species
+subroutine sweep3_allspec(fs,vs3i,dt,x,frank)
+  real(wp), dimension(-1:,-1:,-1:,:), intent(inout) :: fs    !fs includes ghost cells and all species
+  real(wp), dimension(:,:,:,:), intent(in) :: vs3i           ! includes all species velocities
+  real(wp), intent(in) :: dt
+  class(curvmesh), intent(in) :: x
+  integer, intent(in) :: frank
+  integer :: isp,lsp
+
+  if (lsp>size(fs,4)) error stop 'number of swept species must be less than or equal to total species number'
+  do isp=1,lsp
+    call sweep2(fs(:,:,:,isp),vs3i(:,:,:,isp),dt,x,frank)
+  end do
+end subroutine sweep3_allspec
+
+
+!> dimensionally split advection along the 1-axis for a single species (i.e. only 3D arrays)
 subroutine sweep1(f,v1i,dt,x)
   real(wp), dimension(-1:,-1:,-1:), intent(inout) :: f    !f includes ghost cells
   real(wp), dimension(:,:,:), intent(in) :: v1i
