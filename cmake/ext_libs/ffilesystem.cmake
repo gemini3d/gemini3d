a9cd6e27798d61baf2ee01a29975ab80797ac05f
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-24 11:37:39 -0500

    fortran-filesystem v3.2.3, better link

--- cmake/ext_libs/ffilesystem.cmake
+++ cmake/ext_libs/ffilesystem.cmake
@@ -11,9 +11,14 @@ if(NOT ffilesystem_ROOT)
   set(ffilesystem_ROOT ${CMAKE_INSTALL_PREFIX})
 endif()
 
-
+set(ffs_implib)
 if(BUILD_SHARED_LIBS)
-  set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
+  if(WIN32)
+    set(ffs_implib ${ffilesystem_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/bin/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
+  else()
+    set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}filesystem${CMAKE_SHARED_LIBRARY_SUFFIX})
+  endif()
 else()
   set(ffilesystem_LIBRARIES ${ffilesystem_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}filesystem${CMAKE_STATIC_LIBRARY_SUFFIX})
 endif()
@@ -25,6 +30,9 @@ set(ffilesystem_cmake_args
 -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
 -DCMAKE_BUILD_TYPE=Release
 -DBUILD_TESTING:BOOL=false
+-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+-DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
 )
 
 ExternalProject_Add(FFILESYSTEM
@@ -32,7 +40,7 @@ GIT_REPOSITORY ${ffilesystem_git}
 GIT_TAG ${ffilesystem_tag}
 CMAKE_ARGS ${ffilesystem_cmake_args}
 CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
-BUILD_BYPRODUCTS ${ffilesystem_LIBRARIES}
+BUILD_BYPRODUCTS ${ffilesystem_LIBRARIES} ${ffs_implib}
 INACTIVITY_TIMEOUT 15
 CONFIGURE_HANDLED_BY_BUILD true
 )
@@ -40,13 +48,20 @@ CONFIGURE_HANDLED_BY_BUILD true
 file(MAKE_DIRECTORY ${ffilesystem_INCLUDE_DIRS})
 # avoid generate race condition
 
-add_library(ffilesystem::filesystem INTERFACE IMPORTED)
-target_link_libraries(ffilesystem::filesystem INTERFACE ${ffilesystem_LIBRARIES})
+if(BUILD_SHARED_LIBS)
+  add_library(ffilesystem::filesystem SHARED IMPORTED)
+  if(WIN32)
+  set_target_properties(ffilesystem::filesystem PROPERTIES IMPORTED_IMPLIB ${ffs_implib})
+  endif()
+else()
+  add_library(ffilesystem::filesystem STATIC IMPORTED)
+endif()
+
+set_target_properties(ffilesystem::filesystem PROPERTIES IMPORTED_LOCATION ${ffilesystem_LIBRARIES})
 target_include_directories(ffilesystem::filesystem INTERFACE ${ffilesystem_INCLUDE_DIRS})
 set_target_properties(ffilesystem::filesystem PROPERTIES LINKER_LANGUAGE CXX)
-# if(CMAKE_CXX_COMPILER_ID STREQUAL GNU)
-#   target_link_libraries(ffilesystem::filesystem INTERFACE stdc++)  # did not help
-# endif()
+
+# target_link_libraries(ffilesystem::filesystem INTERFACE stdc++)  # did not help
 # instead, set linker_langauge CXX for the specific targets linking ffilesystem::filesystem
 
 add_dependencies(ffilesystem::filesystem FFILESYSTEM)
