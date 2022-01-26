885e490588031865c90c1ae40573c0abbcf38096
Michael Hirsch <scivision@users.noreply.github.com>
2022-01-26 13:56:26 -0500

    hwm14 test name handling

--- cmake/test/sim.cmake
+++ cmake/test/sim.cmake
@@ -5,13 +5,21 @@ set(sim_timeout 1800)  # seconds to allow simulation tests to run
 
 # --- setup tests
 
-set(_tests mini2dns_fang mini2dew_fang mini3d_fang)
+set(gemini_test_sim_names)
+
+if(hwm14)
+  list(APPEND gemini_test_sim_names mini2dns_hwm14 mini2dew_hwm14 mini3d_hwm14)
+else()
+  list(APPEND gemini_test_sim_names mini2dns_fang mini2dew_fang mini3d_fang)
+endif()
+
 
 if(glow)
-  list(APPEND _tests mini2dns_glow mini2dew_glow mini3d_glow)
+  list(APPEND gemini_test_sim_names mini2dns_glow mini2dew_glow mini3d_glow)
 endif(glow)
 
-foreach(_s ${_tests})
+
+foreach(_s ${gemini_test_sim_names})
   setup_gemini_test(${_s} ${sim_timeout})
 
   if(python)
