bfc318e0f8aa0f9f2eb701b21e26d11fbd1982b1
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-11 12:09:07 -0500

    tests: update hwm14 test names to match gemci

--- cmake/test/sim.cmake
+++ cmake/test/sim.cmake
@@ -8,15 +8,16 @@ set(sim_timeout 1800)  # seconds to allow simulation tests to run
 set(gemini_test_sim_names)
 
 if(hwm14)
-  list(APPEND gemini_test_sim_names mini2dns_hwm14 mini2dew_hwm14 mini3d_hwm14)
+  list(APPEND gemini_test_sim_names mini2dns_hwm14_fang mini2dew_hwm14_fang mini3d_hwm14_fang)
+  if(glow)
+    list(APPEND gemini_test_sim_names mini2dns_hwm14_glow mini2dew_hwm14_glow mini3d_hwm14_glow)
+  endif(glow)
 else()
   list(APPEND gemini_test_sim_names mini2dns_fang mini2dew_fang mini3d_fang)
+  if(glow)
+    list(APPEND gemini_test_sim_names mini2dns_glow mini2dew_glow mini3d_glow)
+  endif(glow)
 endif()
-
-
-if(glow)
-  list(APPEND gemini_test_sim_names mini2dns_glow mini2dew_glow mini3d_glow)
-endif(glow)
 
 
 foreach(_s ${gemini_test_sim_names})
