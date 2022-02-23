d724bac8b212d0643dd398c045b398795f288b64
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-23 11:39:22 -0500

    core test working_directory target_file_dir for hWM14 and similar

--- cmake/test/config.cmake
+++ cmake/test/config.cmake
@@ -29,30 +29,39 @@ COMMAND ${test_cmd} -dryrun
 
 set_tests_properties(gemini:hdf5:${name}:dryrun PROPERTIES
 TIMEOUT 60
-RESOURCE_LOCK cpu_mpi
 FIXTURES_REQUIRED "gemini_exe_fxt;${name}:download_fxt"
 FIXTURES_SETUP hdf5:${name}:dryrun
-REQUIRED_FILES ${out_dir}/inputs/config.nml
 LABELS core
 DISABLED $<NOT:$<BOOL:${hdf5}>>
+WORKING_DIRECTORY $<TARGET_FILE_DIR:gemini.bin>
 )
+# WORKING_DIRECTORY is needed for tests like HWM14 that need data files in binary directory.
 
 
 add_test(NAME gemini:hdf5:${name} COMMAND ${test_cmd})
 
 set_tests_properties(gemini:hdf5:${name} PROPERTIES
 TIMEOUT ${TIMEOUT}
-RESOURCE_LOCK cpu_mpi
 FIXTURES_REQUIRED hdf5:${name}:dryrun
 FIXTURES_SETUP hdf5:${name}:run_fxt
 LABELS core
 DISABLED $<NOT:$<BOOL:${hdf5}>>
 )
+
 if(WIN32 AND CMAKE_VERSION VERSION_GREATER_EQUAL 3.22)
   set_tests_properties(gemini:hdf5:${name}:dryrun gemini:hdf5:${name} PROPERTIES
   ENVIRONMENT_MODIFICATION "PATH=path_list_prepend:${test_dll_path}"
   )
 endif()
+
+set_tests_properties(gemini:hdf5:${name}:dryrun gemini:hdf5:${name} PROPERTIES
+RESOURCE_LOCK cpu_mpi
+REQUIRED_FILES ${out_dir}/inputs/config.nml
+LABELS core
+DISABLED $<NOT:$<BOOL:${hdf5}>>
+WORKING_DIRECTORY $<TARGET_FILE_DIR:gemini.bin>
+)
+# WORKING_DIRECTORY is needed for tests like HWM14 that need data files in binary directory.
 
 
 if(netcdf)
@@ -62,11 +71,8 @@ COMMAND ${test_cmd} -out_format nc -dryrun
 
 set_tests_properties(gemini:netcdf:${name}:dryrun PROPERTIES
 TIMEOUT 60
-RESOURCE_LOCK cpu_mpi
 FIXTURES_REQUIRED "gemini_exe_fxt;${name}:download_fxt"
 FIXTURES_SETUP netcdf:${name}:dryrun
-REQUIRED_FILES ${out_dir}/inputs/config.nml
-LABELS core
 )
 
 add_test(NAME gemini:netcdf:${name}
@@ -75,11 +81,17 @@ COMMAND ${test_cmd} -out_format nc
 
 set_tests_properties(gemini:netcdf:${name} PROPERTIES
 TIMEOUT ${TIMEOUT}
-RESOURCE_LOCK cpu_mpi
 FIXTURES_REQUIRED netcdf:${name}:dryrun
 FIXTURES_SETUP netcdf:${name}:run_fxt
-LABELS core
 )
+
+set_tests_properties(gemini:netcdf:${name}:dryrun gemini:netcdf:${name} PROPERTIES
+RESOURCE_LOCK cpu_mpi
+REQUIRED_FILES ${out_dir}/inputs/config.nml
+LABELS core
+WORKING_DIRECTORY $<TARGET_FILE_DIR:gemini.bin>
+)
+
 endif(netcdf)
 
 compare_gemini_output(${name} ${out_dir} ${ref_dir})
