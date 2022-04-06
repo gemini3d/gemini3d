cf8408c8cb2f916467eb951d7bf3405a91b5a1cb
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2022-04-06 09:47:32 -0400

    control screen printing for cpp frontend

--- src/libgemini.cpp
+++ src/libgemini.cpp
@@ -86,38 +86,45 @@ void gemini_main(struct params* ps, int* plid2in, int* plid3in){
 
   while(t<tdur){
     dt_select_C(&it,&t,&tout,&tglowout,&dt);
-
-    std::cout << " ...Selected time step (seconds) " << dt << std::endl;
+    if (myid ==0){
+      std::cout << " ...Selected time step (seconds) " << dt << std::endl;
+    }
 
     // neutral data
     if (it!=1 && flagneuBG && t>tneuBG){
       neutral_atmos_winds_C(&ymd[0],&UTsec);
       neutral_atmos_wind_update_C(&v2grid,&v3grid);
       tneuBG+=dtneuBG;
-      std::cout << " Computed neutral background..." << std::endl;
+      if (myid==0){
+        std::cout << " Computed neutral background..." << std::endl;
+      }
     }
     if (flagdneu==1){
       neutral_perturb_C(&dt,&t,&ymd[0],&UTsec,&v2grid,&v3grid);
-      std::cout << " Computed neutral perturbations..." << std::endl;
+      if (myid==0){
+        std::cout << " Computed neutral perturbations..." << std::endl;
+      }
     }
 
     // call electrodynamics solution
-    std::cout << " Start electro solution..." << std::endl;
+    //std::cout << " Start electro solution..." << std::endl;
     electrodynamics_C(&it,&t,&dt,&ymd[0],&UTsec);
-    std::cout << " Computed electrodynamics solutions..." << std::endl;
+    //std::cout << " Computed electrodynamics solutions..." << std::endl;
 
     // advance the fluid state variables
     first=it==1;
     fluid_adv(&t,&dt,&ymd[0],&UTsec,&first,&lsp,&myid);
-    std::cout << " Computed fluid update..." << std::endl;
+    //std::cout << " Computed fluid update..." << std::endl;
 
     check_finite_output_C(&t);
     it+=1; t+=dt;
     dateinc_C(&dt,&ymd[0],&UTsec);
-    std::cout << " Time step %d finished: " << it << " " << ymd[0] << " " << ymd[1] << " " << ymd[2] << " " << UTsec << " " << t << std::endl;
     check_dryrun();
     check_fileoutput(&t,&tout,&tglowout,&tmilestone,&flagoutput,&ymd[0],&UTsec);
-    std::cout << " Output cadence variables:  " << tout << " " << tglowout << " " << tmilestone << std::endl;
+    if (myid==0){
+      std::cout << " Time step " << it << " finished: " << ymd[0] << " " << ymd[1] << " " << ymd[2] << " " << UTsec << " " << t << std::endl;
+      //std::cout << " Output cadence variables:  " << tout << " " << tglowout << " " << tmilestone << std::endl;
+    }
   }
 
   /* Call deallocation procedures */
