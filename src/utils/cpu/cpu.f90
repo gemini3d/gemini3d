4c87e20c3799bf99a327f3121995c2e6ba004d0f
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-12 16:58:30 -0400

    cpu_count: correct cast

--- src/utils/cpu/cpu.f90
+++ src/utils/cpu/cpu.f90
@@ -1,7 +1,6 @@
 module cpu
 
 use, intrinsic :: iso_c_binding, only : c_int
-use, intrinsic :: iso_fortran_env, only : int32
 implicit none (type, external)
 
 interface
@@ -14,7 +13,7 @@ contains
 
 integer function cpu_count()
 
-cpu_count = int(cpu_count_c(), int32)
+cpu_count = int(cpu_count_c())
 
 end function cpu_count
 
