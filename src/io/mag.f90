f0f22344a76588acb3c99e081a7161e2d78ede80
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-01 01:51:36 -0400

    output_magfile: consolidate mpi=.false.

--- src/io/mag.f90
+++ src/io/mag.f90
@@ -2,19 +2,10 @@ submodule (io) mag
 
 use sanity_check, only : check_finite_mag
 use filesystem, only : mkdir
+use timeutils, only : date_filename
+use h5fortran, only : hdf5_file
 
 implicit none (type, external)
-
-interface ! mag_*.f90
-
-module subroutine output_magfields_hdf5(outdir,ymd,UTsec,Br,Btheta,Bphi)
-character(*), intent(in) :: outdir
-integer, intent(in) :: ymd(3)
-real(wp), intent(in) :: UTsec
-real(wp), dimension(:), intent(in)  :: Br,Btheta,Bphi
-end subroutine
-
-end interface
 
 contains
 
@@ -25,21 +16,27 @@ module procedure create_outdir_mag
 integer :: ierr
 
 !NOTE HERE THAT WE INTERPRET OUTDIR AS THE BASE DIRECTORY CONTAINING SIMULATION OUTPUT
-call mkdir(outdir//'/magfields/')
-! call mkdir(outdir//'/magfields/input/')
-! call copy_file(fieldpointfile, outdir//'/magfields/input/magfieldpoints.dat')
+call mkdir(outdir // '/magfields')
 
 end procedure create_outdir_mag
 
 
 module procedure output_magfields
+!! WE ASSUME THE ROOT PROCESS HAS ALREADY REDUCED THE MAGNETIC FIELD DATA
 
-select case (out_format)
-case ('h5')
-  call output_magfields_hdf5(outdir,ymd,UTsec,Br,Btheta,Bphi)
-case default
-  error stop 'ERROR:gemini3d:mag:output_magfields: unknown file format' // out_format
-end select
+character(:), allocatable :: filename
+type(hdf5_file) :: h
+
+filename = date_filename(outdir // '/magfields', ymd, UTsec) // '.h5'
+print *, '  Output file name (magnetic fields):  ',filename
+
+call h%open(filename, action='rw', comp_lvl=comp_lvl, mpi=.false.)
+
+call h%write('/magfields/Br',     real(Br))
+call h%write('/magfields/Btheta', real(Btheta))
+call h%write('/magfields/Bphi',   real(Bphi))
+
+call h%close()
 
 call check_finite_mag(outdir, Br, Btheta, Bphi)
 
