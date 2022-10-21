32ce55475766a0f66e520454363ddc2c18524fef
Matthew D. Zettergren <mattzett@gmail.com>
2022-10-21 17:27:22 -0400

    output working now

--- src/libgemini_c.f90
+++ src/libgemini_c.f90
@@ -294,7 +294,7 @@ contains
 
 
   !> wrapper to have a worker dump their state var data to a file
-  subroutine plasma_output_nompi_C(cfgC,ymd,UTsec,fluidvarsC,electrovarsC)
+  subroutine plasma_output_nompi_C(cfgC,ymd,UTsec,fluidvarsC,electrovarsC) bind(C,name="plasma_output_nompi_C")
     type(c_ptr), intent(in) :: cfgC
     integer, dimension(3), intent(in) :: ymd
     real(wp), intent(in) :: UTsec
