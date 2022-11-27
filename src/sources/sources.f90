6ee4e75776d8ff34f55b2e965e575f1caafe1c86
jdiazpena <jdiazpena@gmail.com>
2022-11-27 15:47:18 -0500

    Add loss factor

--- src/sources/sources.f90
+++ src/sources/sources.f90
@@ -536,7 +536,7 @@ call FBIheating(nn,Tn,ns,Ts,E1,E2,E3,x,FBIproduction,FBIlossfactor)
 !CORRECT TEMP EXPRESSIONS TO CORRESPOND TO INTERNAL ENERGY SOURCES
 Pr(:,:,:,lsp)=Pr(:,:,:,lsp)+FBIproduction+iePT*ns(1:lx1,1:lx2,1:lx3,lsp)*kB/(gammas(lsp)-1)   !Arg, forgot about the damn ghost cells in original code...
 !Lo(:,:,:,lsp)=Lo(:,:,:,lsp)*FBIlossfactor+ieLT
-Lo(:,:,:,lsp)=Lo(:,:,:,lsp)+ieLT
+Lo(:,:,:,lsp)=Lo(:,:,:,lsp)+(ieLT*FBIlossfactor)
 
 end subroutine srcsEnergy
 
