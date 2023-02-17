0d6f34e6039f1150b83a96ad4360bb5d15603a67
scivision <scivision@users.noreply.github.com>
2023-02-17 15:30:49 -0500

    interp: lint

--- src/numerical/interpolation/interpolation.f90
+++ src/numerical/interpolation/interpolation.f90
@@ -101,25 +101,28 @@ pure real(wp) function interp3(x1,x2,x3,f,x1i,x2i,x3i)
 
 real(wp), dimension(:), intent(in) :: x1,x2,x3,x1i,x2i,x3i
 real(wp), dimension(:,:,:), intent(in) :: f
-dimension :: interp3(1:size(x1i,1))     !interpolated points are a flat list
+dimension :: interp3(1:size(x1i,1))
+!! interpolated points are a flat list
 
-real(wp) :: fx1ix2pix3p,fx1ix2nix3p,fx1ix2pix3n,fx1ix2nix3n    !function estimates at x1i point  vs. at x2 interfaces
+real(wp) :: fx1ix2pix3p,fx1ix2nix3p,fx1ix2pix3n,fx1ix2nix3n
+!! function estimates at x1i point  vs. at x2 interfaces
 real(wp) :: fx2ix3p,fx2ix3n
-real(wp) :: slope                     !temp value for slope for interpolations
+real(wp) :: slope
+!! temp value for slope for interpolations
 
 integer :: lx1,lx2,lx3,lxi,ix1,ix2,ix3,ixi
 integer :: ix10,ix1fin,ix20,ix2fin,ix30,ix3fin
 
 
-
-lx1=size(x1,1)
-lx2=size(x2,1)
-lx3=size(x3,1)
-lxi=size(x1i,1)    !only one size since this a flat list of grid points
-
+lx1  = size(x1,1)
+lx2 = size(x2,1)
+lx3 = size(x3,1)
+lxi = size(x1i,1)
+!! only one dimension since this a flat list of grid points
 
 do ixi=1,lxi
-  !find the x1 'bin' for this point; i.e. find ix1 s.t. xi(ix1i) is between x(ix1-1) and x(ix1)
+  !! find the x1 'bin' for this point;
+  !! i.e. find ix1 s.t. xi(ix1i) is between x(ix1-1) and x(ix1)
   ix10=1
   ix1=lx1/2
   ix1fin=lx1
@@ -127,7 +130,8 @@ do ixi=1,lxi
     do while(.not.(x1i(ixi)>=x1(ix1-1) .and. x1i(ixi)<=x1(ix1)))    !keep going until we are in the interval we want
       if (x1i(ixi)>=x1(ix10) .and. x1i(ixi)<=x1(ix1)) then    !left half (correct guess)
         ix1fin=ix1
-      else    !wrong take the "right" half (har har)
+      else
+        !! wrong take the "right" half (har har)
         ix10=ix1
       end if
       ix1=(ix1fin+ix10)/2
@@ -141,8 +145,7 @@ do ixi=1,lxi
     ix1=lx1
   end if
 
-
-  !find the x2 'bin' for this point; i.e. find ix2 s.t. x2i(ix2i) is between x2(ix2-1) and x2(ix2)
+  !> find the x2 'bin' for this point; i.e. find ix2 s.t. x2i(ix2i) is between x2(ix2-1) and x2(ix2)
   ix20=1
   ix2=lx2/2
   ix2fin=lx2
@@ -150,30 +153,32 @@ do ixi=1,lxi
     do while(.not.(x2i(ixi)>=x2(ix2-1) .and. x2i(ixi)<=x2(ix2)))    !keep going until we are in the interval we want
       if (x2i(ixi)>=x2(ix20) .and. x2i(ixi)<=x2(ix2)) then    !left half (correct guess)
         ix2fin=ix2
-      else    !wrong take the "right" half (har har)
+      else
+        !! wrong take the "right" half (har har)
         ix20=ix2
       end if
-      ix2=(ix2fin+ix20)/2
-      if (ix20==ix2) then
-        ix2=lx2
-      end if
+      ix2 = (ix2fin + ix20) / 2
+      if (ix20 == ix2) ix2=lx2
     end do
-  else if (x2i(ixi)<x2(1)) then
-    ix2=1
+  else if (x2i(ixi) < x2(1)) then
+    ix2 = 1
   else
-    ix2=lx2
+    ix2 = lx2
   end if
 
-
-  !find the x3 'bin' for this point; i.e. find ix3 s.t. x3i(ix3i) is between x3(ix3-1) and x3(ix3)
+  !> find the x3 'bin' for this point; i.e. find ix3 s.t. x3i(ix3i) is between x3(ix3-1) and x3(ix3)
   ix30=1
-  ix3 = max(lbound(x3, dim=1)+1, lx3/2)  !< avoid bounds error when lx3==2 in "do while(... x3(ix3-1) ...)"
+  ix3 = max(lbound(x3, dim=1)+1, lx3/2)
+  !! avoid bounds error when lx3==2 in "do while(... x3(ix3-1) ...)"
   ix3fin=lx3
   if (x3i(ixi) >= x3(1) .and. x3i(ixi) <= x3(lx3)) then    !in bounds
-    do while(.not.(x3i(ixi) >= x3(ix3-1) .and. x3i(ixi) <= x3(ix3)))    !keep going until we are in the interval we want
-      if (x3i(ixi) >= x3(ix30) .and. x3i(ixi) <= x3(ix3)) then    !left half (correct guess)
+    do while(.not.(x3i(ixi) >= x3(ix3-1) .and. x3i(ixi) <= x3(ix3)))
+      !! keep going until we are in the interval we want
+      if (x3i(ixi) >= x3(ix30) .and. x3i(ixi) <= x3(ix3)) then
+        !! left half (correct guess)
         ix3fin=ix3
-      else    !wrong take the "right" half (har har)
+      else
+        !! wrong take the "right" half (har har)
         ix30=ix3
       end if
       ix3=(ix3fin+ix30)/2
@@ -184,7 +189,6 @@ do ixi=1,lxi
   else
     ix3=lx3
   end if
-
 
   if (ix1>1 .and. ix1<=lx1 .and. ix2>1 .and. ix2<=lx2 .and. ix3>1 .and. ix3<=lx3) then   !interpolation
     !interpolate x1 for fixed values of x2,x3 (four separate interps)
@@ -204,7 +208,6 @@ do ixi=1,lxi
     slope=(f(ix1,ix2,ix3)-f(ix1-1,ix2,ix3))/(x1(ix1)-x1(ix1-1))
     fx1ix2nix3n=f(ix1-1,ix2,ix3)+slope*(x1i(ixi)-x1(ix1-1))
 
-
     !interpolate between each x2 value (two separate interps)
     !interp in x2 for the x3 prev points
     slope=(fx1ix2nix3p-fx1ix2pix3p)/(x2(ix2)-x2(ix2-1))
@@ -214,22 +217,23 @@ do ixi=1,lxi
     slope=(fx1ix2nix3n-fx1ix2pix3n)/(x2(ix2)-x2(ix2-1))
     fx2ix3n=fx1ix2pix3n+slope*(x2i(ixi)-x2(ix2-1))
 
-
     !finally an interpolation in x2 to finish things off (single interp)
     slope=(fx2ix3n-fx2ix3p)/(x3(ix3)-x3(ix3-1))
     interp3(ixi)=fx2ix3p+slope*(x3i(ixi)-x3(ix3-1))
   else
-    interp3(ixi)=0._wp
+    interp3(ixi) = 0
   end if
 end do
 
-
 !THERE IS SOME ISSUE WITH POINTS OUTSIDE INTERPOLANT DOMAIN - THIS IS A WORKAROUND UNTIL I CAN PIN DOWN THE EXACT PROBLEM
 do ixi=1,lxi
-if(x1i(ixi)<x1(1) .or. x1i(ixi)>x1(lx1) .or. x2i(ixi)<x2(1) .or. x2i(ixi)>x2(lx2) .or. x3i(ixi)<x3(1) .or. &
-   x3i(ixi)>x3(lx3) .or. lx1==1 .or. lx2==1 .or. lx3==1) then     !also cover the case where there is a singleton dimension...
-  interp3(ixi)=0._wp
-end if
+  if(x1i(ixi)<x1(1) .or. x1i(ixi)>x1(lx1) .or. &
+    x2i(ixi)<x2(1) .or. x2i(ixi)>x2(lx2) .or. &
+    x3i(ixi)<x3(1) .or. x3i(ixi)>x3(lx3) .or. &
+    lx1==1 .or. lx2==1 .or. lx3==1) then
+    !! also covers if a singleton dimension...
+    interp3(ixi) = 0
+  end if
 end do
 
 end function interp3
