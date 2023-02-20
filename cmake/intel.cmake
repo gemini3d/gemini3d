aaa3f8c26ae1194e58b26bb98cdb00654519f42f
scivision <scivision@users.noreply.github.com>
2023-02-20 17:22:22 -0500

    doc [skip ci]

--- cmake/intel.cmake
+++ cmake/intel.cmake
@@ -22,7 +22,10 @@ if(NOT WIN32)
 endif()
 
 add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-heap-arrays>)
-# heap-arrays: avoid stack overflow, especially in units tests like interp3
+# heap-arrays: avoid stack overflow, for both unit tests and actual simulations
+# it's needed on Linux and Windows
+# https://www.intel.com/content/www/us/en/develop/documentation/fortran-compiler-oneapi-dev-guide-and-reference/top/compiler-reference/compiler-options/advanced-optimization-options/heap-arrays.html
+
 
 # --- IMPORTANT: bounds checking
 # add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-check>")
