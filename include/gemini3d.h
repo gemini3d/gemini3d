2793970a20129c7c36ba404edacb603ff2c85619
Matthew Zettergren <zettergm@Matthews-MacBook-Pro.local>
2023-04-06 17:20:46 -0400

    fix typo in header file

--- include/gemini3d.h
+++ include/gemini3d.h
@@ -103,7 +103,7 @@ extern void get_neutralperturb_interptype_C(void**,int*);
 extern void swap_statevars_C(double**, double**);
 extern void interp3_C(double**,double**,double**,int*,int*,int*,double**,double**,int*,int*,
 		double*,double*,double*,double**,double**);
-extern void inter2_C(double**,double**,int*,int*,double**,double**,int*,int*,double*,double*,double**,
+extern void interp2_C(double**,double**,int*,int*,double**,double**,int*,int*,double*,double*,double**,
 		double**);
 
 /* interfaces for libgemini_mpi */
