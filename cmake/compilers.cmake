dd6db34d8784ac45d92dce21bb89a873a68413ed
scivision <scivision@users.noreply.github.com>
2023-08-22 10:24:29 -0400

    warn if C++ and Fortran compilers have incompatible ABI for exception handling

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,14 +1,23 @@
 include(CheckFortranCompilerFlag)
 
-# check C++ and Fortran compiler ABI compatibility
+# --- abi check: C++ and Fortran compiler ABI compatibility
 
-if(NOT abi_ok)
-  message(CHECK_START "checking that compilers can link together")
-  try_compile(abi_ok
+function(abi_check)
+if(NOT abi_compile)
+  message(CHECK_START "checking that C, C++, and Fortran compilers can link")
+  try_compile(abi_compile
   ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check
   abi_check
+  OUTPUT_VARIABLE abi_output
   )
-  if(abi_ok)
+  if(abi_output MATCHES "ld: warning: could not create compact unwind for")
+    message(WARNING "C++ exception handling will not work reliably due to incompatible compilers:
+    C++ compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}
+    Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}"
+    )
+  endif()
+
+  if(abi_compile)
     message(CHECK_PASS "OK")
   else()
     message(FATAL_ERROR "ABI-incompatible compilers:
@@ -18,11 +27,8 @@ if(NOT abi_ok)
     )
   endif()
 endif()
-
-# avoid MacOS unwind warnings
-if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-  add_link_options(-Wl,-no_compact_unwind)
-endif()
+endfunction(abi_check)
+abi_check()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   include(${CMAKE_CURRENT_LIST_DIR}/intel.cmake)
