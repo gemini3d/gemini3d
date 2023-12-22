2a7d7ef0862dc5dc14bf5d121247896496eb44fd
scivision <scivision@users.noreply.github.com>
2023-12-22 15:09:43 -0500

    better test for MPI-3 Fortran

--- cmake/mpi.cmake
+++ cmake/mpi.cmake
@@ -13,23 +13,21 @@ set(MPI_DETERMINE_LIBRARY_VERSION true)
 
 find_package(MPI COMPONENTS C CXX Fortran REQUIRED)
 
-find_file(mpi_f08_mod NAMES mpi_f08.mod
-NO_DEFAULT_PATH
-HINTS ${MPI_Fortran_INCLUDE_DIRS}
-)
-
 message(STATUS "${MPI_Fortran_LIBRARY_VERSION_STRING}")
 message(STATUS "MPI libs: ${MPI_Fortran_LIBRARIES}")
 message(STATUS "MPI include: ${MPI_Fortran_INCLUDE_DIRS}")
-message(STATUS "MPI_f08 module: ${mpi_f08_mod}")
 message(STATUS "MPI compile flags: ${MPI_Fortran_COMPILER_FLAGS}")
 message(STATUS "MPI link flags: ${MPI_Fortran_LINK_FLAGS}")
 
-if(NOT mpi_f08_mod)
-  message(FATAL_ERROR "Fortran MPI ${MPI_Fortran_VERSION} doesn't have MPI-3 Fortran mpi_f08.mod, searched using ${MPI_Fortran_INCLUDE_DIRS}")
+if(MPI_Fortran_HAVE_F08_MODULE)
+  return()
 endif()
 include(cmake/openmpi.cmake)
 
+set(CMAKE_REQUIRED_LIBRARIES MPI::MPI_Fortran)
+
+# sometimes factory FindMPI.cmake doesn't define this
+message(CHECK_START "Checking for Fortran MPI-3 binding")
 check_source_compiles(Fortran
 [=[
 program test
@@ -42,6 +40,9 @@ end program
 MPI_Fortran_HAVE_F08_MODULE
 )
 
-if(NOT MPI_Fortran_HAVE_F08_MODULE)
-  message(FATAL_ERROR "Fortran MPI ${MPI_Fortran_VERSION} doesn't have MPI-3")
+if(MPI_Fortran_HAVE_F08_MODULE)
+  message(CHECK_PASS "yes")
+else()
+  message(CHECK_FAIL "no")
+  message(WARNING "MPI-3 Fortran module mpi_f08 not found, builds may fail.")
 endif()
