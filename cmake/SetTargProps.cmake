c0f28f02ac0ba47599d0e23e82fe7cf7e3c7d243
scivision <scivision@users.noreply.github.com>
2024-01-09 09:25:08 -0500

    cmake lint

--- cmake/SetTargProps.cmake
+++ cmake/SetTargProps.cmake
@@ -1,14 +1,16 @@
 function(set_targ_props)
 
-foreach(t ${ARGV})
+foreach(t IN ITEMS ${ARGV})
 
   # get_target_property(_bin ${t} BINARY_DIR)
   set(_bin ${PROJECT_BINARY_DIR})
 
   target_include_directories(${t} INTERFACE
   $<BUILD_INTERFACE:${_bin}/include>
-  $<INSTALL_INTERFACE:include>)
-  set_target_properties(${t} PROPERTIES Fortran_MODULE_DIRECTORY ${_bin}/include)
+  $<INSTALL_INTERFACE:include>
+  )
+
+  set_property(TARGET ${t} PROPERTY Fortran_MODULE_DIRECTORY ${_bin}/include)
 endforeach()
 
 endfunction(set_targ_props)
