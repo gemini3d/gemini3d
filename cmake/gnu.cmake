70290758f7b607ede6b00e0bd6f7e9716f992b88
scivision <scivision@users.noreply.github.com>
2024-05-07 03:44:18 -0400

    apply compiler flags per directory to guard submodules
    there is little use warning on submodule packages

--- cmake/gnu.cmake
+++ cmake/gnu.cmake
@@ -2,23 +2,19 @@ include(CheckCompilerFlag)
 # NOTE: don't use -march=native as GCC doesn't support all CPU arches with that option.
 # add_compile_options(-mtune=native)
 
-add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>)
+# flags we don't want leaking into Git submodules to avoid excessive warnings on projects we don't control.
+set(${PROJECT_NAME}_flags
+-Wall
+$<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
+$<$<COMPILE_LANGUAGE:Fortran>:-Wno-uninitialized>
+$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<VERSION_LESS:$<Fortran_COMPILER_VERSION>,10>>:-Wno-conversion>
+$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<Fortran_COMPILER_VERSION:9.3.0>>:-Wno-maybe-uninitialized>
+)
+# lot of spurious warnings on allocatable scalar character -Wno-maybe-uninitialized
 
 # --- IMPORTANT
 add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-Werror=array-bounds;-fcheck=all>")
 # --- IMPORTANT: options help trap array indexing/bounds errors at runtime
-
-add_compile_options(-Wall)
-# Fortran -Wall causes false positives in many Fortran projects
-add_compile_options(
-$<$<COMPILE_LANGUAGE:Fortran>:-Wno-uninitialized>
-$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<VERSION_LESS:$<Fortran_COMPILER_VERSION>,10>>:-Wno-conversion>
-)
-
-if(dev)
-  add_compile_options(-Wextra)
-  # -Wpedantic makes too many false positives
-endif()
 
 # avoid backtrace that's unusable without -g
 add_compile_options($<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>)
@@ -30,6 +26,3 @@ if(dosubflag)
 endif()
 
 # add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-ffpe-trap=invalid,zero,overflow>")#,underflow)
-
-# lot of spurious warnings on allocatable scalar character
-add_compile_options($<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<Fortran_COMPILER_VERSION:9.3.0>>:-Wno-maybe-uninitialized>)
--- cmake/intel.cmake
+++ cmake/intel.cmake
@@ -1,7 +1,12 @@
-add_compile_options(
+
+# flags we don't want leaking into Git submodules to avoid excessive warnings on projects we don't control.
+set(${PROJECT_NAME}_flags
 $<$<COMPILE_LANGUAGE:Fortran>:-warn>
 $<$<COMPILE_LANGUAGE:C,CXX>:-Wall>
 $<$<COMPILE_LANGUAGE:Fortran>:-traceback>
+)
+
+add_compile_options(
 $<$<CONFIG:Debug>:-Rno-debug-disables-optimization>
 )
 # remark #5415: Feature not yet implemented: Some 'check' options temporarily disabled.
@@ -30,16 +35,14 @@ add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-heap-arrays>)
 # add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-check>")
 # -check is an alias for -check all. However, MUMPS trips on -check, so we have to use a less stringent check.
 add_compile_options(
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-CB>"
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:RelWithDebInfo>>:-CB>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-CB>"
 )
 # -CB is an alias for -check bounds.
 # separate $<CONFIG: lines for CMake < 3.19
 # --- IMPORTANT
 
 add_compile_options(
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-debug>"
-"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:RelWithDebInfo>>:-debug>"
+"$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-debug>"
 )
 # -debug is an alias for -debug all
 # -fpe0 causes MUMPS failures (internal to MUMPS)
