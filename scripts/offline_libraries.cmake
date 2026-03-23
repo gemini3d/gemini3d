ae3054097c8552a54eca3c34d42d86cb2a86b05e
scivision <scivision@users.noreply.github.com>
2026-03-23 14:14:34 -0400

    offline build toolchain
    This can serve the offline funcitonality of Git submodules

--- scripts/offline_libraries.cmake
+++ scripts/offline_libraries.cmake
@@ -7,20 +7,23 @@ if(NOT DEFINED offline_dir)
   set(offline_dir ${CMAKE_CURRENT_LIST_DIR}/../offline)
 endif()
 
-file(MAKE_DIRECTORY ${offline_dir})
+file(MAKE_DIRECTORY "${offline_dir}")
 if(NOT IS_DIRECTORY "${offline_dir}")
   message(FATAL_ERROR "Failed to create offline directory: ${offline_dir}")
 endif()
-if(NOT EXISTS ${offline_dir}/.gitignore)
-  file(WRITE ${offline_dir}/.gitignore "*\n")
+if(NOT EXISTS "${offline_dir}/.gitignore")
+  file(WRITE "${offline_dir}/.gitignore" "*\n")
 endif()
 
-file(READ ${CMAKE_CURRENT_LIST_DIR}/../cmake/libraries.json json)
+file(READ "${CMAKE_CURRENT_LIST_DIR}/../cmake/libraries.json" json)
 
 foreach(lib IN ITEMS ffilesystem glow h5fortran hwm14 msis)
-  string(JSON url GET "${json}" "${lib}")
-  FetchContent_Populate(${lib} URL ${url}
-    SOURCE_DIR ${offline_dir}/${lib}-src
-    SUBBUILD_DIR ${offline_dir}/${lib}-subbuild
+  string(JSON ${lib}_url GET "${json}" "${lib}")
+
+  set(${lib}_src "${offline_dir}/${lib}-src")
+
+  FetchContent_Populate(${lib}_source
+    URL ${${lib}_url}
+    SOURCE_DIR ${${lib}_src}
   )
 endforeach()
