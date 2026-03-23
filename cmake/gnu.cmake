3cbd03d25d7f976fa4d472d98c8326f4ee435455
scivision <scivision@users.noreply.github.com>
2026-03-23 14:57:42 -0400

    no need for extra flags

--- cmake/gnu.cmake
+++ cmake/gnu.cmake
@@ -2,11 +2,10 @@ include(CheckCompilerFlag)
 # NOTE: don't use -march=native as GCC doesn't support all CPU arches with that option.
 # add_compile_options(-mtune=native)
 
-# flags we don't want leaking into Git submodules to avoid excessive warnings on projects we don't control.
-set(${PROJECT_NAME}_flags
+# flags we don't want leaking into child projects to avoid excessive warnings on projects we don't control.
+set(gemini3d_flags
 $<$<CONFIG:Debug,RelWithDebInfo>:-Wall>
 $<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
-$<$<COMPILE_LANGUAGE:Fortran>:-Wno-uninitialized>
 $<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<VERSION_LESS:$<Fortran_COMPILER_VERSION>,10>>:-Wno-conversion>
 $<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<Fortran_COMPILER_VERSION:9.3.0>>:-Wno-maybe-uninitialized>
 )
@@ -16,22 +15,22 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU" AND
    CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL "15.0" AND
    CMAKE_Fortran_COMPILER_VERSION VERSION_LESS "15.2"
    )
-  list(APPEND ${PROJECT_NAME}_flags "$<$<COMPILE_LANGUAGE:Fortran>:-Wno-external-argument-mismatch>")
+  list(APPEND gemini3d_flags "$<$<COMPILE_LANGUAGE:Fortran>:-Wno-external-argument-mismatch>")
 endif()
 # workaround for src/numerical/coord/newton.f90 build fail despite the code being correct.
 # https://www.scivision.dev/gfortran-15-external-argument-mismatch/
 
 # --- IMPORTANT
-list(APPEND ${PROJECT_NAME}_flags "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-Werror=array-bounds$<SEMICOLON>-fcheck=all>")
+list(APPEND gemini3d_flags "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-Werror=array-bounds$<SEMICOLON>-fcheck=all>")
 # --- IMPORTANT: options help trap array indexing/bounds errors at runtime
 
 # avoid backtrace that's unusable without -g
-list(APPEND ${PROJECT_NAME}_flags "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>")
+list(APPEND gemini3d_flags "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>")
 
 # Wdo-subscript is known to warn on obvious non-problems
 check_compiler_flag(Fortran -Wdo-subscript dosubflag)
 if(dosubflag)
-  list(APPEND ${PROJECT_NAME}_flags $<$<COMPILE_LANGUAGE:Fortran>:-Wno-do-subscript>)
+  list(APPEND gemini3d_flags $<$<COMPILE_LANGUAGE:Fortran>:-Wno-do-subscript>)
 endif()
 
 # add_compile_options("$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-ffpe-trap=invalid,zero,overflow>")#,underflow)
