903f8c149a17bfba74c548b77246e756bd591456
scivision <scivision@users.noreply.github.com>
2026-08-03 12:11:42 -0400

    msis_setup: -features outputs MSIS2 if present, and puts msis_setup in top binary dir

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -4,6 +4,7 @@ if(gemini3d_IS_TOP_LEVEL)
 
 add_feature_info(GLOW gemini3d_glow "airglow / aurora model")
 add_feature_info(HWM14 gemini3d_hwm14 "HWM14 neutral winds model")
+add_feature_info(MSIS2 gemini3d_msis2 "enable MSIS 2.x neutral atmosphere model")
 
 add_feature_info(PyGemini gemini3d_python "simulation generation, HPC script generator and plotting")
 add_feature_info(MatGemini gemini3d_matlab "checks not as extensive as Python, and slow")
